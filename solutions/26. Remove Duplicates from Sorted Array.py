c75b7179d75c3cf310137c72a2936265e0a6e680
Karry Wong <ucdwong@ucdavis.edu>
2021-01-08 00:09:54 -0800

    108 ms

--- solutions/26. Remove Duplicates from Sorted Array.py
+++ solutions/26. Remove Duplicates from Sorted Array.py
@@ -20,11 +20,17 @@ class Solution:
         # return j
         
         ### Soln 3 - two pointer,
-        if not nums: return 0
-        j = 0
-        for i in range(1, len(nums)):
-            if nums[i] != nums[j]:
-                j += 1
-                nums[j] = nums[i]
-        return j + 1
+        # if not nums: return 0
+        # j = 0
+        # for i in range(1, len(nums)):
+        #     if nums[i] != nums[j]:
+        #         j += 1
+        #         nums[j] = nums[i]
+        # return j + 1
+        
+        ### Soln 4 - clever, reverse pop
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i] == nums[i-1]:
+                nums.pop(i)
+        return len(nums)
 ​
