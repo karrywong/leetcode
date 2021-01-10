62fdda393b94ba502203c7ff5f3f193da8a15210
Karry Wong <ucdwong@ucdavis.edu>
2021-01-09 22:20:17 -0800

    248 ms

---
+++ solutions/628. Maximum Product of Three Numbers.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        ### Soln 1 - intuition
+        nums.sort()
+        return max( nums[-1]*nums[-2]*nums[-3], nums[0]*nums[1]*nums[-1] )
+​
