2188b577139f2cd270856d79b4b5e18b2ee1c59c
Karry Wong <ucdwong@ucdavis.edu>
2021-01-13 16:15:27 -0800

    632 ms

--- solutions/973. K Closest Points to Origin.py
+++ solutions/973. K Closest Points to Origin.py
@@ -1,24 +1,41 @@
-class Solution:
-    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        ### Soln 1 - Hashmap O(n)
-        lib = {}
+​
+#             while True:
+#                 while i < j and dist(i) < pivot:
+#                     i += 1
+#                 while i <= j and dist(j) >= pivot:
+#                     j -= 1
+#                 if i >= j: break
+#                 points[i], points[j] = points[j], points[i]
+​
+#             points[oi], points[j] = points[j], points[oi]
+#             return j
+​
+#         sort(0, len(points) - 1, K)
+#         return points[:K]
         
-        for n in points:
-            dist = n[0]**2 + n[1]**2
-            if dist not in lib:
-                lib[dist] = [n]
-            else:
-                lib[dist].append(n)
+        # ### Soln 0 - LeetCode O(NlogN) (We should learn from it, lambda!!!):
+        points.sort(key = lambda P: P[0]**2 + P[1]**2)
+        return points[:K]
+        
+#         ### Soln 1 - Hashmap O(NlogN), my original attempt
+#         lib = {}
+        
+#         for n in points:
+#             dist = n[0]**2 + n[1]**2
+#             if dist not in lib:
+#                 lib[dist] = [n]
+#             else:
+#                 lib[dist].append(n)
                 
-        res = []
-        lst = [v for v in sorted(lib.keys())]
-        i = 0
-        while K > 0: 
-            k = lst[i]
-            val = len(lib[k])
-            res += lib[k]
+#         res = []
+#         lst = [v for v in sorted(lib.keys())]
+#         i = 0
+#         while K > 0: 
+#             k = lst[i]
+#             val = len(lib[k])
+#             res += lib[k]
             
-            K -= val
-            i += 1
+#             K -= val
+#             i += 1
             
-        return res
+#         return res
