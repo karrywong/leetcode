61a476ba8b2d92c551a0309675289765fd2c67f0
Karry Wong <ucdwong@ucdavis.edu>
2021-01-15 00:02:51 -0800

    104 ms

---
+++ solutions/1572. Matrix Diagonal Sum.py
@@ -0,0 +1,4 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        ### Soln 1 - use i and ~i and iterate through entire matrix
+        return sum([mat[i][i] + mat[i][~i] if i - ~i != len(mat) else mat[i][i] for i in range(len(mat))])
