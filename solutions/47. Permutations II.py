6664ce62d41c2c64829fc6919f35606dc6aa71de
Karry Wong <ucdwong@ucdavis.edu>
2021-01-18 00:39:16 -0800

    104 ms

---
+++ solutions/47. Permutations II.py
@@ -0,0 +1,29 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        ### Soln 1 - recursion II from Permutations I with slight modification
+        def helper(nums):
+            if len(nums) == 1:
+                return [nums]
+            
+            curr = helper(nums[1:])
+            tmp = []
+            for i in range(len(nums)):
+                for x in curr:
+                    tmp1 = x.copy()
+                    tmp1.insert(i,nums[0])
+                    if tmp1 not in tmp:
+                        tmp.append(tmp1)
+            return tmp
+        
+        return helper(nums)
+        
+#         ### Soln 0 - repeat solution for Permutations I and get unique elements
+#         def helper(nums):
+#             if len(nums) == 1:
+#                 return [nums]         
+#             curr = helper(nums[1:])
+#             return [x[:i] + [nums[0]] + x[i:] for x in curr for i in range(len(nums))]
+        
+#         lst = helper(nums)
+#         counts = collections.Counter(tuple(v) for v in lst)
+#         return [x for x in counts.keys()]
