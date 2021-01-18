1866b34a0f3dea4254aad172fa662f987b76329a
Karry Wong <ucdwong@ucdavis.edu>
2021-01-18 00:36:50 -0800

    28 ms

--- solutions/46. Permutations.py
+++ solutions/46. Permutations.py
@@ -1,12 +1,29 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ### Soln 2 - recursion
+        ### Soln 3 - recursion II
         def helper(nums):
             if len(nums) == 1:
-                return [nums]         
+                return [nums]
+            
             curr = helper(nums[1:])
-            return [x[:i] + [nums[0]] + x[i:] for x in curr for i in range(len(nums))]
+            
+            tmp = []
+            for i in range(len(nums)):
+                for x in curr:
+                    tmp1 = x.copy()
+                    tmp1.insert(i,nums[0])
+                    tmp.append(tmp1)
+            return tmp
         return helper(nums)
+​
+        
+        # ### Soln 2 - recursion
+        # def helper(nums):
+        #     if len(nums) == 1:
+        #         return [nums]         
+        #     curr = helper(nums[1:])
+        #     return [x[:i] + [nums[0]] + x[i:] for x in curr for i in range(len(nums))]
+        # return helper(nums)
         
         
 #         ### Soln 1 - backtracking (Heap's algorithm)
@@ -14,20 +31,3 @@ class Solution:
 #         def backtrack(first = 0):
 #             # if all integers are used up
 #             if first == n:
-#                 output.append(nums[:])
-        
-#             for i in range(first, n):
-#                 # place i-th integer first 
-#                 # in the current permutation
-#                 nums[first], nums[i] = nums[i], nums[first]
-#                 # use next integers to complete the permutations
-#                 backtrack(first + 1)
-#                 # backtrack
-#                 nums[first], nums[i] = nums[i], nums[first]
-        
-#         n = len(nums)
-#         output = []
-#         backtrack()
-#         return output
-        
-        
