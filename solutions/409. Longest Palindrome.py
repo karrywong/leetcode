6de1c6d8fbe8c472ef9ef84b5acb6b20bc05256d
Karry Wong <ucdwong@ucdavis.edu>
2021-01-17 21:41:42 -0800

    24 ms

--- solutions/409. Longest Palindrome.py
+++ solutions/409. Longest Palindrome.py
@@ -8,3 +8,7 @@ class Solution:
             if res % 2 == 0 and x % 2 == 1:
                 res += 1
         return res
+​
+        ### Soln 2 - two liners by agave
+        # c = collections.Counter(s)
+        # return sum(v - 1 * (v % 2 != 0) for v in c.values()) + 1 * any(v % 2 != 0 for v in c.values())
