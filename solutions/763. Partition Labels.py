b1aeb410e012917fc75bfac2bf30f598fa550250
Karry Wong <ucdwong@ucdavis.edu>
2021-01-18 21:28:59 -0800

    36 ms

--- solutions/763. Partition Labels.py
+++ solutions/763. Partition Labels.py
@@ -1,18 +1,39 @@
 class Solution:
     def partitionLabels(self, S: str) -> List[int]:
-        ### Soln 2 - Leet Code, very clever idea using hashmap to record the last seen letter
-        last = {c: i for i, c in enumerate(S)}
-        # print(last)
-        j = anchor = 0
-        ans = []
-        for i, c in enumerate(S):
-            j = max(j, last[c])
-            if i == j:
-                ans.append(i - anchor + 1)
-                anchor = i + 1
-            # print(i,j,anchor,ans)
+        ### Soln 3 - Leet Code, very clever idea using hashmap to record the last seen letter
+#         last = {c: i for i, c in enumerate(S)}
+#         # print(last)
+#         j = anchor = 0
+#         ans = []
+#         for i, c in enumerate(S):
+#             j = max(j, last[c])
+#             if i == j:
+#                 ans.append(i - anchor + 1)
+#                 anchor = i + 1
+#             # print(i,j,anchor,ans)
     
-        return ans
+#         return ans
+    
+        ### Soln 2 - Jake & Karry, Greedy algorithm, optimized
+        lastseen = {}
+        lengthStack = []
+        
+        for i,s in enumerate(S):
+        
+            if s not in lastseen: # record new element and start new partition
+                lastseen[s] = i
+                lengthStack.append(1)
+            else:
+                temp = 1 # combinded partition length
+                # sum partition lengths from stack until partition contains
+                # both s and the previous occurence of s
+                while temp < i - lastseen[s] + 1:
+                    temp += lengthStack.pop()
+                lastseen[s] = i # update lastseen
+                lengthStack.append(temp) 
+        
+        return lengthStack
+    
     
         ### Soln 1 - Jake & Karry, Greedy algorithm 
         ### idea: use a set (seen) to keep track of letters and stack the partition
@@ -21,10 +42,3 @@ class Solution:
         
 #         for s in S:
 #             if s not in seen:
-#                 seen.add(s)
-#                 stack.append([s,1])
-#             else:
-#                 tempseen = []
-#                 tempcount = 1
-#                 lastPartition = stack.pop()
-​
