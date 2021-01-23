692376eddae5752cecb24e25619a835b6642e684
Karry Wong <ucdwong@ucdavis.edu>
2021-01-22 23:36:46 -0800

    560 ms

--- solutions/111. Minimum Depth of Binary Tree.py
+++ solutions/111. Minimum Depth of Binary Tree.py
@@ -21,10 +21,3 @@ class Solution:
         
         return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
         
-#         if not root:
-#             return 0
-​
-#         if not root.left and not root.right:
-#             return 1
-​
-#         left = self.minDepth(root.left)
