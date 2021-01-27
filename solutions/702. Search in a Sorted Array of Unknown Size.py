92033afde1aafcdef58b029a208a9915b3c2dba9
Karry Wong <ucdwong@ucdavis.edu>
2021-01-26 22:40:41 -0800

    36 ms

---
+++ solutions/702. Search in a Sorted Array of Unknown Size.py
@@ -0,0 +1,40 @@
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+​
+class Solution:
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        ### Soln 0 - find right boundary
+        r = 10000
+        l = 0
+        while (l <= r):
+            mid = (l+r) // 2
+            val = reader.get(mid)
+            if val == target:
+                return mid
+            elif val < target:
+                l = mid + 1
+            else: 
+                r = mid - 1
+        
+        return -1
+​
+#         ### Soln 1 - LeetCode solution
+#         if reader.get(0) == target:
+#             return 0
+        
+#         # search boundaries
+#         left, right = 0, 1
+#         while reader.get(right) < target:
+#             left = right
+#             right <<= 1
+        
+#         # binary search
