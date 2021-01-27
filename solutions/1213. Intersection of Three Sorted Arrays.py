aa3dec814e4e33bd51ba054c9cdd4cf44b93f901
Karry Wong <ucdwong@ucdavis.edu>
2021-01-27 14:23:10 -0800

    80 ms

--- solutions/1213. Intersection of Three Sorted Arrays.py
+++ solutions/1213. Intersection of Three Sorted Arrays.py
@@ -1,8 +1,14 @@
 class Solution:
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
-        ### Soln 0 - set intersection, time complexity O(N1 + N2 + N3)
-        set1 = set(arr1)
-        set2 = set(arr2)
-        set3 = set(arr3)
-        return sorted(list(set1 & set2 & set3))
+        ### Soln 0 - set intersection, time complexity O(N1 + N2 + N3), space complexity O(N1 + N2 + N3)
+        # set1 = set(arr1)
+        # set2 = set(arr2)
+        # set3 = set(arr3)
+        # return sorted(list(set1 & set2 & set3))
+    
+        ### Soln 1 - hashmap, same time complexity O(N1 + N2 + N3), space complexity O(N1 + N2 + N3)
+        counts = collections.Counter(arr1 + arr2 + arr3) 
+        return [c for c in counts if counts[c] == 3]
+    
+        
         
