f64ac4b6f619faf10a0a4e31870ec5ff3a106ab7
Karry Wong <ucdwong@ucdavis.edu>
2021-01-27 21:15:45 -0800

    28 ms

--- solutions/62. Unique Paths.py
+++ solutions/62. Unique Paths.py
@@ -3,5 +3,13 @@ class Solution:
         ### Soln 0 - combinatorics
         return math.comb(m + n - 2, m - 1)
     
-        ### Soln 1 - 
+        ### Soln 1 - Dynamic programming
+        if m == 1 or n == 1: return 1
+        # initialize
+        res = [[1 for _ in range(n)] for _ in range(m)]
         
+        for i in range(1,m):
+            for j in range(1,n):
+                res[i][j] = res[i-1][j] + res[i][j-1]
+        return res[-1][-1]
+​
