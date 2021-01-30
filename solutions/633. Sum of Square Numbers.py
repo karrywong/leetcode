f6fc82b7c1fdbc780a665e4976cdc8a82236634c
Karry Wong <ucdwong@ucdavis.edu>
2021-01-30 11:56:56 -0800

    140 ms

--- solutions/633. Sum of Square Numbers.py
+++ solutions/633. Sum of Square Numbers.py
@@ -1,14 +1,32 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
         ### Soln - native attempt, look at sqrt(c - i*i) for i = 0,1,2,...
-        n = floor(sqrt( 2**31 - 1))
+        def bsearch(a, b, target):
+            if a > b:
+                return False
+            mid = a + (b-a) // 2
+            if mid * mid == target:
+                return True
+            if mid * mid > target:
+                return bsearch(a, mid - 1, target)
+            return bsearch(mid + 1, b, target)        
+        
+        n = floor(sqrt( c)) + 1
         for i in range(n):
             i2 = i*i
             if i2 > c:
                 break
             else:
-                m = sqrt(c - i*i)
-                if m == floor(m):
+                ### use built-in square function, much faster
+                m = sqrt(c - i2) 
+                if m == floor(m): 
                     return True
+                
+                ### use binary search
+                # m = c - i2
+                # if bsearch(0, m, m):
+                #     return True
         return False
+        
+​
         
