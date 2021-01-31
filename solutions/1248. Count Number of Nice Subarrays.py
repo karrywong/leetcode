a44d2923c5fb315847ca8609436389e28a19cc43
Karry Wong <ucdwong@ucdavis.edu>
2021-01-31 15:00:27 -0800

    976 ms

--- solutions/1248. Count Number of Nice Subarrays.py
+++ solutions/1248. Count Number of Nice Subarrays.py
@@ -1,9 +1,20 @@
 class Solution:
-    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:        
+        # Soln 0 - my attempt
         n = len(nums)
         odd = [-1] +  [i for i in range(n) if nums[i] % 2 == 1] + [n]
         if len(odd[1:-1]) < k: return 0
         return sum([(odd[i] - odd[i-1]) * (odd[i+k] - odd[i+k-1]) for i in range(1,len(odd) - k)])
+​
+        # Soln 1 - DP by Jake Reschke
+        # nums[0] %= 2
+        # for i in range(1,len(nums)):
+        #     nums[i] = nums[i] % 2 + nums[i-1]
+        # print(nums)
+        # count = collections.Counter(nums)
+        # print(count, count[k], [count[x-k] for x in nums])
+        # total = count[k] + sum([count[x-k] for x in nums])
+        # return total
     
 #ex1 odd = [0,1,3,4], k = 3
 # i=0, j=2 -> res += (0 + 1) * (4 - 3) = 1
@@ -14,11 +25,4 @@ class Solution:
 ​
 #ex3, odd = [3,6], k = 2
 # return (3+1)*(10 - 6) = 16
-​
-#Input: nums = [2,2,2,1,2,2,1,2,2,2,1,2,2], k = 2
-#ex4 odd = [3,6,10], k = 2
-# i=0, j=1, res += (3+1) * (10 - 6) = 16
-# i=1, j=2, res += (6-3) * (13 - 10) = 9
-# return 25
-​
 ​
