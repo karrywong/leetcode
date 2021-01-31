a27d72f6fe921455bc8de415ca2228f0b8665bc8
Karry Wong <ucdwong@ucdavis.edu>
2021-01-31 11:33:15 -0800

    844 ms

--- solutions/1248. Count Number of Nice Subarrays.py
+++ solutions/1248. Count Number of Nice Subarrays.py
@@ -1,28 +1,9 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        odd = [i for i in range(n) if nums[i] % 2 == 1]
-        if len(odd) < k: return 0
-        if len(odd) == k: return (odd[0] + 1) * (n - odd[-1])
-        
-        # len(odd) > k 
-        i, j = 0, k-1
-        res = 0
-        
-        while j < len(odd):
-            if i == 0:
-                res += (odd[i] + 1) * (odd[j+1] - odd[j])
-                
-            elif j == len(odd) - 1:
-                res +=  (odd[i] - odd[i-1]) * (n - odd[j])
-                
-            else:
-                res += (odd[i] - odd[i-1]) * (odd[j+1] - odd[j])
-​
-            i += 1
-            j += 1
-            
-        return res
+        odd = [-1] +  [i for i in range(n) if nums[i] % 2 == 1] + [n]
+        if len(odd[1:-1]) < k: return 0
+        return sum([(odd[i] - odd[i-1]) * (odd[i+k] - odd[i+k-1]) for i in range(1,len(odd) - k)])
     
 #ex1 odd = [0,1,3,4], k = 3
 # i=0, j=2 -> res += (0 + 1) * (4 - 3) = 1
@@ -37,7 +18,7 @@ class Solution:
 #Input: nums = [2,2,2,1,2,2,1,2,2,2,1,2,2], k = 2
 #ex4 odd = [3,6,10], k = 2
 # i=0, j=1, res += (3+1) * (10 - 6) = 16
-# i=1, j=2, res += (6-3) * (13 - 10) = 6
-# return 22
+# i=1, j=2, res += (6-3) * (13 - 10) = 9
+# return 25
 ​
 ​
