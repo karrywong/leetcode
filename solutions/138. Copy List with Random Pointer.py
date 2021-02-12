825256a363ea73425b64e0779fc42987a8e065b1
Karry Wong <ucdwong@ucdavis.edu>
2021-02-11 23:29:49 -0800

    40 ms

--- solutions/138. Copy List with Random Pointer.py
+++ solutions/138. Copy List with Random Pointer.py
@@ -13,17 +13,18 @@ class Solution:
         ### Soln 0 - first attempt, idea from Jake Reschke
         if not head:
             return None
-        inhead = inp = head
-        copyhead = inc = Node(inp.val)
-        lib = {inp : copyhead, None : None }
-        while inp.next:
+        inhead = inp = head             # input head, input pointer
+        copyhead = cp = Node(inp.val)   # copy head, copy pointer
+        lib = {inp : copyhead, None : None } # initalize mapping
+        while inp.next:                 # first make copy of linked list, ignore random
             inp = inp.next
-            inc.next = Node(inp.val)
-            inc = inc.next
-            lib[inp] = inc
-        inc = copyhead
-        while head:
-            inc.random = lib[head.random]
+            cp.next = Node(inp.val)
+            cp = cp.next
+            lib[inp] = cp       # build mapping between nodes
+        cp = copyhead          # reset copy pointer to head for second loop through
+        while head:             # can change head now, won't need reference to input head
+            cp.random = lib[head.random]    # set cp.random using the map
             head = head.next
-            inc = inc.next
+            cp = cp.next
         return copyhead
+​
