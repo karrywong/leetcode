56965099ed1ef01666437001b04ab9a358e6077d
Karry Wong <ucdwong@ucdavis.edu>
2021-10-09 12:06:48 -0700

    392 ms

--- solutions/448. Find All Numbers Disappeared in an Array.py
+++ solutions/448. Find All Numbers Disappeared in an Array.py
@@ -11,13 +11,26 @@ class Solution:
         nums.sort() 
         count = len(nums)
         ans = []
-        
+​
         while nums:
             ele = nums.pop()
             if ele < count:
                 ans += [i for i in range(ele+1, count+1)]    
-            count = ele - 1
-            
+            count = ele - 1    
         if count != 0:
             ans += list(range(1, count+1))
         return ans
+        
+        #solution 4 - leetcode solution, time: O(N), memory: O(1) 
+        #for i in range(0,len(nums)):
+        #    if nums[abs(nums[i])-1] > 0:
+        #        nums[abs(nums[i])-1] *= -1
+        #ans = []
+        #for i in range(0,len(nums)):
+        #    if nums[i] > 0:
+        #        ans.append(i+1)
+        #return ans 
+    
+        #solution 5 - leetcode solution, time: O(N), memory: O(N) 
+        #ht = {}
+        #for num in nums:
