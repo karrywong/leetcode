ea5cc09d1e81c5bcc7eab1750c2dd01be8483a29
Karry Wong <ucdwong@ucdavis.edu>
2021-10-11 23:26:19 -0700

    1152 ms

--- solutions/752. Open the Lock.py
+++ solutions/752. Open the Lock.py
@@ -1,6 +1,26 @@
 from collections import deque
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
+        #soln 2 - Leetcode's solution
+        def neighbors(node):
+            for i in range(0,4):
+                x = int(node[i])
+                for d in (-1, 1):
+                    y = (x + d) % 10
+                    yield node[:i] + str(y) + node[i+1:]
+        dead = set(deadends)
+        queue = collections.deque([('0000', 0)])
+        seen = {'0000'}
+        while queue:
+            node, depth = queue.popleft()
+            if node == target: return depth
+            if node in dead: continue
+            for nei in neighbors(node):
+                if nei not in seen:
+                    seen.add(nei)
+                    queue.append((nei, depth+1))
+        return -1
+        
         #soln 1 - Jake's BFS
         def getNeighbors(s, deadends):
             neighbors = []
@@ -23,17 +43,3 @@ class Solution:
         while ptQ:
             curPt = ptQ.popleft()
             # Any additional code
-            if curPt == target:
-                dist = 0
-                while curPt != '0000':
-                    curPt = pre[curPt]
-                    dist += 1
-                return dist
-            neighbors = getNeighbors(curPt, deadends)
-            
-            for node in neighbors:
-                if node not in visited:
-                    ptQ.append(node)
-                    visited[node] = None
-                    pre[node] = curPt
-        return -1
