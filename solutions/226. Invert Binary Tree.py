df840b9654049a04c47db0c27b0ee927276fe1b1
Karry Wong <ucdwong@ucdavis.edu>
2021-10-12 22:24:30 -0700

    12 ms

--- solutions/226. Invert Binary Tree.py
+++ solutions/226. Invert Binary Tree.py
@@ -6,13 +6,27 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        #soln 2 - BFS
+        if not root: 
+            return root
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            cur_node = queue.popleft()
+            cur_node.left, cur_node.right = cur_node.right, cur_node.left
+            if cur_node.left:
+                queue.append(cur_node.left)
+            if cur_node.right:
+                queue.append(cur_node.right)
+        return root
+    
         #soln 1 - recursion
         #base case 
-        if not root:
-            return root
-        else: # leave the root alone
+        #if not root:
+        #    return root
+        #else: # leave the root alone
             # then apply invertTree to left and right subtree
             # swap left tree and right tree
             #print(root.right, root.left)
-            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
-            return root
+        #    root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        #    return root
