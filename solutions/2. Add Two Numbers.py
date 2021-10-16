679bdc3e38e5b96cef92c2f09f70ea18eb1d2fc3
Karry Wong <ucdwong@ucdavis.edu>
2021-10-16 16:04:23 -0700

    68 ms

--- solutions/2. Add Two Numbers.py
+++ solutions/2. Add Two Numbers.py
@@ -5,24 +5,32 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        n = 0
-        i = 0
+        #Leetcode soln 1
+        dummyhead = ListNode(0)
+        curr = dummyhead
+        carry = 0
         while l1 or l2:
-            if l1: 
-                n += l1.val * 10**i 
-                l1 = l1.next
-            if l2:
-                n += l2.val * 10**i
-                l2 = l2.next
-            i += 1
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sval = carry + x + y
+            carry, r = divmod(sval, 10)
+            curr.next = ListNode(r)
+            curr = curr.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+                
+        if carry > 0: curr.next = ListNode(carry)
+        return dummyhead.next
+            
+        #my solution
+        #n = 0
+        #i = 0
+        #while l1 or l2:
+        #    if l1: 
+        #        n += l1.val * 10**i 
+        #        l1 = l1.next
+        #    if l2:
+        #        n += l2.val * 10**i
+        #        l2 = l2.next
+        #    i += 1
         
-        n, r = divmod(n, 10)
-        ans = ListNode(r)
-        ans_tail = ans
-        
-        while n > 0:
-            n, r = divmod(n, 10)
-            ans_tail.next = ListNode(r)
-            ans_tail = ans_tail.next
-    
-        return ans
