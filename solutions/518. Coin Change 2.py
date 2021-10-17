176b8e6023ea0b61360c20b574fad0e2f08c99ef
Karry Wong <ucdwong@ucdavis.edu>
2021-10-17 10:53:37 -0700

    136 ms

---
+++ solutions/518. Coin Change 2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # soln 2 - optimized
+        tb = [1] + [None] * amount
+        for i in range(len(coins)):
+            c = coins[i]
+            if i == 0:
+                for val in range(1, amount+1):
+                    tb[val] = 1 if val % c == 0 else 0
+            else:
+                if c <= amount:                 
+                    tb[c] += 1
+                    for val in range(c+1, amount+1):
+                        tb[val] += tb[val-c]
+        return tb[-1]   
+    
+        # soln 1 - first attempt
+        #tb = [[1] + [None] * amount] * len(coins)
+        #for i in range(len(coins)):
+        #    c = coins[i]
+        #    if i == 0:
+        #        for val in range(1, amount+1):
+        #            tb[0][val] = 1 if val % c == 0 else 0
+        #    else:
+        #        for val in range(1, amount+1):
+        #            if val < c: 
+        #                tb[i][val] = tb[i-1][val]
+        #            elif val == c:
+        #                tb[i][val] = tb[i-1][val] + 1
+        #            else:
+        #                tb[i][val] = tb[i-1][val] + tb[i][val-c]
+        #return tb[-1][-1]   
