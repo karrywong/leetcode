57498ffd352454012bf2a46c10dc33a7200c13c6
Karry Wong <ucdwong@ucdavis.edu>
2021-10-19 09:05:09 -0700

    54 ms

--- solutions/20. Valid Parentheses.py
+++ solutions/20. Valid Parentheses.py
@@ -22,12 +22,19 @@ class Solution:
             if p in lib:
                 stack.append(p)
             else:
-                if not stack:
-                    return False
-                if p != lib[stack.pop()]:
-                    return False
+                if not stack: return False
+                if p != lib[stack.pop()]: return False
         return stack == []
     
         ### Soln 0
         # temp = []
         # i = 0 
+        # if len(s) % 2: return False
+        # else:
+        #     while i < len(s):
+        #         if s[i] in ["(", "{", "["]:
+        #             temp.append(s[i])
+        #         elif s[i] in [")", "}", "]"] and temp == []:
+        #             return False
+        #         else:
+        #             if s[i] == ")" and temp[-1] != "(": return False
