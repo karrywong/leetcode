fbbea50e13a5b5f2a2cca9c9b47473acd56987aa
Karry Wong <ucdwong@ucdavis.edu>
2021-10-19 12:07:06 -0700

    32 ms

--- solutions/394. Decode String.py
+++ solutions/394. Decode String.py
@@ -1,21 +1,34 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        #Modified Leetcode soln 1 w stack of single character or digit
-        stack = [] 
-        for i in range(len(s)):
-            if s[i] == "]":
-                decoded = ""
-                #get encoded string
-                while stack[-1] != "[":
-                    decoded += stack.pop();
-                stack.pop() #pop [ from stack
-                base, k = 1, 0
-                while stack and stack[-1].isdigit():
-                    k += int(stack.pop()) * base #get number k
-                    base *= 10 
-                #decode k[decodedString], by pushing decodedString k times into stack
-                if k: stack.append(k*decoded)
-                #print(i, stack, decoded)
+        #Leetcode soln 2 w two stacks, one for count and one for string
+        ctStack, strStack = [], [] #stacks
+        curstr, k = "", 0
+​
+        for e in s:
+            if e.isdigit(): 
+                k = k * 10 + int(e)
+            elif e == '[':
+                ctStack.append(k) #push the number k to countStack
+                strStack.append(curstr) #push the currentString to stringStack
+                curstr, k = "", 0
+            elif e == ']':
+                decoded = strStack.pop()
+                # decode currentK[currentString] by appending currentString k times
+                decoded += curstr * ctStack.pop()
+                curstr = decoded
             else:
-                stack.append(s[i])
-        return ''.join([i[::-1] for i in stack])
+                curstr += e
+            #print(e, ctStack, strStack, curstr, k)
+        return curstr
+        
+        
+        #Modified Leetcode soln 1 w stack of single character or digit
+        # stack = [] 
+        # for i in range(len(s)):
+        #     e = s[i]
+        #     if e == "]":
+        #         decoded = ""
+        #         #get encoded string
+        #         while stack[-1] != "[":
+        #             decoded += stack.pop();
+        #         stack.pop() #pop [ from stack
