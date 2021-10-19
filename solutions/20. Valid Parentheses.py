85686bd8f4a523e945578b8602fe84bc6ae66140
Karry Wong <ucdwong@ucdavis.edu>
2021-10-19 08:58:07 -0700

    47 ms

--- solutions/20. Valid Parentheses.py
+++ solutions/20. Valid Parentheses.py
@@ -1,35 +1,33 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        ### Soln 1
+        # ### Soln 1 - stack
+        # if len(s) % 2: return False
+        # else:      
+        #     stack = []
+        #     mapping = {")": "(", "}": "{", "]": "["}
+        #     for char in s: 
+        #         if char in mapping: 
+        #             top_element = stack.pop() if stack else '#'
+        #             if mapping[char] != top_element:
+        #                 return False
+        #         else:
+        #             stack.append(char)
+        # return not stack
+        
+        #soln 1 - first attempt, be careful of stack being empty
+        if len(s) % 2: return False
+        stack = []
+        lib = {'(': ')', '{':'}', '[':']'}
+        for p in s:
+            if p in lib:
+                stack.append(p)
+            else:
+                if not stack:
+                    return False
+                if p != lib[stack.pop()]:
+                    return False
+        return stack == []
+    
+        ### Soln 0
         # temp = []
         # i = 0 
-        # if len(s) % 2: return False
-        # else:
-        #     while i < len(s):
-        #         if s[i] in ["(", "{", "["]:
-        #             temp.append(s[i])
-        #         elif s[i] in [")", "}", "]"] and temp == []:
-        #             return False
-        #         else:
-        #             if s[i] == ")" and temp[-1] != "(": return False
-        #             if s[i] == "}" and temp[-1] != "{": return False
-        #             if s[i] == "]" and temp[-1] != "[": return False
-        #             temp.pop(-1)
-        #         i += 1
-        #     return False if temp else True
-        
-        
-        ### Soln 2 - stack
-        if len(s) % 2: return False
-        else:      
-            stack = []
-            mapping = {")": "(", "}": "{", "]": "["}
-            for char in s: 
-                if char in mapping: 
-                    top_element = stack.pop() if stack else '#'
-                    if mapping[char] != top_element:
-                        return False
-                else:
-                    stack.append(char)
-        return not stack
-​
