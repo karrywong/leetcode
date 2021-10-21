d15b1664ae642154e3a2f7dcde380a31b774b5e6
Karry Wong <ucdwong@ucdavis.edu>
2021-10-20 23:22:35 -0700

    285 ms

--- solutions/283. Move Zeroes.py
+++ solutions/283. Move Zeroes.py
@@ -3,6 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        ### Soln 4 from LeetCode - Optimal
+        pointer = 0
+        for cur in range(0, len(nums)):
+            if nums[cur] != 0:
+                #swap
+                nums[cur], nums[pointer] = nums[pointer], nums[cur]
+                pointer += 1
+                
         ### Soln 1 - me and Jake Reschke
 #         count1 = 0 #original list
 #         count2 = 0 #position
@@ -26,29 +34,3 @@ class Solution:
 #         for n in nums:
 #             if n != 0:
 #                 lst.append(n)
-        
-#         lst = lst + [0] * i      
-#         for i in range(0, len(nums)):
-#             nums[i] = lst[i]
-​
-        ###Soln 3 from LeetCode - two pointers (i, pointer)
-#         pointer = 0
-#         for i, n in enumerate(nums):
-#             if n != 0:
-#                 nums[pointer] = nums[i]
-#                 pointer += 1
-                
-#         for i in range(pointer, len(nums)):
-#             nums[i] = 0
-        
-        ### Soln 4 from LeetCode - Optimal
-        pointer = 0
-        
-        for cur in range(0, len(nums)):
-            if nums[cur] != 0:
-                #swap
-                temp = nums[cur]
-                nums[cur] = nums[pointer]
-                nums[pointer] = temp
-                
-                pointer += 1
