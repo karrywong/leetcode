98e3d2d9aee09b30cd8563d49b75aed9f1b7246c
Karry Wong <ucdwong@ucdavis.edu>
2021-10-21 20:48:59 -0700

    55 ms

--- solutions/22. Generate Parentheses.py
+++ solutions/22. Generate Parentheses.py
@@ -5,6 +5,7 @@ class Solution:
         def backtrack(r, l, A=[]):
             if len(A) == 2*n:
                 ans.append("".join(A))
+                return
             
             if l < n:
                 A.append("(")
@@ -31,11 +32,3 @@ class Solution:
         # ### Soln 0 - first attempt
         # lib = {1:set(["()"])}
         # for i in range(2,n+1):
-        #     temp = set()
-        #     for j in range(1, i):
-        #         for (k,h) in itertools.product(lib[j],lib[i-j]):
-        #             temp.add(k+h)   
-        #     for x in lib[i-1]:
-        #         temp.add("(" + x + ")")
-        #     lib[i] = temp
-        # return lib[n]
