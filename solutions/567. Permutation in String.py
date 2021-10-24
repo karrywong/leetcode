1d68cb6af747a242cd8807215237bafdcb152846
Karry Wong <ucdwong@ucdavis.edu>
2021-10-24 12:00:37 -0700

    120 ms

--- solutions/567. Permutation in String.py
+++ solutions/567. Permutation in String.py
@@ -1,6 +1,6 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        ### Soln 1 - Sliding window + Hashmap
+        ### Soln 1 - Leetcode Sliding window + Hashmap
         n1, n2 = len(s1), len(s2)
         if n1 > n2 : return False
         
@@ -17,16 +17,5 @@ class Solution:
             lib2[s2[i]] -= 1
             if lib2[s2[i]] == 0:
                 del lib2[s2[i]]
-​
+                
         return lib1 == lib2
-        
-        
-#         ### Soln 0 - naive attempt
-#         if len(s1) > len(s2): return False
-#         if len(s1) == len(s2): 
-#             x1 = sorted(tuple(s1))
-#             x2 = sorted(tuple(s2))
-#             if x1 == x2: 
-#                 return True
-#             else:
-#                 return False
