d1bd5225cec5c04246b7326a86266871eeb57c35
Karry Wong <ucdwong@ucdavis.edu>
2021-10-24 17:22:32 -0700

    68 ms

--- solutions/116. Populating Next Right Pointers in Each Node.py
+++ solutions/116. Populating Next Right Pointers in Each Node.py
@@ -10,25 +10,35 @@ class Node:
 ​
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
+        #soln 1 - Leetcode BFS
         if not root: return root
-        queue = collections.deque()
-        queue.append(root) #1
-        n, count = 0, 1
-        
+        queue = collections.deque([root])
         while queue:
-            if (count == 2**n):
-                prev, cur = None, queue.popleft()
-                n += 1
-            else:
-                prev, cur = cur, queue.popleft()
-            count += 1
-                
-            cur.next = prev
-            if cur.right:
-                queue.append(cur.right)
-                queue.append(cur.left)
-                
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if i < size - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                    queue.append(node.right)
         return root
-            
         
-​
+        #soln 0 - first attempt with Jake Reschke
+#         if not root: return root
+#         queue = collections.deque([root])
+#         n, count = 0, 1
+        
+#         while queue:
+#             if (count == 2**n):
+#                 prev, cur = None, queue.popleft()
+#                 n += 1
+#             else:
+#                 prev, cur = cur, queue.popleft()
+#             count += 1
+                
+#             cur.next = prev
+#             if cur.right:
+#                 queue.append(cur.right)
+#                 queue.append(cur.left)
+                
