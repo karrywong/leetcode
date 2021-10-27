3ec006de77c2acfcb55eddfa6f3f325f29b98701
Karry Wong <ucdwong@ucdavis.edu>
2021-10-26 23:50:28 -0700

    28 ms

--- solutions/24. Swap Nodes in Pairs.py
+++ solutions/24. Swap Nodes in Pairs.py
@@ -5,14 +5,29 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #first attempt, Time O(N), Space O(N)
-        if not head: return head #empty 
-        if not head.next: return head #one node
+        #second attempt, iterative, Time O(N), Space O(1)
+        head_ptr1 = ListNode(-1)
+        head_ptr1.next = head
+        prev_node = head_ptr1
         
-        head_ptr1 = head
-        head_ptr2 = head.next
+        while head and head.next:
+            first = head
+            second = head.next
+            
+            prev_node.next = second #-1 -> 2->
+            first.next = second.next # 1-> 3
+            second.next = first # 2 -> 1
+            
+            prev_node = first 
+            head = first.next
+            
+        return head_ptr1.next
+    
+#         #first attempt w/ Jake, recursion, Time O(N), Space O(N)
+#         if not head: return head #empty 
+#         if not head.next: return head #one node
         
-        head_ptr1.next = self.swapPairs(head.next.next) #1->3
-        head_ptr2.next = head_ptr1 #2->1
-​
-        return head_ptr2
+#         head_ptr1 = head
+#         head_ptr2 = head.next
+        
+#         head_ptr1.next = self.swapPairs(head.next.next) #1->3
