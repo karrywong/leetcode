1c2a6fe4f73c5ed5939a9d635872c17fe16a2f97
Karry Wong <ucdwong@ucdavis.edu>
2021-10-26 23:53:47 -0700

    32 ms

---
+++ solutions/25. Reverse Nodes in k-Group.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # first attempt by Jake
+        if k == 1: return head
+        scan = head
+        new_head = None
+        old_tail = None
+        stack = []
+        while scan:
+            if len(stack) < k:
+                stack.append(scan)
+            else:
+                last = stack.pop()
+                last.next = stack[-1]
+                if not new_head:
+                    new_head = last
+                else:
+                    old_tail.next = last
+                for _ in range(k - 2):
+                    last = stack.pop()
+                    last.next = stack[-1]
+                last = stack.pop()
+                last.next = scan
+                old_tail = last
+                stack.append(scan)
+​
+            scan = scan.next
+        if len(stack) == k:
+            last = stack.pop()
+            last.next = stack[-1]
+            if not new_head:
+                new_head = last
+            else:
+                old_tail.next = last
+            for _ in range(k - 2):
+                last = stack.pop()
+                last.next = stack[-1]
+            last = stack.pop()
+            last.next = scan
+            old_tail = last
+​
