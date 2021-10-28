50560a1bfc892ff5f36e62a89d11e9b3797b5c39
Karry Wong <ucdwong@ucdavis.edu>
2021-10-27 18:42:54 -0700

    24 ms

--- solutions/118. Pascal's Triangle.py
+++ solutions/118. Pascal's Triangle.py
@@ -1,17 +1,27 @@
 class Solution:
-    def generate(self, numRows: int, library = {0:[],1:[1],2:[1,1]}) -> List[List[int]]:
-        pTriangle=[]
-        if numRows in library:
-            for i in range(0,numRows):
-                pTriangle.append(library[i+1])
-            return pTriangle
-        else:
-            temp = [1]
-            previous = self.generate(numRows-1)[-1]
-            for j in range(1,numRows-1):
-                temp.append(previous[j-1]+previous[j])
-            temp.append(1)
-            library[numRows] = temp
-​
-            return self.generate(numRows)
+    def generate(self, numRows: int) -> List[List[int]]:
+        # soln 0 - first attempt recursion
+        lst = [[1]]
+        def helper(n):
+            if n > 1:
+                lst.append([1]+ [lst[-1][i] + lst[-1][i+1] for i in range(len(lst[-1])-1)] + [1])
+                helper(n-1)
         
+        if numRows > 1:
+            helper(numRows)
+        return lst
+    
+#         # soln 00 - old solution with Jake
+#         pTriangle=[]
+#         if numRows in library:
+#             for i in range(0,numRows):
+#                 pTriangle.append(library[i+1])
+#             return pTriangle
+#         else:
+#             temp = [1]
+#             previous = self.generate(numRows-1)[-1]
+#             for j in range(1,numRows-1):
+#                 temp.append(previous[j-1]+previous[j])
+#             temp.append(1)
+#             library[numRows] = temp
+#             return self.generate(numRows)
