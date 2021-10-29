e4d19358846502564cd4b03da2988a6736ebc4e5
Karry Wong <ucdwong@ucdavis.edu>
2021-10-28 22:49:23 -0700

    44 ms

---
+++ solutions/81. Search in Rotated Sorted Array II.py
@@ -0,0 +1,5 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        #soln 0, fewest step in first attempt (kind of cheating)
+        return target in nums
+        
