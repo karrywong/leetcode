4e6d57698ce7e4f005f3480ce936cf064009b2dd
Karry Wong <ucdwong@ucdavis.edu>
2021-11-02 15:22:57 -0700

    60 ms

---
+++ solutions/150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        oper = {"+": lambda x,y: x + y, "-": lambda x,y: x - y, "*": lambda x,y: x * y, "/": lambda x,y: int(x/y)}
+        
+        for s in tokens:
+            if s not in oper:
+                stack.append(s)
+            else:
+                last = int(stack.pop())
+                sec_last = int(stack.pop())
+                stack.append(oper[s](sec_last, last))
+            
+        return stack[-1]
