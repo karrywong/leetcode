41b473fb3c133ad9b4dc732d42d76993309f31d8
Karry Wong <ucdwong@ucdavis.edu>
2021-11-03 20:35:47 -0700

    44 ms

--- solutions/784. Letter Case Permutation.py
+++ solutions/784. Letter Case Permutation.py
@@ -1,28 +1,31 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        #soln 0 - first attempt, backtracking
-        # alpha = [a for a in s if a.isalpha()]
-        lst = list(s)
-        n = len(s)
+        #smart - Leetcode two liners
+        f = lambda x : (x.lower(), x.upper()) if x.isalpha() else x
+        return [''.join(x) for x in itertools.product(*map(f,s))]   
+    
+#         #soln 0 - first attempt, backtracking
+#         # alpha = [a for a in s if a.isalpha()]
+#         lst = list(s)
+#         n = len(s)
         
-        def backtrack(i=0, A=[]):
-            if len(A) == n:
-                ans.append(''.join(A))
-                return
+#         def backtrack(i=0, A=[]):
+#             if len(A) == n:
+#                 ans.append(''.join(A))
+#                 return
             
-            if lst[i].isalpha():
-                A.append(lst[i].lower())
-                backtrack(i+1)
-                A.pop()
-                A.append(lst[i].upper())
-                backtrack(i+1)
-                A.pop()
-            else:
-                A.append(lst[i])
-                backtrack(i+1)
-                A.pop()
+#             if lst[i].isalpha():
+#                 A.append(lst[i].lower())
+#                 backtrack(i+1)
+#                 A.pop()
+#                 A.append(lst[i].upper())
+#                 backtrack(i+1)
+#                 A.pop()
+#             else:
+#                 A.append(lst[i])
+#                 backtrack(i+1)
+#                 A.pop()
                 
-        ans = []
-        backtrack()
-        return ans
-​
+#         ans = []
+#         backtrack()
+#         return ans
