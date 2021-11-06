ed397d4a1033da3162b2f5b1fe6135a25357b6be
Karry Wong <ucdwong@ucdavis.edu>
2021-11-06 14:19:27 -0700

    32 ms

--- solutions/198. House Robber.py
+++ solutions/198. House Robber.py
@@ -1,14 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 0: return 0
-        elif len(nums) == 1: return nums[0]
-        elif len(nums) == 2: return max(nums)
+        #soln 1 - recursion with memoization
+        n = len(nums)
+        memo = {}
+        def helper(i):
+            if i >= n:
+                return 0
+            if i in memo:
+                return memo[i]
+            val = max(helper(i+1), helper(i+2) + nums[i])
+            memo[i] = val
+            return val
+        return helper(0)
         
-        nums[2] += nums[0]
-        
-        for i in range(3, len(nums)):
-            nums[i] += max(nums[i-2], nums[i-3])
-        
-        return max(nums)
-                
+        # #soln 0 - dynamic programming
+        # n = len(nums)
+        # if n <= 2: return max(nums)
+        # nums[2] += nums[0]
+        # for i in range(3,n):
+        #     nums[i] += max(nums[i-2], nums[i-3])
+        # return max(nums[-2],nums[-1])
         
