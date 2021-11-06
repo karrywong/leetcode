4f1fa2154e8cb88baf23e1690f03684c0a02abf1
Karry Wong <ucdwong@ucdavis.edu>
2021-11-05 18:25:55 -0700

    88 ms

--- solutions/566. Reshape the Matrix.py
+++ solutions/566. Reshape the Matrix.py
@@ -1,13 +1,28 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        #First attempt, Time O(m*n), Space O(m*n)
+        #Second attempt, without divmod, Time O(m*n), Space O(m*n)
         m, n = len(mat), len(mat[0])
         if m * n != r * c:
-            return mat
-        else:
-            lst = [[0 for _ in range(c)] for _ in range(r)]
+            return mat     
+        lst = [[0 for _ in range(c)] for _ in range(r)]
+        r0, c0 = 0, 0
         for i in range(m):
             for j in range(n):
-                x, y = divmod(i*n + j, c)
-                lst[x][y] = mat[i][j]
+                lst[r0][c0] = mat[i][j]
+                c0 += 1
+                if c0 == c:
+                    r0 += 1
+                    c0 = 0
         return lst
+                
+#         #First attempt, Time O(m*n), Space O(m*n)
+#         m, n = len(mat), len(mat[0])
+#         if m * n != r * c:
+#             return mat
+​
+#         lst = [[0 for _ in range(c)] for _ in range(r)]
+#         for i in range(m):
+#             for j in range(n):
+#                 x, y = divmod(i*n + j, c)
+#                 lst[x][y] = mat[i][j]
+#         return lst
