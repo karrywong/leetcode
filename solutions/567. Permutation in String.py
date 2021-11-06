b9ca0e40fd054978d27599492b7188cd65c6f2bf
Karry Wong <ucdwong@ucdavis.edu>
2021-11-05 17:42:36 -0700

    68 ms

--- solutions/567. Permutation in String.py
+++ solutions/567. Permutation in String.py
@@ -4,11 +4,11 @@ class Solution:
         n1, n2 = len(s1), len(s2)
         if n1 > n2 : return False
         
-        lib1 = collections.Counter()
-        lib2 = collections.Counter()
-        for i in range(n1):
-            lib1[s1[i]] += 1
-            lib2[s2[i]] += 1
+        lib1 = collections.Counter(s1[:])
+        lib2 = collections.Counter(s2[:n1])
+        # for i in range(n1):
+        #     lib1[s1[i]] += 1
+        #     lib2[s2[i]] += 1
         
         for i in range(n2 - n1):
             if lib1 == lib2:
