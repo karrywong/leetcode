846e69e683df3ab085980f555fe4a8a58fc13ea9
Karry Wong <ucdwong@ucdavis.edu>
2021-11-06 00:24:13 -0700

    24 ms

--- solutions/342. Power of Four.py
+++ solutions/342. Power of Four.py
@@ -1,9 +1,15 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n < 1: return False
-        if n == 1: 
-            return True
-        else:
-            if n%4: return False
-            else:
-                return self.isPowerOfFour(n/4)
+        # #soln 1 - first attempt, bit manipulation
+        return not n&(n-1) and n&0x55555555 if n > 0 else False
+    
+        # #soln 0 - old attempt, recursion
+        # if n < 1: return False
+        # if n == 1: return True
+        # else:
+        #     if n%4: 
+        #         return False
+        #     else: 
+        #         return self.isPowerOfFour(n/4)
+        
+​
