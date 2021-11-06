4b4c171974622636bfd3d1cef92e0bb07d8dc81d
Karry Wong <ucdwong@ucdavis.edu>
2021-11-06 00:02:54 -0700

    32 ms

--- solutions/231. Power of Two.py
+++ solutions/231. Power of Two.py
@@ -1,21 +1,19 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        ### Soln 1
+        soln 3 - first attempt, bit manipulation
+        return not n&(n-1) if n > 0 else False
+        
+        # #soln 2 - old attempt
         # if n < 0: return False
         # return True if bin(n)[2:].count("1") == 1 else False
 ​
-        ### Soln 2
-        # while n % 2 == 0 and n > 1:
-        #     n /= 2
+        # #soln 1 - old attempt
+        # while n % 2 == 0 and n > 1: n /= 2
         # return True if n == 1 else False
         
-        ###Soln 3
-        if n < 1: 
-            return False
-        if n in [1,2]: 
-            return True
-        else:
-            if n % 2:
-                return False
-            else:
-                return self.isPowerOfTwo( n/2 )
+        # # soln 0 - old attempt
+        # if n < 1: return False
+        # if n == 1 or n == 2: return True
+        # else: # n >= 3
+        #     if n % 2: return False
+        #     else: return self.isPowerOfTwo(n/2)
