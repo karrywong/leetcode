376037bfc2935b091ad33775d191ebcaebfc31f1
Karry Wong <ucdwong@ucdavis.edu>
2021-11-09 22:22:33 -0800

    56 ms

--- solutions/141. Linked List Cycle.py
+++ solutions/141. Linked List Cycle.py
@@ -6,21 +6,26 @@
 ​
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        #soln 0 - first attempt
-        if not head or not head.next: 
-            return False
-        else:
-            lib = {}
-            
-        head_ptr = head
-        prev = head
-        head_ptr = head_ptr.next
-        while head_ptr:
-            if head_ptr in lib: 
-                if lib[head_ptr] == prev:
-                    return True
-            else:
-                lib[head_ptr] = prev
-            head_ptr = head_ptr.next
-        return False
+        #soln 2 - Leetcode Floyd's algorithm
+        if head is None: return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
         
+        # #soln 1 - Leetcode Hashtable 
+        # nodes_seen = set()
+        # while head:
+        #     if head in nodes_seen:
+        #         return True
+        #     nodes_seen.add(head)
+        #     head = head.next
+        # return False
+        
+#         #soln 0 - first attempt
+#         if not head or not head.next: 
+#             return False
