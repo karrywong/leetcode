eaac3120b904601cbb570a26b150d1a72d0f00c5
Karry Wong <ucdwong@ucdavis.edu>
2021-11-11 15:12:45 -0800

    5680 ms

--- solutions/55. Jump Game.py
+++ solutions/55. Jump Game.py
@@ -1,12 +1,24 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        #soln 1 - LeetCode Greedy
+        # #soln 1 - LeetCode Greedy
+        # n = len(nums)
+        # lastPos = n - 1
+        # for i in range(n-2,-1,-1):
+        #     if i + nums[i] >= lastPos:
+        #         lastPos = i
+        # return lastPos == 0
+        
+        #soln 2 - Leetcode DP bottom-up w memoization
         n = len(nums)
-        lastPos = n - 1
+        memo = ['U'] * n 
+        memo[-1] = 'G'
         for i in range(n-2,-1,-1):
-            if i + nums[i] >= lastPos:
-                lastPos = i
-        return lastPos == 0
+            furthestJump = min(i+nums[i], n-1)
+            for j in range(i+1, furthestJump+1):
+                if memo[j] == 'G':
+                    memo[i] = 'G'
+                    break
+        return memo[0] == 'G'
         
         # #soln 0 - first attempt by Jake, Time O(n), Space O(1)
         # jump = False
@@ -21,5 +33,3 @@ class Solution:
         #         j += 1
         #         if nums[i] > j:
         #             jump = False
-        # return not jump
-​
