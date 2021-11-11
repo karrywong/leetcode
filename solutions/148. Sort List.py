43df8fbf4d5960ac210308585059212a6e5d01bd
Karry Wong <ucdwong@ucdavis.edu>
2021-11-11 11:11:47 -0800

    548 ms

--- solutions/148. Sort List.py
+++ solutions/148. Sort List.py
@@ -5,17 +5,51 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #soln 0 - naive attempt, two pass
-        if not head: return head
-        stack = []
-        while head:
-            stack.append(head.val)
-            head = head.next
-        stack.sort(reverse=True)
-        ans_ptr = ListNode(stack.pop())
-        ans = ans_ptr
-        while stack:
-            ans_ptr.next = ListNode(stack.pop())
-            ans_ptr = ans_ptr.next
-        return ans
+        #soln 1 - Leetcode top-down merge sort
+        def getMid(head):
+            slow = fast = head
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            dummyhead = head
+            while slow.next:
+                slow = slow.next
+                dummyhead = dummyhead.next
+            mid = dummyhead.next
+            dummyhead.next = None
+            return mid
         
+        def merge(list1, list2):
+            dummyhead = ListNode();
+            tail = dummyhead
+            while list1 and list2:
+                if (list1.val < list2.val):
+                    tail.next = list1
+                    list1 = list1.next
+                else:
+                    tail.next = list2
+                    list2 = list2.next
+                tail = tail.next
+            tail.next = list1 if list1 else list2
+            return dummyhead.next
+​
+        if not head or not head.next:
+            return head
+        mid = getMid(head)
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        return merge(left, right)
+​
+        # #soln 0 - naive attempt, two pass, Space O(N)
+        # if not head: return head
+        # stack = []
+        # while head:
+        #     stack.append(head.val)
+        #     head = head.next
+        # stack.sort(reverse=True)
+        # ans_ptr = ListNode(stack.pop())
+        # ans = ans_ptr
+        # while stack:
+        #     ans_ptr.next = ListNode(stack.pop())
+        #     ans_ptr = ans_ptr.next
+        # return ans
