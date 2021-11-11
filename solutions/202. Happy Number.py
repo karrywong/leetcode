5c6280748fe92e439dbe3333280185fa9a6ccf95
Karry Wong <ucdwong@ucdavis.edu>
2021-11-11 14:54:36 -0800

    36 ms

---
+++ solutions/202. Happy Number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        #soln 1 - Floyd's cycle finding algorithm, Space O(1)
+        def get_next(number):
+            digits = [int(s) for s in str(number)]
+            return sum([i*i for i in digits])
+        slow = n
+        fast = get_next(n)
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+        return fast == 1
+        
+        # #soln 0 - first attempt, hashtable, Space O(logN)
+        # seen = set([n])
+        # val = n
+        # while val != 1:
+        #     digits = [int(s) for s in str(val)]
+        #     val = sum([i*i for i in digits])
+        #     if val in seen:
+        #         return False
+        #     seen.add(val)
+        # return True
