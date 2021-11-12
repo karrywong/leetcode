0b37b971fb821b14f95d5183406ce4bef3ba0bc1
Karry Wong <ucdwong@ucdavis.edu>
2021-11-11 20:58:49 -0800

    52 ms

--- solutions/110. Balanced Binary Tree.py
+++ solutions/110. Balanced Binary Tree.py
@@ -4,44 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-​
 class Solution:
-    def isBalancedHelper(self, root: TreeNode) -> (bool, int):
-        # An empty tree is balanced and has height -1
-        if not root:
-            return True, -1
-        
-        # Check subtrees to see if they are balanced. 
-        leftIsBalanced, leftHeight = self.isBalancedHelper(root.left)
-        if not leftIsBalanced:
-            return False, 0
-        rightIsBalanced, rightHeight = self.isBalancedHelper(root.right)
-        if not rightIsBalanced:
-            return False, 0
-        
-        # If the subtrees are balanced, check if the current tree is balanced
-        # using their height
-        return (abs(leftHeight - rightHeight) < 2), 1 + max(leftHeight, rightHeight)
-            
-    
-    def isBalanced(self, root: TreeNode) -> bool:
-        ### Soln 1 - Bottom-up recursion, LeetCode solution! Clever
-        return self.isBalancedHelper(root)[0]
-        
-#         ### Soln 0 - first attempt, get the height of left and right subtree at every node
-#         ### Top-down recursion
-#         def height(root):
-#             if not root:
-#                 return 0
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        #soln 1 - recursion bottom-up
+        def helper(node):
+            if not node: 
+                return True, -1
+            leftIsBalanced, leftHeight = helper(node.left)
+            if not leftIsBalanced:
+                return False, 0
+            rightIsBalanced, rightHeight = helper(node.right)
+            if not rightIsBalanced:
+                return False, 0  
+            return (abs(leftHeight - rightHeight) < 2), 1 + max(leftHeight, rightHeight)
+        return helper(root)[0]
 ​
-#             left_height = height(root.left)
-#             right_height = height(root.right)
-#             return max(left_height, right_height) + 1
+        # #soln 0 - first attempt, recursion top-down
+        # def helper(node): #compute tree height
+        #     if not node: return 0
+        #     return max(helper(node.right), helper(node.left)) + 1
+        # if not root: return True
+        # left_height = helper(root.left)
+        # right_height = helper(root.right)
+        # if abs(left_height - right_height) <= 1:
+        #     return self.isBalanced(root.left) and self.isBalanced(root.right)
+        # else:
+        #     return False
+​
         
-#         if not root:
-#             return True
-        
-#         lh = height(root.left)
-#         rh = height(root.right)
-#         # print(root.val, lh, rh)
-#         return abs(lh - rh) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
