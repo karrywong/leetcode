00495546777217acf2af4e7c707688da26c676dd
Karry Wong <ucdwong@ucdavis.edu>
2021-11-12 14:40:04 -0800

    840 ms

--- solutions/15. 3Sum.py
+++ solutions/15. 3Sum.py
@@ -1,17 +1,39 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        dups = set()
+        #soln 2 - Leetcode w no-sorting
+        res, dups = set(), set()
         seen = {}
-        answer = set()
-        
-        for i, n in enumerate(nums):
-            if n not in dups:
-                dups.add(n)
-                for j,m in enumerate(nums[i+1:]):
-                    complement = -n - m
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 - val2
                     if complement in seen and seen[complement] == i:
-                        answer.add(tuple(sorted((n,m,complement))))
-                    seen[m] = i
-​
-        return answer
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
+        return res
         
+#         #soln 1 - Leetcode two pointers, Time O(n^2), Space O(n)
+#         def twoSum(i, ans):
+#             l, r = i+1, len(nums)-1
+#             while l < r:
+#                 val = nums[i] + nums[l] + nums[r]
+#                 if val < 0:
+#                     l += 1
+#                 elif val > 0:
+#                     r -= 1
+#                 else:
+#                     ans.append([nums[i], nums[l], nums[r]])
+#                     l += 1
+#                     r -= 1
+#                     while l < r and nums[l] == nums[l-1]:
+#                         l += 1
+        
+#         ans = []
+#         nums.sort()
+#         for i in range(len(nums)):
+#             if nums[i] > 0:
+#                 break
+#             if i == 0 or nums[i] != nums[i-1]:
+#                 twoSum(i, ans)
+#         return ans
