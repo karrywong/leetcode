dadbb66b4a32f6435e2969c109ce37da973a3fbb
Karry Wong <ucdwong@ucdavis.edu>
2021-11-12 15:26:55 -0800

    756 ms

---
+++ solutions/259. 3Sum Smaller.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        #soln 1 - Leetcode two pointers, fast
+        ans, n = 0, len(nums)
+        nums.sort()
+        for i in range(n-2):
+            target2 = target - nums[i]
+            l, r = i+1, n-1
+            while l < r:
+                if nums[l] + nums[r] < target2:
+                    ans += r-l
+                    l += 1
+                else:
+                    r -= 1
+        return ans
+        
+        # #soln 0 - first attempt, two pointers, slow
+        # ans, n = 0, len(nums)
+        # nums.sort()
+        # for i in range(n):
+        #     target2 = target - nums[i]
+        #     for r in range(n-1, i+1, -1):
+        #         l = i+1
+        #         if nums[l] + nums[r] >= target2:
+        #             continue
+        #         while l < r and nums[l] + nums[r] < target2:
+        #             ans += 1
+        #             l += 1
+        # return ans
+        
