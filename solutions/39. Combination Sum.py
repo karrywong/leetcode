75c8700503186efc85637bfd4d787a1f4ae0d53e
Karry Wong <ucdwong@ucdavis.edu>
2021-11-11 22:06:10 -0800

    84 ms

---
+++ solutions/39. Combination Sum.py
@@ -0,0 +1,38 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        #soln 1 - Leetcode backtracking, optimized
+        ans, n = [], len(candidates)
+        def backtrack(num, start, A=[]):
+            if num == 0:
+                ans.append(A.copy())
+                return
+            if num < 0:
+                return
+​
+            for i in range(start, n):
+                A.append(candidates[i])
+                backtrack(num-candidates[i], i, A)
+                A.pop()
+            
+        backtrack(target, 0)
+        return ans
+        
+        #soln 0 - first attempt, backtracking with hashtable, slow
+#         ans, seen, n = [], set(), len(candidates)
+#         def backtrack(num, A=[], count = [0]*n):
+#             tp = tuple(count)
+#             if num == 0 and tp not in seen:
+#                 ans.append(A.copy())
+#                 seen.add(tp)
+#                 return
+            
+#             for i, c in enumerate(candidates):
+#                 if num - c >= 0:
+#                     A.append(c)
+#                     count[i] += 1
+#                     backtrack(num-c, A, count)
+#                     A.pop()
+#                     count[i] -= 1
+​
+#         backtrack(target)
+#         return ans
