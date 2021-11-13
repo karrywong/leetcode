09b00e5764ff9205d5b06a94eb190e51f6eded59
Karry Wong <ucdwong@ucdavis.edu>
2021-11-13 10:51:33 -0800

    84 ms

--- solutions/56. Merge Intervals.py
+++ solutions/56. Merge Intervals.py
@@ -1,15 +1,12 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        #soln 0 - first attempt
+        #soln 0 - first attempt with sorting, Time O(NlogN), Space O(N)
         intervals.sort()
         ans = [intervals[0]]
         for interval in intervals[1:]:
-            pre_interval = ans[-1]
-            start, end = pre_interval[0], pre_interval[1]
-            if interval[0] <= end:
-                end = max(end, interval[1])
-                start = min(start, interval[0])
-                ans[-1] = [start, end]
+            if interval[0] <= ans[-1][1]:
+                ans[-1][1] = max(ans[-1][1], interval[1])
+                # ans[-1][0] = min(ans[-1][0], interval[0]) #Not necessary because of sorting
             else:
                 ans.append(interval)
         return ans
