d2490f9f60d22dbc38ec4528c4bc65f6ebe6f22f
Karry Wong <ucdwong@ucdavis.edu>
2021-11-12 23:52:17 -0800

    32 ms

--- solutions/101. Symmetric Tree.py
+++ solutions/101. Symmetric Tree.py
@@ -5,30 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-#     ### Soln 1 - recursive
-#     def isMirror(self, t1: TreeNode, t2: TreeNode) -> bool:
-#         if not t1 and not t2: return True
-#         if not t1 or not t2: return False
-#         return (t1.val == t2.val) and self.isMirror(t1.left, t2.right) and self.isMirror(t1.right, t2.left)
-    
-#     def isSymmetric(self, root: TreeNode) -> bool:
-#         return self.isMirror(root, root)
-​
-    ### Soln 2 - iterative using queue (deque)
-    def isSymmetric(self, root: TreeNode) -> bool:
-        q = deque()
-        q.append(root)
-        q.append(root)
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # soln 1 - LeetCode recursion
+        # def isMirror(t1, t2):
+        #     if not t1 and not t2:
+        #         return True
+        #     if not t1 or not t2:
+        #         return False
+        #     return (t1.val == t2.val) and isMirror(t1.left, t2.right) and isMirror(t1.right, t2.left)
+        # return isMirror(root, root)
         
-        while q:
-            t1 = q.popleft()
-            t2 = q.popleft()
-            
-            if not t1 and not t2: continue
-            if not t1 or not t2: return False
+        #soln 2 - LeetCode iterative
+        queue = collections.deque([root, root])
+        while queue:
+            t1 = queue.pop()
+            t2 = queue.pop()
+            if t1 is None and t2 is None: continue
+            if t1 is None or t2 is None: return False
             if t1.val != t2.val: return False
-            q.append(t1.left)
-            q.append(t2.right)
-            
-            q.append(t1.right)
-            q.append(t2.left)
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)
+            queue.append(t2.left)
+        return True
