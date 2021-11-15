7242e4d7e66fa29817ac26399220350f62322f20
Karry Wong <ucdwong@ucdavis.edu>
2021-11-14 21:30:16 -0800

    36 ms

--- solutions/69. Sqrt(x).py
+++ solutions/69. Sqrt(x).py
@@ -1,25 +1,25 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        # #Recent attempt - binary search
+        # if x < 2: return x
+        # l, r = 1, x
+        # while l < r:
+        #     mid = l + (r-l) // 2
+        #     temp = mid*mid 
+        #     if temp <= x < (mid+1)*(mid+1):
+        #         return mid
+        #     if temp > x:
+        #         r = mid-1
+        #     else:
+        #         l = mid+1
+        # return l
+    
         ### Newton's method, fastest
         if x < 2:
             return x
-        
         x0 = x
         x1 = (x0 + x / x0) / 2
         while abs(x0 - x1) >= 1:
             x0 = x1
             x1 = (x0 + x / x0) / 2        
-            
         return int(x1)
-        
-        ### built-in function
-        # return floor(sqrt(x))
-        
-        ### Native Approach
-        # if x <= 1: return x
-        # if x == 2: return 1
-        # for i in range(x):
-        #     if i*i > x:
-        #         return (i - 1)            
-            
-        
