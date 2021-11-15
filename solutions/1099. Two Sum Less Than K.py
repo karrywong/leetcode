5f433ed2d8d0891f0c513b83db288005abd0db0a
Karry Wong <ucdwong@ucdavis.edu>
2021-11-15 15:19:39 -0800

    44 ms

--- solutions/1099. Two Sum Less Than K.py
+++ solutions/1099. Two Sum Less Than K.py
@@ -1,33 +1,14 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
-        # Soln 1 - Two pointers + sort, O(nlogn)
-#         nums.sort()
-#         right = len(nums) - 1
-#         left = 0
-#         res = -1
-        
-#         while left < right:
-#             temp = nums[right] + nums[left]
-#             if temp >= k:
-#                 right -= 1
-#             else:
-#                 res = max(res, temp)
-#                 left += 1
-        
-#         return res
-​
-        # Soln 2 - Two pointers, without sort O(n + m)
-        answer = -1
-        count = [0] * 1001
-        for num in nums:
-            count[num] += 1
-        lo = 1
-        hi = 1000
-        while lo <= hi:
-            if lo + hi >= k or count[hi] == 0:
-                hi -= 1
-            else:
-                if count[lo] > (0 if lo < hi else 1):
-                    answer = max(answer, lo + hi)
-                lo += 1
-        return answer
+        #Sort + two pointers, time O(NlogN), Space O(1)
+        ans, diff, n = -1, float('inf'), len(nums)
+        nums.sort()
+        l, r = 0, n-1
+        while l < r:
+            temp = nums[l] + nums[r]
+            if temp >= k:
+                r -= 1
+            else: #temp < k
+                l += 1
+                ans = max(ans, temp)
+        return ans
