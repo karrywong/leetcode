ccf6e0f050d425dbdebed4ff1859525a67f4a9d1
Karry Wong <ucdwong@ucdavis.edu>
2021-11-15 14:59:31 -0800

    104 ms

---
+++ solutions/16. 3Sum Closest.py
@@ -0,0 +1,19 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        #After multiple failures, see Leetcode Two pointer, Time O(n^2), Space O(N)
+        #Using selection sort instead of built-sort, Space O(1) possible
+        diff, n = float('inf'), len(nums)
+        nums.sort() #-4,-1,2,1
+        for i in range(n-2):
+            l, r = i+1, n-1
+            while l < r:
+                temp = nums[i] + nums[l] + nums[r]
+                if abs(target - temp) < abs(diff):
+                    diff = target - temp
+                    if diff == 0: return target
+                if temp < target:
+                    l += 1
+                else:
+                    r -= 1
+            if diff == 0: break
+        return target - diff
