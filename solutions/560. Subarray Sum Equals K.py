471ae7b430a79faa71a1fb35454bf27b894f1c77
Karry Wong <ucdwong@ucdavis.edu>
2021-11-15 10:44:11 -0800

    256 ms

---
+++ solutions/560. Subarray Sum Equals K.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        #Leetcode prefix sum, Time O(N), Space O(N)
+        lib = collections.defaultdict(int)
+        cur_sum = 0
+        count = 0
+        for n in nums:
+            cur_sum += n            
+            #situation 1: prefix sum equal targetSum
+            if cur_sum == k: 
+                count += 1
+            #situation 2: subarray starting in middle equal targetSum
+            #proof: cur_sum - (cur_sum - target) = target
+            count += lib[cur_sum - k]
+            #update
+            lib[cur_sum] += 1
+        return count
+            
+        
