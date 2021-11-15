41c7246abd8cd5714920f41e7b9a5a3bef22d939
Karry Wong <ucdwong@ucdavis.edu>
2021-11-15 12:18:32 -0800

    48 ms

--- solutions/437. Path Sum III.py
+++ solutions/437. Path Sum III.py
@@ -6,28 +6,55 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        #soln 0 - Karry's recursion 
-        self.count = 0
-        def ListMake(lst, val):
-            temp = []
-            for i in lst:
-                i += val
-                if i == targetSum: self.count += 1
-                temp.append(i)
-            return temp
-        
-        def helper(node):
+        #soln 1 - Leetcode prefix sum + recursive preorder traversal
+        def preorder(node, cur_sum):
+            nonlocal count
             if not node:
-                return []
-            if node.val == targetSum: self.count += 1
-            lst = [node.val]
-            if node.left:
-                left_lst = helper(node.left)
-                lst += ListMake(left_lst, node.val)
-            if node.right:
-                right_lst = helper(node.right)
-                lst += ListMake(right_lst, node.val)
-            return lst
+                return
+            
+            cur_sum += node.val
+            #situation 1: prefix sum equal targetSum
+            if cur_sum == targetSum: 
+                count += 1
+            #situation 2: subarray starting in middle equal targetSum
+            #proof: cur_sum - (cur_sum - target) = target
+            count += lib[cur_sum - targetSum]
+            #update
+            lib[cur_sum] += 1
+            
+            #recursive preorder traversal
+            preorder(node.left, cur_sum)
+            preorder(node.right, cur_sum)
+            
+            #remove current sum from hashmap in order not to use it in parallel subtree
+            lib[cur_sum] -= 1
+        count = 0
+        lib = collections.defaultdict(int)
+        preorder(root, 0)
+        return count
+        
+#         #soln 0 - Karry's recursion 
+#         self.count = 0
+#         def ListMake(lst, val):
+#             temp = []
+#             for i in lst:
+#                 i += val
+#                 if i == targetSum: self.count += 1
+#                 temp.append(i)
+#             return temp
+        
+#         def helper(node):
+#             if not node:
+#                 return []
+#             if node.val == targetSum: self.count += 1
+#             lst = [node.val]
+#             if node.left:
+#                 left_lst = helper(node.left)
+#                 lst += ListMake(left_lst, node.val)
+#             if node.right:
+#                 right_lst = helper(node.right)
+#                 lst += ListMake(right_lst, node.val)
+#             return lst
     
-        helper(root)
-        return self.count
+#         helper(root)
+#         return self.count
