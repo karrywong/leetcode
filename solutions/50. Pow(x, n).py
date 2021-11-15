a1f4601f5ee0e486e685b9a88c6fabda2893dcb3
Karry Wong <ucdwong@ucdavis.edu>
2021-11-14 20:23:16 -0800

    24 ms

--- solutions/50. Pow(x, n).py
+++ solutions/50. Pow(x, n).py
@@ -1,5 +1,28 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        #cheating not allowed, return x**n
+        if n == 0: return 1
+        if -1 < x < 1: 
+            x = 1/x
+            n *= -1
+        elif x == 1:
+            return 1
+        elif x == -1:
+            return 1 if n%2==0 else -1
+        # n != 0 and (x < -1 or x > 1)
+        lib = {}
+        nb = bin(abs(n))[2:][::-1] #n=3, bin(n) = "0b1010", nb = "0101"
+        temp = x
+        for i in range(1,len(nb)+1):
+            lib[i] = temp
+            temp *= temp
+        
+        ans = 1
+        for i in range(len(nb)):
+            if nb[i] == "1":
+                ans *= lib[i+1]
+        return ans if n > 0 else 1/ans
+​
         ### Soln 0 - recursion, fast
 #         if n < 0: 
 #             x, n = 1/x, -n
@@ -15,17 +38,16 @@ class Solution:
 #         return recPower(x,n)
     
         ### Soln 1 - another recusion by Haotian Li
-        if x == 0:
-            return 0
-        elif n < 0:
-            return self.myPow( 1/ x, -n)
-        elif n == 0:
-            return 1
-        else:
-            if n == 1:
-                return x 
-            else:
-                remainderTwo = n % 2
-                halfExponent = n // 2
-                return (self.myPow(x,halfExponent) ** 2) * self.myPow(x,remainderTwo)
-                
+        # if x == 0:
+        #     return 0
+        # elif n < 0:
+        #     return self.myPow( 1/ x, -n)
+        # elif n == 0:
+        #     return 1
+        # else:
+        #     if n == 1:
+        #         return x 
+        #     else:
+        #         remainderTwo = n % 2
+        #         halfExponent = n // 2
+        #         return (self.myPow(x,halfExponent) ** 2) * self.myPow(x,remainderTwo)
