ef0f08b040ce7691970abbf9019ba7a4eee46dd1
Karry Wong <ucdwong@ucdavis.edu>
2021-11-14 23:41:49 -0800

    140 ms

--- solutions/701. Insert into a Binary Search Tree.py
+++ solutions/701. Insert into a Binary Search Tree.py
@@ -6,31 +6,29 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        #soln 1 - Leetcode, recursion
-        if not root:
-            return TreeNode(val)        
-        if root.val > val:
-            root.left = self.insertIntoBST(root.left, val)
-        else:
-            root.right = self.insertIntoBST(root.right, val)
-        return root
+        # #soln 1 - Leetcode, recursion
+        # if not root:
+        #     return TreeNode(val)        
+        # if root.val > val:
+        #     root.left = self.insertIntoBST(root.left, val)
+        # else:
+        #     root.right = self.insertIntoBST(root.right, val)
+        # return root
     
-#         #soln 0 - first attempt, recursion
-#         def helper(node):
-#             if node.val > val:
-#                 if node.left is not None:
-#                     helper(node.left)
-#                     return root
-#                 else:
-#                     node.left = TreeNode(val)
-#             else:
-#                 if node.right is not None:
-#                     helper(node.right)
-#                     return root
-#                 else:
-#                     node.right = TreeNode(val)
+        #soln 0 - first attempt, recursion
+        def helper(node):
+            if node.val > val:
+                if node.left is not None:
+                    helper(node.left)
+                else:
+                    node.left = TreeNode(val)
+            else:
+                if node.right is not None:
+                    helper(node.right)
+                else:
+                    node.right = TreeNode(val)
                 
-#         if not root: 
-#             return TreeNode(val)
-#         helper(root)
-#         return root
+        if not root: 
+            return TreeNode(val)
+        helper(root)
+        return root
