950a583ec6f7f52475a3b479fc54f72064c68687
Karry Wong <ucdwong@ucdavis.edu>
2021-11-15 22:38:56 -0800

    72 ms

--- solutions/235. Lowest Common Ancestor of a Binary Search Tree.py
+++ solutions/235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -7,23 +7,23 @@
 ​
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        #soln 1 - iterative
-        pval, qval = p.val, q.val
-        node = root
-        while node:
-            val = node.val
-            if pval < val and qval < val: 
-                node = node.left
-            elif pval > val and qval > val:
-                node = node.right
-            else:
-                return node
-        
-        # #soln 0 - recursion
+        # #soln 1 - iterative
         # pval, qval = p.val, q.val
-        # if pval < root.val and qval < root.val: 
-        #     return self.lowestCommonAncestor(root.left, p, q)
-        # elif pval > root.val and qval > root.val: 
-        #     return self.lowestCommonAncestor(root.right, p, q)
-        # else: 
-        #     return root
+        # node = root
+        # while node:
+        #     val = node.val
+        #     if pval < val and qval < val: 
+        #         node = node.left
+        #     elif pval > val and qval > val:
+        #         node = node.right
+        #     else:
+        #         return node
+        
+        #soln 0 - recursion
+        pval, qval = p.val, q.val
+        if pval < root.val and qval < root.val: 
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif pval > root.val and qval > root.val: 
+            return self.lowestCommonAncestor(root.right, p, q)
+        else: 
+            return root
