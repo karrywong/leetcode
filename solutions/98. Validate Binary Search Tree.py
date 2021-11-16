19e7784ff1023a3f19db62d07ec0d029c1ec2c86
Karry Wong <ucdwong@ucdavis.edu>
2021-11-15 21:27:24 -0800

    44 ms

--- solutions/98. Validate Binary Search Tree.py
+++ solutions/98. Validate Binary Search Tree.py
@@ -6,11 +6,33 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # #soln 0 - standard inorder traversal
-        def helper(node):
+        #soln 2 - Recursive Inorder Traversal
+        def inorder(node):
             if not node:
-                return []
-            return helper(node.left) + [node.val] + helper(node.right)
-        lst = helper(root)
-        return all([lst[i] < lst[i+1] for i in range(len(lst)-1)])
+                return True
+            if not inorder(node.left):
+                return False
+            if node.val <= self.prev:
+                return False
+            self.prev = node.val
+            return inorder(node.right)
+        self.prev = float('-inf')
+        return inorder(root)
+        
+        # #soln 1 - Recursive Traversal with Valid Range
+        # def validate(node, low=float('-inf'), high=float('inf')):
+        #     if not node:
+        #         return True
+        #     if node.val <= low or node.val >= high:
+        #         return False
+        #     return validate(node.left, low, node.val) and validate(node.right, node.val, high)
+        # return validate(root)
+        
+        # # #soln 0 - standard inorder traversal, slow
+        # def helper(node):
+        #     if not node:
+        #         return []
+        #     return helper(node.left) + [node.val] + helper(node.right)
+        # lst = helper(root)
+        # return all([lst[i] < lst[i+1] for i in range(len(lst)-1)])
             
