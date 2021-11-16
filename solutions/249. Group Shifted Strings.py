df859eec0ead4a670a9301c95b1c6babd314a069
Karry Wong <ucdwong@ucdavis.edu>
2021-11-16 13:13:48 -0800

    36 ms

--- solutions/249. Group Shifted Strings.py
+++ solutions/249. Group Shifted Strings.py
@@ -1,5 +1,21 @@
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        #Recent attempt, using tuple + defaultdict
+        lib = collections.defaultdict(list)
+        for string in strings:
+            n = len(string)
+            if n == 1:
+                lib[tuple([None])].append(string)
+            else:
+                lst = [0]*(n-1)
+                for i in range(1,n):
+                    val = ord(string[i]) - ord(string[i-1])
+                    if val < 0: val += 26
+                    lst[i-1] = val
+                lib[tuple(lst)].append(string)
+        # print(lib)
+        return lib.values()
+    
         ### Soln 2 - Stefan Pochmann using itertools
         # key = lambda s: [(ord(c) - ord(s[0])) % 26 for c in s]
         # return [list(g) for _, g in itertools.groupby(sorted(strings, key=key), key)]
@@ -24,6 +40,3 @@ class Solution:
         #         groups += 1
         #         out.append([w])
         # return out
-            
-                
-                
