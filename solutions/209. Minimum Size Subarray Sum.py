12528a441967fb7fd97d56fa3a55a4d90019ae2f
Karry Wong <ucdwong@ucdavis.edu>
2021-11-19 10:52:37 -0800

    64 ms

--- solutions/209. Minimum Size Subarray Sum.py
+++ solutions/209. Minimum Size Subarray Sum.py
@@ -1,43 +1,18 @@
 class Solution:
-    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
-        ### Soln 1 - brute force, O(n^2) time exceeded
-#         if len(nums) == 0: return 0
-        
-#         ans = inf
-#         sums = [None] * len(nums)
-        
-#         sums[0] = nums[0]
-#         for i in range(1, len(nums)):
-#             sums[i] = sums[i-1] + nums[i]
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        #first attempt, prefix sum + sliding window
+        cur_sum, ans = 0, float('inf')
+        n, i, j = len(nums), 0,0
+        while i < n and cur_sum+nums[i] < target:
+            cur_sum += nums[i] 
+            i += 1
+        #cur_sum + nums[i] >= target
+        while i < n:
+            cur_sum += nums[i]
+            while j < n and cur_sum - nums[j] >= target: #sliding window
+                cur_sum -= nums[j]
+                j += 1
+            ans = min(ans, i-j+1)
+            i += 1    
+        return 0 if ans == float('inf') else ans
             
-#         for i in range(0, len(nums)):
-#             for j in range(i, len(nums)):
-#                 val = sums[j] - sums[i] + nums[i] # sum of subarray i to j
-#                 if val >= s:
-#                     ans = min(ans, j - i + 1)
-#                     break
-#         return ans if ans != inf else 0
-​
-        ### Soln 2 - brute force with binary search, improved O(nlogn)
-#         if len(nums) == 0: return 0 
-        
-#         ans = inf
-#         sums = [0] * (len(nums) + 1)
-        
-#         for i in range(1, len(nums) + 1):
-#             sums[i] = sums[i-1] + nums[i-1]
-            
-#         for i in range(1, len(nums) + 1):
-#             to_find = s + sums[i-1]
-#             greater_to_find = [v for v, x in enumerate(sums) if x >= to_find]
-#             if greater_to_find:
-#                 ans = min(ans, greater_to_find[0]-i+1)
-​
-#         return ans if ans != inf else 0
-​
-        ### Soln 3 - two pointer O(n)
-        ans = inf
-        left = 0 #pointer 1
-        sums = 0
-        for i in range(0, len(nums)):
-            sums += nums[i]
