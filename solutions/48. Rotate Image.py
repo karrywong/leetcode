a8488b18ef4c2a8718467f3f0ed5748fad6f2682
Karry Wong <ucdwong@ucdavis.edu>
2021-11-21 10:31:00 -0800

    32 ms

--- solutions/48. Rotate Image.py
+++ solutions/48. Rotate Image.py
@@ -3,9 +3,34 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        ### Soln 0 - Jake & Karry, mathematical transformation
-        for i in range(len(matrix)//2):
-            matrix[i], matrix[~i] = matrix[~i], matrix[i]
-            
-        matrix[:] = list(map(list,zip(*matrix)))
-        # matrix[:] = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+        ##soln1 - Leetcode more logical, first transpose, then reflect
+        n = len(matrix)
+        def transpose(M):
+            for i in range(n):
+                for j in range(i+1,n):
+                    M[i][j], M[j][i] = M[j][i], M[i][j]
+                    
+        def reflect(M):
+            for i in range(n):
+                for j in range(n//2):
+                    M[i][j], M[i][n-1-j] = M[i][n-1-j], M[i][j]
+                    
+        transpose(matrix)
+        reflect(matrix)        
+        
+        # ##soln 0 - Leetcode, standard entry-by-entry rotation
+        # n = len(matrix)
+        # for i in range(n//2+n%2):
+        #     for j in range(n//2):
+        #         tmp = matrix[n-1-j][i]
+        #         matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+        #         matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+        #         matrix[j][n-1-i] = matrix[i][j]
+        #         matrix[i][j] = tmp
+        
+        # #Old attempt
+        # for i in range(len(matrix)//2):
+        #     matrix[i], matrix[~i] = matrix[~i], matrix[i]
+        # matrix[:] = list(map(list,zip(*matrix)))
+​
+​
