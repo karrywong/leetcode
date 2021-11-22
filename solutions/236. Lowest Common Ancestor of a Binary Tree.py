2cd39331e1c479bad531b5e74f4cbf1b8d0d5e42
Karry Wong <ucdwong@ucdavis.edu>
2021-11-21 20:36:05 -0800

    99 ms

--- solutions/236. Lowest Common Ancestor of a Binary Tree.py
+++ solutions/236. Lowest Common Ancestor of a Binary Tree.py
@@ -1,23 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-​
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':         # Karry's solution w Haotian's
-        htb = {} #key=cur.val, value=prev.val
-        queue = collections.deque([root])
-        pval, qval = p.val, q.val
-        bo1, bo2 = False, False
-        
-        while queue and (not bo1 or not bo2):
-            node = queue.popleft()
-            if node.val == pval: bo1 = True
-            elif node.val == qval: bo2 = True
-            if node.left:
-                htb[node.left] = node
                 queue.append(node.left)
             if node.right:
                 htb[node.right] = node
@@ -33,6 +13,22 @@ class Solution:
             if q in pset: return q
             q = htb[q]
         return q
+​
+        # #Leetcode - DFS w recursion
+        # self.ans = None
+        # global pval, qval
+        # def dfs(node):
+        #     if not node: 
+        #         return False
+        #     left = dfs(node.left)
+        #     right = dfs(node.right)
+        #     mid = True if node.val == pval or node.val == qval else False
+        #     if left + right + mid >= 2:
+        #         self.ans = node
+        #     return mid or left or right
+        # pval, qval = p.val, q.val
+        # dfs(root)
+        # return self.ans
 ​
 #         #soln 0 - Jake's soln 
 #         pval = p.val
