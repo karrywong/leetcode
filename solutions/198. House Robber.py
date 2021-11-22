ae328b6c5b12cca890850861207605039f555592
Karry Wong <ucdwong@ucdavis.edu>
2021-11-22 15:14:46 -0800

    36 ms

--- solutions/198. House Robber.py
+++ solutions/198. House Robber.py
@@ -1,17 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #soln 1 - recursion with memoization
+        #soln 2 - Leetcode optimized DP, bottom-up
         n = len(nums)
-        memo = {}
-        def helper(i):
-            if i >= n:
-                return 0
-            if i in memo:
-                return memo[i]
-            val = max(helper(i+1), helper(i+2) + nums[i])
-            memo[i] = val
-            return val
-        return helper(0)
+        if n <= 2: return max(nums)
+        RobNextNext = 0
+        RobNext = nums[n-1]
+        for i in range(n-2, -1, -1):
+            current = max(RobNext, RobNextNext+nums[i])
+            RobNextNext = RobNext
+            RobNext = current
+        return current
+        
+        # #soln 1 - recursion with memoization
+        # n = len(nums)
+        # memo = {}
+        # def helper(i):
+        #     if i >= n:
+        #         return 0
+        #     if i in memo:
+        #         return memo[i]
+        #     val = max(helper(i+1), helper(i+2) + nums[i])
+        #     memo[i] = val
+        #     return val
+        # return helper(0)
         
         # #soln 0 - dynamic programming
         # n = len(nums)
