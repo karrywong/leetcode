15161b81701958e21cf19ab706e596e23e89bf6a
Karry Wong <ucdwong@ucdavis.edu>
2021-11-22 15:28:48 -0800

    24 ms

--- solutions/213. House Robber II.py
+++ solutions/213. House Robber II.py
@@ -1,20 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        ### Reduce the given problem into two original problems of House Robber
-        if len(nums) == 0: return 0
-        elif len(nums) == 1: return nums[0]
-        elif len(nums) == 2: return max(nums)
-        elif len(nums) == 3: return max(nums)
-        
-        lst1 = nums[1:]
-        lst2 = nums[:-1]
-        
-        lst1[2] += lst1[0]
-        for i in range(3, len(lst1)):
-            lst1[i] += max(lst1[i-2], lst1[i-3])
+        #Second attempt, same idea but cleaner
+        if len(nums) <= 3: return max(nums)
+        def rob_alg(lst):
+            n = len(lst)
+            RobNextNext = 0
+            RobNext = lst[n-1]
+            for i in range(n-2,-1,-1):
+                current = max(RobNext, RobNextNext + lst[i])
+                RobNextNext = RobNext
+                RobNext = current
+            return current
+        return max(rob_alg(nums[1:]), rob_alg(nums[:-1]))
             
-        lst2[2] += lst2[0]
-        for i in range(3, len(lst2)):
-            lst2[i] += max(lst2[i-2], lst2[i-3])
-            
-        return max(max(lst1), max(lst2))
+#         #First attempt, reduced to 198. House Robber
+#         n = len(nums)
+#         if n <= 3: return max(nums)
+        
+#         rob1 = nums[:n-1] #length of n-1
+#         rob1[2] += rob1[0]
+#         for i in range(3,n-1):
+#             rob1[i] += max(rob1[i-2], rob1[i-3])
+#         ans1 = max(rob1[-1], rob1[-2])
+        
+#         rob2 = nums[1:]
+#         rob2[2] += rob2[0]
+#         for i in range(3,n-1):
+#             rob2[i] += max(rob2[i-2], rob2[i-3])
+#         ans2 = max(rob2[-1], rob2[-2])   
+#         return max(ans1, ans2)
+        
