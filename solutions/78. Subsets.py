e988f31a55248b6a53e62dba424c044cd86c7eb0
Karry Wong <ucdwong@ucdavis.edu>
2021-11-25 11:08:10 -0800

    28 ms

--- solutions/78. Subsets.py
+++ solutions/78. Subsets.py
@@ -1,12 +1,49 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[], [nums[0]]]
-        if len(nums) == 1: return res
+        #soln 3 - Leetcode bit manipulation
+        n, ans = len(nums), []
+        for i in range(2**n, 2**(n+1)):
+            bitmask = bin(i)[3:]
+            
+            A = []
+            for j in range(n):
+                if bitmask[j] == '1':
+                    A.append(nums[j])
+            ans.append(A)
+        return ans
         
-        for i in range(1, len(nums)):
-            temp = [[] for x in range(len(res))]
-            for j in range(0, len(res)):
-                temp[j] = res[j] + [nums[i]]
-            res = res + temp
-  
-        return res
+        #soln 2 - Leetcode backtracking, fastere
+#         n, ans = len(nums), []
+#         def backtrack(start = 0, A = []):
+#             if len(A) == k:  
+#                 ans.append(A[:])
+#                 return
+#             for i in range(start, n):
+#                 A.append(nums[i])
+#                 backtrack(i + 1, A)
+#                 A.pop()
+                
+#         for k in range(n + 1):
+#             backtrack()
+#         return ans
+        
+        #soln 1 - Leetcode cascading, much faster
+        # n, ans = len(nums), [[]]
+        # for num in nums:
+        #     ans += [cur + [num] for cur in ans]
+        # return ans
+    
+#         #first attempt, backtracking, Time O(N*2^N), Space O(N*2^N), much slower due to repetitions
+#         n, ans = len(nums), []
+#         def backtrack(start=0, A=[]):
+#             if A not in ans:
+#                 ans.append(A[:])
+            
+#             for i in range(start, n):
+#                 if nums[i] not in A:
+#                     A.append(nums[i])
+#                     backtrack(i+1, A)
+#                     A.pop()
+            
+#         backtrack()
+#         return ans
