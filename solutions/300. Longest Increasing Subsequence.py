e927a9d9e6a551b2c603393289cc8501e136a550
Karry Wong <ucdwong@ucdavis.edu>
2021-11-26 12:06:51 -0800

    97 ms

---
+++ solutions/300. Longest Increasing Subsequence.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        #soln 2 - use python bisect(), refer <https://docs.python.org/3/library/bisect.html>
+        n, sub = len(nums), [nums[0]]
+        for i in range(1,n):
+            j = bisect_left(sub, nums[i]) #return index j such that #nums[i] <= sub[j] 
+            if j == len(sub):
+                sub.append(nums[i])
+            else:
+                sub[j] = nums[i]
+        return len(sub)
+    
+        #soln 1 - Leetcode build subsequence
+        # n, sub = len(nums), [nums[0]]
+        # for i in range(1,n):
+        #     if sub[-1] >= nums[i]:
+        #         j = 0
+        #         while nums[i] > sub[j]:
+        #             j += 1
+        #         sub[j] = nums[i] #nums[i] <= sub[j] 
+        #     else: #sub[-1] < nums[i]
+        #         sub.append(nums[i])
+        # return len(sub)
+                
+        # #soln 0 - Leetcode DP 
+        # n = len(nums)
+        # dp = [1] * n 
+        # for i in range(1,n):
+        #     for j in range(0,i):
+        #         if nums[j] < nums[i]:
+        #             dp[i] = max(dp[i], dp[j]+1)
+        # return max(dp)
