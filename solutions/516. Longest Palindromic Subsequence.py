1403f09fd45623ca73e9f3e9e5f40d5197d9a2b4
Karry Wong <ucdwong@ucdavis.edu>
2021-11-26 09:19:32 -0800

    1723 ms

--- solutions/516. Longest Palindromic Subsequence.py
+++ solutions/516. Longest Palindromic Subsequence.py
@@ -1,22 +1,15 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        ### Soln from discussion by HT_Wang
-        ### Similar questions: palindromic substrings, shortest palindrome
-​
+        #Old attempt - DP programming
+        #Refer to almost identical problems, 5. Longest Palindromic Substring & 647. Palindromic Substrings
         n = len(s)
-        dp = [[0]*(n) for _ in range(n) ]
-        
-        # handles cases for single character palindromes
+        dp = [[0]*n for _ in range(n)]
         for i in range(n):
             dp[i][i] = 1
-​
-                    
-        for i in range(n-1, -1, -1):
+        
+        for i in range(n-2, -1, -1):
             for j in range(i+1, n):
                 # if current left / right characters are equal, then extend the largest pal by 2
-                if s[i]==s[j]:
-                    dp[i][j] = dp[i+1][j-1]+2
                 # else, max between the two last largest palindromes found 
-                else:
-                    dp[i][j] = max(dp[i][j-1], dp[i+1][j])
+                dp[i][j] = dp[i+1][j-1] + 2 if s[i] == s[j] else max(dp[i+1][j],dp[i][j-1])
         return dp[0][-1]
