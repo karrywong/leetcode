20b69878eb4b6ff2d4b81b1d477ae204131cec9b
Karry Wong <ucdwong@ucdavis.edu>
2021-11-29 20:36:17 -0800

    52 ms

--- solutions/337. House Robber III.py
+++ solutions/337. House Robber III.py
@@ -1,34 +1,49 @@
-#                 return result
+            if i not in graph: #node i is a leaf
+                dp_rob[i] = tree[i]
+                dp_not_rob[i] = 0
+            else:
+                dp_rob[i] = tree[i] + sum(dp_not_rob[j] for j in graph[i])
+                dp_not_rob[i] = sum(max(dp_rob[j], dp_not_rob[j]) for j in graph[i])
+        
+        return max(dp_rob[0], dp_not_rob[0])
+    
+#         #soln 1 - Leetcode recursion w memoization
+#         rob_memo, not_rob_memo = {}, {}
+        
+#         def helper(node, parent_robbed = False):
+#             if not node: 
+#                 return 0
             
+#             if parent_robbed:
+#                 #cannot rob this node
+#                 if node in rob_memo:
+#                     return rob_memo[node]
+#                 val = helper(node.left, False) + helper(node.right, False)
+#                 rob_memo[node] = val
+#                 return val
 #             else:
-#                 if node in not_rob_saved:
-#                     return not_rob_saved[node]
-                
+#                 #calculate 'rob' and 'not rob', return max(rob, not_rob)
+#                 if node in not_rob_memo:
+#                     return not_rob_memo[node]
 #                 rob = node.val + helper(node.left, True) + helper(node.right, True)
 #                 not_rob = helper(node.left, False) + helper(node.right, False)
-                
-#                 result = max(rob, not_rob)
-#                 not_rob_saved[node] = result
-#                 return result
-            
-#         return helper(root, False)
+#                 val = max(rob, not_rob)
+#                 not_rob_memo[node] = val
+#                 return val
+#         return helper(root)
         
-        ### LeetCode solution - only recursion
-        def helper(node):
-            #return [rob this node, not rob this node]
-            if not node: #at leaf
-                return (0,0)
+#         #soln 0 - Leetcode recursion, time complexity O(N), space O(1)
+#         def helper(node): #return (rob, not_rob)
+#             if not node:
+#                 return (0,0)
             
-            left = helper(node.left)
-            right = helper(node.right)
+#             left = helper(node.left)
+#             right = helper(node.right)
             
-            #if we rob this node, we cannot rob its children
-            rob = node.val + left[1] + right[1]
+#             #if we rob this node, we cannot rob its children
+#             rob = node.val + left[1] + right[1]
+#             #else we could choose to either rob its children or not
+#             not_rob = max(left) + max(right)
             
-            #else we could choose to either rob its children or not
-            not_rob = max(left) + max(right)
-            
-            # print(rob, not_rob)
-            return [rob, not_rob]
-        
-        return max(helper(root))
+#             return (rob, not_rob)
+#         return max(helper(root))
