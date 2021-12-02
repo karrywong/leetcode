998f240f48eba7f4ddf5d99030931b1eee8afd35
Karry Wong <ucdwong@ucdavis.edu>
2021-12-01 21:17:00 -0800

    104 ms

--- solutions/49. Group Anagrams.py
+++ solutions/49. Group Anagrams.py
@@ -1,32 +1,34 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ### Soln 1 - LeetCode answer, smart! defaultdict is the key
-        lib = collections.defaultdict(list)
-        for s in strs:
-            lib[tuple(sorted(s))].append(s)
-        return lib.values()
-        
-        ### Soln 2 - even better
-        # ans = collections.defaultdict(list)
+        #  ### Soln 1 - LeetCode answer, smart! defaultdict is the key
+        # lib = collections.defaultdict(list)
         # for s in strs:
-        #     count = [0] * 26
-        #     for c in s:
-        #         count[ord(c) - ord('a')] += 1
-        #     ans[tuple(count)].append(s)
-        # return ans.values()
+        #     lib[tuple(sorted(s))].append(s)
+        # return lib.values()
         
-#         ### Soln 0 - original attempt, O(N^2), super slow
-#         lst = [sorted(tuple(s)) for s in strs]
-#         ele = set([tuple(e) for e in lst])
-#         res = []
+        ## Soln 2 - even better using key code
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
         
-#         i = 0
-#         for a in ele:
-#             res.append([])
-#             alist = list(a)
-#             for j, b in enumerate(lst):
-#                 if alist == b: 
-#                     if not res[i]: 
-#                         res[i] = [strs[j]]
-#                     else:
-#                         res[i].append(strs[j])
+        # #soln 0 - first attemp, super slow, Time O(N^2)
+        # n = len(strs)
+        # ans = [[strs[0]]]
+        # lst_lib = [collections.Counter(strs[0])]
+        # for i in range(1, n):
+        #     bo = True
+        #     word = strs[i]
+        #     lib = collections.Counter(word)
+        #     for j, lib0 in enumerate(lst_lib):
+        #         if lib == lib0:
+        #             ans[j].append(word)
+        #             bo = False
+        #             break
+        #     if bo:
+        #         ans.append([word])
+        #         lst_lib.append(lib)
+        # return ans
