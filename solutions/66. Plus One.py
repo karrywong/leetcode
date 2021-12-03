dce70de92e3c2a81ac7531a7e616bdd337acbd61
Karry Wong <ucdwong@ucdavis.edu>
2021-12-02 17:20:46 -0800

    36 ms

--- solutions/66. Plus One.py
+++ solutions/66. Plus One.py
@@ -1,30 +1,23 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for idx in range(len(digits)-1, -1, -1):
-            if digits[idx] != 9:
-                digits[idx] += 1
-                break
-            else:
+        #Leetcode Textbook addition with carry, clean, time O(N), space O(N)
+        n = len(digits)
+        for idx in range(n-1, -1, -1):
+            if digits[idx] == 9:
                 digits[idx] = 0
-                
-            if digits[0] == 0:
-                digits.insert(0,1)
-        return digits
-    
-#         temp = digits[::-1]
-#         val = 0
-#         for i in range(0, len(temp)):
-#             val += temp[i] * 10**i
+            else:
+                digits[idx] += 1
+                return digits
+        return [1] + digits
         
-#         val += 1
-#         answer = []
-#         while val != 0:
-#             answer.append(val % 10)
-#             val = val // 10
-        
-#         answer = answer[::-1]
-#         if len(answer) < len(digits):
-#             answer = ['0']* (len(digits) - len(answer)) + answer
-        
-#         return answer
-        
+        # # My first attempt in-place, time O(N), space O(N)
+        # val = digits[-1] + 1
+        # carry, digits[-1] = divmod(val, 10)
+        # if carry == 0: 
+        #     return digits
+        # i = -2
+        # while -i <= len(digits) and carry == 1:
+        #     val = digits[i]+carry
+        #     carry, digits[i] = divmod(val, 10)
+        #     i -= 1
+        # return [1]+digits if carry else digits
