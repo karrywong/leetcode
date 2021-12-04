c4d894f4249e840a95bc0168225e9399b410fe0c
Karry Wong <ucdwong@ucdavis.edu>
2021-12-04 12:37:28 -0800

    132 ms

--- solutions/1249. Minimum Remove to Make Valid Parentheses.py
+++ solutions/1249. Minimum Remove to Make Valid Parentheses.py
@@ -1,20 +1,49 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        # eg1, "lee(t(c)o)de)" -> "(()))" -> "(())"
-        stack = []
-        tbremoved = []
-        for i, char in enumerate(s): 
-            if char == "(":
-                stack.append((char,i))
-            if char == ")":
-                if not stack or stack[-1][0] != "(":
-                    tbremoved.append(i)
-                else:
-                    stack.pop()
+        #Leetcode two-pass string builder
+        # Pass 1: Remove all invalid ")"
+        first_pass_chars = []
+        balance = 0
+        open_seen = 0
+        for c in s:
+            if c == "(":
+                balance += 1
+                open_seen += 1
+            if c == ")":
+                if balance == 0:
+                    continue
+                balance -= 1
+            first_pass_chars.append(c)
+​
+        # Pass 2: Remove the rightmost "("
+        result = []
+        open_to_keep = open_seen - balance
+        for c in first_pass_chars:
+            if c == "(":
+                open_to_keep -= 1
+                if open_to_keep < 0:
+                    continue
+            result.append(c)
+​
+        return "".join(result)
         
-        while stack: 
-            char, i = stack.pop()
-            tbremoved.append(i)
+#         #First attempt, mock interview on 10/24
+#         #Three passes, time O(N), space O(N)
+#         # eg1, "lee(t(c)o)de)" -> "(()))" -> "(())"
+#         stack = []
+#         tbremoved = []
+#         for i, char in enumerate(s): 
+#             if char == "(":
+#                 stack.append((char,i))
+#             if char == ")":
+#                 if not stack or stack[-1][0] != "(":
+#                     tbremoved.append(i)
+#                 else:
+#                     stack.pop()
         
-        lst = [char for i, char in enumerate(s) if i not in tbremoved]
-        return ''.join(lst)        
+#         while stack: 
+#             char, i = stack.pop()
+#             tbremoved.append(i)
+        
+#         lst = [char for i, char in enumerate(s) if i not in tbremoved]
+#         return ''.join(lst)        
