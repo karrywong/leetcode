2d967132b5475ad408961e3e53434c7d04b83420
Karry Wong <ucdwong@ucdavis.edu>
2021-12-04 12:05:03 -0800

    28 ms

--- solutions/206. Reverse Linked List.py
+++ solutions/206. Reverse Linked List.py
@@ -5,26 +5,35 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]: 
-        #soln 2 - Leetcode recursion
-        if not head or not head.next:
-            return head
-        p = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return p 
+        # #soln 2 - Leetcode recursion, time O(N), space O(N)
+        # if not head or not head.next:
+        #     return head
+        # p = self.reverseList(head.next)
+        # head.next.next = head
+        # head.next = None
+        # return p 
         
-        # #soln 1 - one pass
-        # if not head: return head
-        # prev = ListNode(head.val)
-        # curr = head
-        # curr = curr.next
-        # while curr:
-        #     next_temp = curr.next
-        #     curr.next = prev
-        #     prev = curr
-        #     curr = next_temp
-        # return prev
+        #soln 1 - one pass, time O(N), space O(1)
+        if not head: return head
+        prev = ListNode(head.val)
+        curr = head
+        curr = curr.next
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+        return prev
         
         # #soln 0 - naive attempt, two pass
         # if not head: return head
         # stack = []
+        # while head:
+        #     stack.append(head.val)
+        #     head = head.next
+        # ans_ptr = ListNode(stack.pop())
+        # ans = ans_ptr
+        # while stack:
+        #     ans_ptr.next = ListNode(stack.pop())
+        #     ans_ptr = ans_ptr.next
+        # return ans
