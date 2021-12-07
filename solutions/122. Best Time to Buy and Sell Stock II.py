9fd5f991adf4959f7717570bdc0f5cf02e4993f6
Karry Wong <ucdwong@ucdavis.edu>
2021-12-06 20:24:32 -0800

    60 ms

--- solutions/122. Best Time to Buy and Sell Stock II.py
+++ solutions/122. Best Time to Buy and Sell Stock II.py
@@ -1,8 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-            ### One pass
-            answer = 0
-            for i in range(1, len(prices)):
-                if prices[i-1] < prices[i]:
-                    answer += prices[i] - prices[i-1]
-            return answer
+        #One-liner
+        return sum([prices[i] - prices[i-1] for i in range(1, len(prices)) if prices[i] - prices[i-1] > 0])
+        
+        # #First attempt, solved in 5 min
+        # #Time: O(N), Space: O(1)
+        # ans = 0
+        # for i in range(1, len(prices)):
+        #     change = prices[i] - prices[i-1]
+        #     if change > 0:
+        #         ans += change
+        # return ans
