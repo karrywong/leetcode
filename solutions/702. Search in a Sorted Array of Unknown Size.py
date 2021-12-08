cec9ff54b28462272608074759910a17b0eb47cb
Karry Wong <ucdwong@ucdavis.edu>
2021-12-07 22:19:55 -0800

    61 ms

--- solutions/702. Search in a Sorted Array of Unknown Size.py
+++ solutions/702. Search in a Sorted Array of Unknown Size.py
@@ -12,29 +12,20 @@ class Solution:
         :type target: int
         :rtype: int
         """
-        ### Soln 0 - find right boundary
-        r = 10000
-        l = 0
-        while (l <= r):
-            mid = l + (r-l) // 2
-            val = reader.get(mid)
-            if val == target:
+        #Time O(logN + logN) = O(logN), Space O(1) 
+        #Bit manipulation to find the boundary
+        if reader.get(0) == target: return 0
+        l, r = 0, 1
+        while reader.get(r) < target: #python doesn't have overflow issue
+            l = r
+            r <<= 1
+        # reader.get(r) >= target
+        while l <= r: #BS
+            mid = l + ((r-l)>>1)
+            if reader.get(mid) == target:
                 return mid
-            elif val < target:
+            elif reader.get(mid) < target:
                 l = mid + 1
-            else: 
+            else:
                 r = mid - 1
-        
         return -1
-​
-#         ### Soln 1 - LeetCode solution
-#         if reader.get(0) == target:
-#             return 0
-        
-#         # search boundaries
-#         left, right = 0, 1
-#         while reader.get(right) < target:
-#             left = right
-#             right <<= 1
-        
-#         # binary search
