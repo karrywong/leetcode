a356e55e1bdd1c94d715ab238fdf99094f4a42f7
Karry Wong <ucdwong@ucdavis.edu>
2021-12-13 10:35:07 -0800

    68 ms

--- solutions/567. Permutation in String.py
+++ solutions/567. Permutation in String.py
@@ -6,9 +6,6 @@ class Solution:
         
         lib1 = collections.Counter(s1[:])
         lib2 = collections.Counter(s2[:n1])
-        # for i in range(n1):
-        #     lib1[s1[i]] += 1
-        #     lib2[s2[i]] += 1
         
         for i in range(n2 - n1):
             if lib1 == lib2:
