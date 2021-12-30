96a86fce309ba6692e2d9f7dce19b7c94f9d4446
Karry Wong <ucdwong@ucdavis.edu>
2021-12-29 20:45:23 -0800

    158 ms

--- solutions/347. Top K Frequent Elements.py
+++ solutions/347. Top K Frequent Elements.py
@@ -1,23 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ### Soln 1 - using heap structure
-        counts = collections.Counter(nums)
-        heap = [(v, k) for k, v in counts.items()]
-        lst = heapq.nlargest(k, heap)
-        res = [k for v, k in lst]
-        return res
+        # O(1) time 
+        if k == len(nums):
+            return nums
         
-#         ### Soln 0 - O(nlogn), time complexity
-#         counts = collections.Counter(nums) #hashtable
-        
-#         lst = [v for v in counts.values()]
-#         lst.sort(reverse = True) #worst case O(nlog(n))
-#         freq = lst[0:k] # largest k frequencies
-        
-#         res = [] 
-#         # find the keys corresponding to the k frequencies
-#         for i in range(k):
-#             for k, v in counts.items():
-#                 if v == freq[i] and k not in res:
-#                     res.append(k)
-#         return res
+        # 1. build hash map : character and how often it appears
+        # O(N) time
+        count = Counter(nums)   
+        # 2-3. build heap of top k frequent elements and
+        # convert it into an output array
+        # O(N log k) time
+        return heapq.nlargest(k, count.keys(), key=count.get)        
