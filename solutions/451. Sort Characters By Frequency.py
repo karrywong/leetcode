e8bc6031f671d4fc03d902c9ea9a31ad958f3328
Karry Wong <ucdwong@ucdavis.edu>
2022-01-02 08:36:51 -0800

    100 ms

--- solutions/451. Sort Characters By Frequency.py
+++ solutions/451. Sort Characters By Frequency.py
@@ -1,7 +1,17 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        #soln 0 - first attempt, two-liner, Time O(NlogN), Space O(N)
-        lib = collections.Counter(s)
-        return ''.join([ k*v for k, v in sorted(lib.items(), key = lambda x : x[1], reverse = True)])
+        # #soln 0 - Hashtable, two-liner, Time O(NlogN), Space O(N)
+        # lib = collections.Counter(s)
+        # return ''.join([ k*v for k, v in sorted(lib.items(), key = lambda x : x[1], reverse = True)])
         
+        #soln 1 - BucketSort, time O(N), space O(N)
+        lib = collections.Counter(s) #O(n)
+        max_freq = max(lib.values())
+        buckets = [[] for _ in range(max_freq+1)]
+        
+        for letter,freq in lib.items():
+            buckets[freq].append(letter)  #O(k)
+        
+        return "".join([freq*letter for freq in range(max_freq,0,-1) for letter in buckets[freq]])
+            
         
