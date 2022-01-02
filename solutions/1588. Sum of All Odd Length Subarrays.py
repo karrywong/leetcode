fed0f8ca07a3150f4f1764a11b1f03cb2483818c
Karry Wong <ucdwong@ucdavis.edu>
2022-01-01 17:18:17 -0800

    48 ms

---
+++ solutions/1588. Sum of All Odd Length Subarrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        #Time O(N^2), Space O(N)
+        htb = {-1:0} #key = index, value = culmulative sum
+        ans = 0
+        cur_sum = 0
+        for i in range(len(arr)):
+            ans += arr[i]
+            cur_sum += arr[i]
+            htb[i] = cur_sum
+            
+            count = 1 
+            while i-2*count-1 >= -1:
+                ans += htb[i] - htb[i-2*count-1]
+                count += 1
+        return ans
+        
+        #[1,4,2,5,3]
+        #cur_sum = [1,5,7,12,15]
+        
+        #i = 2, ans = 1 + 4 + (7-0)
+        #i = 3, ans += 5 + (12-1)
+        #i = 4, ans += 3 + (15-5) + (15-0)
