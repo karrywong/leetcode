450fcb00d56d9f85fe38e0d8d525cfb1058dcfcb
Karry Wong <ucdwong@ucdavis.edu>
2022-01-05 15:26:17 -0800

    84 ms

--- solutions/44. Wildcard Matching.py
+++ solutions/44. Wildcard Matching.py
@@ -1,32 +1,41 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        #Recursion w/ memoization, slow, time O(SP), space O(SP)
-        #S = len(s) and P = len(p)
-        def cleanup(p):
-            p_cleaned = []
-            for char in p:
-                if not p_cleaned or char != "*" or (char == "*" and p_cleaned[-1] != "*"):
-                    p_cleaned.append(char)
-            return ''.join(p_cleaned)
+        #Leetcode Two-pointer, time O(SP), space O(1)
+        S, P = len(s), len(p)
+        i, j = 0,0
+        star_idx, s_tmp_idx = -1, -1
         
-        def dp(i,j):
-            if (i,j) not in memo:
-                if s[i:] == p[j:] or p[j:]=="*":
-                    ans = True
-                elif s[i:] == "" or p[j:] == "":
-                    ans = False                
-                elif s[i] == p[j] or p[j] == "?":
-                    ans = dp(i+1,j+1)
-                elif p[j] == "*":
-                    ans = dp(i,j+1) or dp(i+1,j)
-                else:
-                    ans = False
-                memo[(i,j)] = ans
-            return memo[(i,j)]
+        while i < S:
+            if j < P and p[j] in {s[i],'?'}:
+                i += 1
+                j += 1
+            elif j < P and p[j] == "*":
+                s_tmp_idx = i
+                star_idx = j
+                j += 1
+            elif star_idx == -1:
+                return False
+            else: #backtrack
+                i = s_tmp_idx + 1
+                s_tmp_idx = i
+                j = star_idx + 1
+        return all(p[k] == "*" for k in range(j,P))
+    
+#         #Recursion w/ memoization, slow, time O(SP), space O(SP)
+#         #S = len(s) and P = len(p)
+#         def cleanup(p):
+#             p_cleaned = []
+#             for char in p:
+#                 if not p_cleaned or char != "*" or (char == "*" and p_cleaned[-1] != "*"):
+#                     p_cleaned.append(char)
+#             return ''.join(p_cleaned)
         
-        memo = {}
-        p = cleanup(p)
-        if p == s or p == "*":
-            return True
-        return dp(0,0)
-                    
+#         def dp(i,j):
+#             if (i,j) not in memo:
+#                 if s[i:] == p[j:] or p[j:]=="*":
+#                     ans = True
+#                 elif s[i:] == "" or p[j:] == "":
+#                     ans = False                
+#                 elif s[i] == p[j] or p[j] == "?":
+#                     ans = dp(i+1,j+1)
+#                 elif p[j] == "*":
