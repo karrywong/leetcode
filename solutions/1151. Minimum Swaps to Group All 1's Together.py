d4ab07125b46da8e2cc37bfa1fd38ab16f6f576d
Karry Wong <ucdwong@ucdavis.edu>
2022-01-05 17:48:43 -0800

    896 ms

--- solutions/1151. Minimum Swaps to Group All 1's Together.py
+++ solutions/1151. Minimum Swaps to Group All 1's Together.py
@@ -1,13 +1,26 @@
 class Solution:
     def minSwaps(self, data: List[int]) -> int:
-        #First attempt, after hints 1, 2 & 3, time: O(N), space O(1)
+        # #First attempt, after hints 1, 2 & 3, time: O(N), space O(1)
+        # ones = sum(data)
+        # val = sum(data[0:ones]) #sliding window
+        # i = 0
+        # ans = ones - val
+        # while i+ones < len(data):
+        #     val -= data[i]
+        #     val += data[i+ones]
+        #     ans = min(ans, ones-val)
+        #     i += 1
+        # return ans
+        
+        #Leetcode Deque, same idea from sliding window, time O(N), space O(1)
         ones = sum(data)
-        val = sum(data[0:ones]) #sliding window
-        i = 0
-        ans = ones - val
-        while i+ones < len(data):
-            val -= data[i]
-            val += data[i+ones]
-            ans = min(ans, ones-val)
-            i += 1
-        return ans
+        count = max_one = 0
+        deque = collections.deque()
+        for i in range(len(data)):
+            deque.append(data[i])
+            count += data[i]
+            
+            if len(deque) > ones:
+                count -= deque.popleft()
+            max_one = max(max_one, count)
+        return ones - max_one
