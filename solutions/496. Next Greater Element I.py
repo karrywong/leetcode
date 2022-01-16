a16a90716fcfef2d46038a8017db7fb4efdec115
Karry Wong <ucdwong@ucdavis.edu>
2022-01-15 16:01:13 -0800

    82 ms

--- solutions/496. Next Greater Element I.py
+++ solutions/496. Next Greater Element I.py
@@ -1,32 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        #Soln 1 - brute force
-#         library = {k: v for v, k in enumerate(nums2)} 
-#         print(library)
-        
-#         answer = [-1]*len(nums1)
-#         for i, n in enumerate(nums1):
-#             for m in nums2[library[n]+1:]:
-#                 if m > n: 
-#                     answer[i] = m
-#                     break
-#         return answer
-​
-        #Soln 2 Stack
-        if len(nums2) == 0: return []
-        stack = [nums2[0]]
-        library = {}
-        answer = []
-        
-        for n in nums2[1:]:
-            while stack and n > stack[-1]:
-                library[stack[-1]] = n
-                stack.pop()
-            stack.append(n)
-​
-        while stack:
-            library[stack.pop()] = -1
-        for m in nums1:
-            answer.append(library[m])
-        return answer
-        
+        #First attempt, time O(N), space O(N), where N = len(nums2)
+        m, n = len(nums1), len(nums2)
+        ans = [-1]*m
+        htb = collections.defaultdict(lambda:-1)
+        for ind, num in enumerate(nums1):
+            htb[num] = ind
+        stack = [] #Monotonic stack
+        for num in nums2:
+            while stack and num > stack[-1]:
+                ind = htb[stack.pop()]
+                if ind > -1:
+                    ans[ind] = num
+            stack.append(num)
+        return ans    
