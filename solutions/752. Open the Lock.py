193129bf9c91e76e76592b4ab21da4aea77d51cc
Karry Wong <ucdwong@ucdavis.edu>
2022-01-17 15:28:24 -0800

    1204 ms

--- solutions/752. Open the Lock.py
+++ solutions/752. Open the Lock.py
@@ -1,25 +1,25 @@
-from collections import deque
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        #soln 2 - Leetcode's solution
-        def neighbors(node):
-            for i in range(0,4):
-                x = int(node[i])
-                for d in (-1, 1):
-                    y = (x + d) % 10
-                    yield node[:i] + str(y) + node[i+1:]
-        dead = set(deadends)
-        queue = collections.deque([('0000', 0)])
-        seen = {'0000'}
-        while queue:
-            node, depth = queue.popleft()
-            if node == target: return depth
-            if node in dead: continue
-            for nei in neighbors(node):
-                if nei not in seen:
-                    seen.add(nei)
-                    queue.append((nei, depth+1))
-        return -1
+        #LeetCide BFS, time O(N^2*A^N+D), where A is #digits in alphabet, N #digits in the lock, D = len(deadends)
+#         def neighbors(node):
+#             for i in range(0,4):
+#                 x = int(node[i])
+#                 for d in (-1, 1):
+#                     y = (x + d) % 10
+#                     yield node[:i] + str(y) + node[i+1:]
+​
+#         dead = set(deadends)
+#         queue = collections.deque([('0000', 0)])
+#         seen = {'0000'}
+#         while queue:
+#             node, depth = queue.popleft()
+#             if node == target: return depth
+#             if node in dead: continue
+#             for nei in neighbors(node):
+#                 if nei not in seen:
+#                     seen.add(nei)
+#                     queue.append((nei, depth+1))
+#         return -1
         
         #soln 1 - Jake's BFS
         def getNeighbors(s, deadends):
@@ -43,3 +43,17 @@ class Solution:
         while ptQ:
             curPt = ptQ.popleft()
             # Any additional code
+            if curPt == target:
+                dist = 0
+                while curPt != '0000':
+                    curPt = pre[curPt]
+                    dist += 1
+                return dist
+            neighbors = getNeighbors(curPt, deadends)
+            
+            for node in neighbors:
+                if node not in visited:
+                    ptQ.append(node)
+                    visited[node] = None
+                    pre[node] = curPt
+        return -1
