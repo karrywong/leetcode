4486e382a1ddfb80ae9cb3a49eec29620ed64186
Karry Wong <ucdwong@ucdavis.edu>
2022-01-17 21:34:17 -0800

    66 ms

--- solutions/31. Next Permutation.py
+++ solutions/31. Next Permutation.py
@@ -3,30 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        ### Soln 1 - idea from Jake Reschke
+        #Time O(N), space O(N)
+        #Modified Jake's solution for 556. Next Greater Element III
         n = len(nums)
         if n == 1: return nums
-        
-        check = False
-        # get index of first digit from right smaller than predecessor
-        # if none return -1
+        # get index of first digit from right smaller than predecessor, return -1 if none
         j = -2
-        while nums[j] >= nums[j+1]:
+        while j > -n-1 and nums[j] >= nums[j+1]:
             j -= 1
-            if j == -n-1: 
-                nums[:] = nums[::-1]
-                check = True
-                break
-        
-        if not check:
-            i = -1   
-            # get index of first digit from right smaller than digits[j]
+        if j == -n-1: 
+            nums.reverse()
+        else:
+            # get index of first digit from right larger than digits[j]
+            i = -1  
             while nums[j] >= nums[i]:
-                i -= 1    
+                i -= 1
 ​
             ## swap digits at i and j
-            nums[i], nums[j] = nums[j],nums[i]
+            nums[i],nums[j] = nums[j],nums[i]
             # Sort the remaining digits in ascedning order
             if j < -1:
-                nums[j+1:] = sorted(nums[j+1:])   
-​
+                nums[j+1:] = nums[j+1:][::-1]            
