e07bf51a8603a51953842ceddb3055514501b432
Karry Wong <ucdwong@ucdavis.edu>
2022-01-20 21:37:01 -0800

    132 ms

--- solutions/1048. Longest String Chain.py
+++ solutions/1048. Longest String Chain.py
@@ -1,7 +1,24 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
-        ### Soln - by lee215, super clever!
-        dp = {}
-        for w in sorted(words, key=len):
-            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in range(len(w)))
-        return max(dp.values())
+        #Failed attempt, Leetcode top-down DP
+        #Time O(N+N*L^2) = O(N*L^2), space O(N)
+        wordsPresent = set(words)
+        memo = {} #key: word, value: integer, eg ab->a w/ memo[ab]=2
+​
+        def dfs(w): # find the longest word sequence
+            if w in memo:
+                return memo[w]
+            
+            maxLength = 1
+            for i in range(len(w)):
+                temp = w[:i] + w[i+1:]
+                if temp in wordsPresent:
+                    curLength = 1 + dfs(temp)
+                    maxLength = max(maxLength, curLength)
+            memo[w] = maxLength
+            return maxLength
+                
+        ans = 0
+        for word in words:
+            ans = max(ans, dfs(word))
+        return ans
