fb4753223875bef236e824199c015e7b8dfe92b9
Karry Wong <ucdwong@ucdavis.edu>
2022-01-22 16:51:17 -0800

    47 ms

--- solutions/1823. Find the Winner of the Circular Game.py
+++ solutions/1823. Find the Winner of the Circular Game.py
@@ -1,5 +1,13 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
+        # #Second attempt, straight forward using queue, time O(N), space O(1)
+        # queue = collections.deque(range(1,n+1))
+        # while len(queue) > 1:
+        #     for i in range(k-1):
+        #         queue.append(queue.popleft())
+        #     queue.popleft()
+        # return queue[0]
+        
         #First attempt, time O(N), space O(1)
         lst = [i for i in range(1,n+1)]
         cur, m = k-1, len(lst)
