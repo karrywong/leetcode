377c41f90962097bf1c6acf059d8c2e68b3e969d
Karry Wong <ucdwong@ucdavis.edu>
2022-01-23 17:19:15 -0800

    54 ms

--- solutions/274. H-Index.py
+++ solutions/274. H-Index.py
@@ -9,26 +9,29 @@ class Solution:
         
         #[3,1,1]
         # ind = 1, citation = 1, 1st cond yes, 2nd cond yes, return citation
-        
-        #[6,6,6,6,6,6]
-        #1st cond yes, 2nd 
-        
+​
         #[0] or [0,0,0...], true answer = 0
         #[7], true answer = 1
         #[7,6], true answer = 2
         #[7,6,5,5], true answer = 4
-        
         #[2,2,4], true answer=2
         #[2,4,4,4] -> [4,4,4,2], true answer = 3
         
-        #answer must be in range of [0, len(citations)]
+#         #answer must be in range of [0, len(citations)]
+#         citations.sort(reverse=True)
+#         n = len(citations)
+#         #corner case
+#         if citations[-1] >= n:
+#             return n
         
-        citations.sort(reverse=True)
+#         for h in range(n-1, -1, -1):
+#             if citations[h-1] >= h and citations[h] <= h:
+#                 return h         
+       
+        #LeetCode solution
+        citations.sort()
+        i = 0
         n = len(citations)
-        #corner case
-        if citations[-1] >= n:
-            return n
-        
-        for h in range(n-1, -1, -1):
-            if citations[h-1] >= h and citations[h] <= h:
-                return h                        
+        while i < n and citations[n-1-i] > i:
+            i += 1
+        return i 
