e106c6c2a48cd9f5816716a450c7f529ccc5cf68
Karry Wong <ucdwong@ucdavis.edu>
2022-01-23 17:11:59 -0800

    55 ms

---
+++ solutions/274. H-Index.py
@@ -0,0 +1,34 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        #Mock interview with Haotian
+        #[5,5,3,1,0]
+        #sort in reversed order
+        #ind, citation in enumerate(), possible if 
+        # 1 cond: citation <= ind+1 (no of papers)
+        # 2 cond: citations[-citation] <= citation
+        
+        #[3,1,1]
+        # ind = 1, citation = 1, 1st cond yes, 2nd cond yes, return citation
+        
+        #[6,6,6,6,6,6]
+        #1st cond yes, 2nd 
+        
+        #[0] or [0,0,0...], true answer = 0
+        #[7], true answer = 1
+        #[7,6], true answer = 2
+        #[7,6,5,5], true answer = 4
+        
+        #[2,2,4], true answer=2
+        #[2,4,4,4] -> [4,4,4,2], true answer = 3
+        
+        #answer must be in range of [0, len(citations)]
+        
+        citations.sort(reverse=True)
+        n = len(citations)
+        #corner case
+        if citations[-1] >= n:
+            return n
+        
+        for h in range(n-1, -1, -1):
+            if citations[h-1] >= h and citations[h] <= h:
+                return h                        
