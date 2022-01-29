a2504940a5429aef8f1bfcec5549cba3bcf4ad07
Karry Wong <ucdwong@ucdavis.edu>
2022-01-29 11:24:51 -0800

    54 ms

--- solutions/333. Largest BST Subtree.py
+++ solutions/333. Largest BST Subtree.py
@@ -6,34 +6,48 @@
 #         self.right = right
 class Solution:
     def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
-        #Leetcode Inorder optimized, time O(N^2), space O(N)
-        def isValidBST(node) -> bool:
-            nonlocal previous
+#         #Leetcode Inorder optimized, time O(N^2), space O(N)
+#         def isValidBST(node) -> bool:
+#             nonlocal previous
+#             if not node:
+#                 return True
+#             if not isValidBST(node.left):
+#                 return False
+#             if node.val <= previous:
+#                 return False
+#             previous = node.val
+#             return isValidBST(node.right)
+        
+#         def countNodes(node) -> int:
+#             if not node:
+#                 return 0
+#             return 1 + countNodes(node.left) + countNodes(node.right)
+        
+#         if not root:
+#             return 0
+#         previous = float('-inf')
+#         if isValidBST(root):
+#             return countNodes(root)
+#         return max(self.largestBSTSubtree(root.left), self.largestBSTSubtree(root.right))
+        
+        #LeetCode post-order, time O(N), space O(N)
+        def helper(node) ->  (int, int, int): #output count, lo, hi
             if not node:
-                return True
-            if not isValidBST(node.left):
-                return False
-            if node.val <= previous:
-                return False
-            previous = node.val
-            return isValidBST(node.right)
-        
-        def countNodes(node):
-            if not node:
-                return 0
-            return 1 + countNodes(node.left) + countNodes(node.right)
-        
-        if not root:
-            return 0
-        previous = float('-inf')
-        if isValidBST(root):
-            return countNodes(root)
-        return max(self.largestBSTSubtree(root.left), self.largestBSTSubtree(root.right))
-        
+                return 0, float('inf'), float('-inf')
+            
+            left = helper(node.left)
+            right = helper(node.right)
+            
+            if left[2] < node.val < right[1]:
+                return 1+left[0]+right[0], min(node.val, left[1]), max(node.val, right[2])
+            
+            return max(left[0], right[0]), float('-inf'), float('inf')
+        return helper(root)[0]
+    
 #         #Mock interview, time O(N), space O(N)
 #         if not root: return 0
 #         ans = 1
-#         def helper(node) -> (int, int, int): #output count, lo, hi
+#         def helper(node) ->c
 #             nonlocal ans
 #             count, lo, hi = 1, node.val, node.val
             
