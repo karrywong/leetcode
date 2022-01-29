a7d28b641aff02c2c95fa96f0893d7d82a66ad15
Karry Wong <ucdwong@ucdavis.edu>
2022-01-29 12:03:17 -0800

    695 ms

--- solutions/111. Minimum Depth of Binary Tree.py
+++ solutions/111. Minimum Depth of Binary Tree.py
@@ -5,19 +5,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
-        ### Soln 0 - clever, from discussion by joubin
-        if root is None:
-            return 0
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        #LeetCode BFS:
+        if not root: return 0
+        queue = collections.deque([(root,1)])
+        while queue:
+            node, depth = queue.popleft()
+            children = [node.left, node.right]
+            if not any(children):
+                return depth
+            for c in children:
+                if c:
+                    queue.append((c,depth+1))
+                
+        # #LeetCode DFS, cleaner, time O(N), space O(N)
+        # if not root: return 0
+        # children = [root.left, root.right]
+        # if not any(children):
+        #     return 1
+        # return min([self.minDepth(c) for c in children if c]) + 1
         
-        if root.left is None and root.right is None:
-            return 1
-        
-        if root.left is None:
-            return self.minDepth(root.right) + 1
-        
-        if root.right is None:
-            return self.minDepth(root.left) + 1
-        
-        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
-        
+        # #First attempt, DFS, time O(N), space O(N) due to recursion
+        # if not root:
+        #     return 0
+        # elif not root.left and not root.right:
+        #     return 1
+        # elif root.left and not root.right:
+        #     return self.minDepth(root.left) + 1
+        # elif not root.left and root.right:
+        #     return self.minDepth(root.right) + 1
+        # else:
+        #     return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
