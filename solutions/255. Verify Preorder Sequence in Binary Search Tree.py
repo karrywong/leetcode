08eeb033a394f49912306300edf7412ec184d6c4
Karry Wong <ucdwong@ucdavis.edu>
2022-01-30 16:15:34 -0800

    363 ms

--- solutions/255. Verify Preorder Sequence in Binary Search Tree.py
+++ solutions/255. Verify Preorder Sequence in Binary Search Tree.py
@@ -1,32 +1,32 @@
 class Solution:
     def verifyPreorder(self, preorder: List[int]) -> bool:
         #Totally stucked, idea from muleax in discussion
-#         #Time O(N), Space O(N)
-#         lo = float('-inf')
-#         stack = []
-        
-#         for val in preorder:
-#             if val < lo:
-#                 return False
-#             while stack and stack[-1] < val:
-#                 lo = stack.pop()
-#             stack.append(val)
-#         return True
-​
-        #Optimized, time O(N), Space O(1)
+        #Time O(N), Space O(N)
         lo = float('-inf')
-        hi = float('-inf')
+        stack = []
         
-        for i, val in enumerate(preorder):
+        for val in preorder:
             if val < lo:
                 return False
-            
-            if val > hi: 
-                lo = hi
-                hi = val
-            else:
-                j = i-1
-                while j >= 0 and val > preorder[j]:
-                    lo = preorder[j]
-                    j -= 1
+            while stack and stack[-1] < val:
+                lo = stack.pop()
+            stack.append(val)
         return True
+​
+#         #Optimized, time O(N), Space O(1)
+#         lo = float('-inf')
+#         hi = float('-inf')
+        
+#         for i, val in enumerate(preorder):
+#             if val < lo:
+#                 return False
+            
+#             if val > hi: 
+#                 lo = hi
+#                 hi = val
+#             else:
+#                 j = i-1
+#                 while j >= 0 and val > preorder[j]:
+#                     lo = preorder[j]
+#                     j -= 1
+#         return True
