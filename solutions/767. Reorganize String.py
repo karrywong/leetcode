467e224036b7c7b63cbdc01091a7817f033a438d
Karry Wong <ucdwong@ucdavis.edu>
2022-01-31 14:51:46 -0800

    34 ms

--- solutions/767. Reorganize String.py
+++ solutions/767. Reorganize String.py
@@ -1,36 +1,24 @@
 class Solution:
-    def reorganizeString(self, S: str) -> str:
-        ### Soln 1 - Sort by count, LeetCode solution O(A(N+logA))
-#         N = len(S)
-#         A = []
-#         for c, x in sorted((S.count(x), x) for x in set(S)):
-#             if c > (N+1)/2: return ""
-#             A.extend(c * x)
-        
-#         ans = [None] * N
-#         ans[0::2], ans[1::2] = A[N//2:], A[:N//2]
-#         return "".join(ans)
-    
-        ### Soln 2 - greedy with heap, LeetCode solution
-        pq = [(-S.count(x), x) for x in set(S)]
-        heapq.heapify(pq)
-        if any(-nc > (len(S) + 1) / 2 for nc, x in pq):
-            return ""
-        
-        ans = []
-        while len(pq) >= 2:
-            nct1, ch1 = heapq.heappop(pq)
-            nct2, ch2 = heapq.heappop(pq)
-            #This code turns out to be superfluous, but explains what is happening
-            #if not ans or ch1 != ans[-1]:
-            #    ans.extend([ch1, ch2])
-            #else:
-            #    ans.extend([ch2, ch1])
-            ans.extend([ch1, ch2])
-            if nct1 + 1: heapq.heappush(pq, (nct1 + 1, ch1))
-            if nct2 + 1: heapq.heappush(pq, (nct2 + 1, ch2))
-            
-        return "".join(ans) + (pq[0][1] if pq else '')
-    
-    
-        ### Soln 3 - failed attempt, wrong data structure
+    def reorganizeString(self, s: str) -> str:
+        #First attempt, use heap, idea comes from hint "Alternate placing the most common letters."
+        count = collections.Counter(s)
+        maxHeap = [(-1*freq, char) for char, freq in count.items()]
+        heapq.heapify(maxHeap)
+        ans = ""
+        if len(s) % 2 == 0 and -1*maxHeap[0][0] >= len(s)//2 +1:
+            return ans
+        elif len(s) % 2 == 1 and -1*maxHeap[0][0] >= (len(s)+1)//2 + 1:
+            return ans
+        while maxHeap:
+            size = len(maxHeap)
+            freq, char = heapq.heappop(maxHeap)
+            if ans and ans[-1] == char:
+                freq2, char2 = heapq.heappop(maxHeap)
+                ans += char2 + char
+                if -1*freq2 > 1:
+                    heapq.heappush(maxHeap,(freq2+1, char2))
+            else:
+                ans += char
+            if -1*freq > 1:
+                heapq.heappush(maxHeap,(freq+1, char))
+        return ans
