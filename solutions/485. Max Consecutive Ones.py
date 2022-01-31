c4ea0298e3ee92dcd93de5d866dcb0ddc4dc3619
Karry Wong <ucdwong@ucdavis.edu>
2022-01-31 09:39:19 -0800

    355 ms

--- solutions/485. Max Consecutive Ones.py
+++ solutions/485. Max Consecutive Ones.py
@@ -1,5 +1,16 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        #recent attempt, time O(N), space O(1)
+        # i = -1
+        # ans = 0
+        # for j, num in enumerate(nums):
+        #     if num == 0:
+        #         i = j
+        #     else:
+        #         ans = max(ans, j-i)
+        # return ans
+    
+        #old attempt, time O(N), space O(1)
         res = 0
         count = 0        
         for n in nums:
@@ -9,5 +20,3 @@ class Solution:
                 res = max(res, count)
                 count = 0
         return max(res, count)
-            
-            
