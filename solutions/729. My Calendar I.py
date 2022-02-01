6431ae5d9919d86652474fe09f12ff251393080c
Karry Wong <ucdwong@ucdavis.edu>
2022-02-01 10:54:11 -0800

    307 ms

--- solutions/729. My Calendar I.py
+++ solutions/729. My Calendar I.py
@@ -1,17 +1,43 @@
 class MyCalendar:
-​
+    #Bisect + insert by otoc, amortized time O(logN), space O(N)
     def __init__(self):
-        self.lst = []
-​
-    def book(self, start: int, end: int) -> bool:
-        ### Soln 0 - slow, check self.lst every time
-        if self.lst:
-            for i in range(len(self.lst)):
-                if self.lst[i][0] <= start < self.lst[i][1] or start <= self.lst[i][0] < end:
-                    return False
-        self.lst.append([start, end])
+        self.starts = []
+        self.ends = []
+        
+    def book(self, start, end):
+        i = bisect.bisect_left(self.starts, start)
+        if i - 1 >= 0 and start < self.ends[i-1]:
+            return False
+        if i < len(self.starts) and end > self.starts[i]:
+            return False
+        self.starts.insert(i, start)
+        self.ends.insert(i, end)
         return True
-            
+    
+#     #First attempt, use heap, amortized time O(NlogN), space O(N)
+#     def __init__(self):
+#         self.events = []
+#         heapq.heapify(self.events)
+​
+#     def book(self, start: int, end: int) -> bool:
+#         if not self.events:
+#             heapq.heappush(self.events,(start,end))
+#             return True
+        
+#         stack = []
+#         while self.events and self.events[0][1] <= start:
+#             stack.append(heapq.heappop(self.events))
+        
+#         if self.events and self.events[0][0] < end :
+#             while stack:
+#                 heapq.heappush(self.events,stack.pop())
+#             return False
+#         else:
+#             heapq.heappush(self.events,(start,end))
+#             while stack:
+#                 heapq.heappush(self.events,stack.pop())
+#             return True
+​
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
