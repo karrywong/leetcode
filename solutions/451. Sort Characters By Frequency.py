e7ff741e5dfbf28d263a760655b884dd5730fe65
Karry Wong <ucdwong@ucdavis.edu>
2022-02-02 09:14:04 -0800

    65 ms

--- solutions/451. Sort Characters By Frequency.py
+++ solutions/451. Sort Characters By Frequency.py
@@ -1,17 +1,19 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         # #soln 0 - Hashtable, two-liner, Time O(NlogN), Space O(N)
-        # lib = collections.Counter(s)
-        # return ''.join([ k*v for k, v in sorted(lib.items(), key = lambda x : x[1], reverse = True)])
+#         lib = collections.Counter(s)
+#         return ''.join([ k*v for k, v in sorted(lib.items(), key = lambda x : x[1], reverse = True)])
         
-        #soln 1 - BucketSort, time O(N), space O(N)
-        lib = collections.Counter(s) #O(n)
-        max_freq = max(lib.values())
-        buckets = [[] for _ in range(max_freq+1)]
+        #Or use python's counter feature - most_common
+        lib = collections.Counter(s)
+        return ''.join([k*v for k, v in lib.most_common()])
         
-        for letter,freq in lib.items():
-            buckets[freq].append(letter)  #O(k)
+#         #soln 1 - BucketSort, time O(N), space O(N)
+#         lib = collections.Counter(s) #O(n)
+#         max_freq = max(lib.values())
+#         buckets = [[] for _ in range(max_freq+1)]
         
-        return "".join([freq*letter for freq in range(max_freq,0,-1) for letter in buckets[freq]])
-            
+#         for letter,freq in lib.items():
+#             buckets[freq].append(letter)  #O(k)
         
+#         return "".join([freq*letter for freq in range(max_freq,0,-1) for letter in buckets[freq]])
