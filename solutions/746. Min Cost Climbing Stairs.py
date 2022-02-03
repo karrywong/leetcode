211c5fb123d774413b12a55abc056edf04efc402
Karry Wong <ucdwong@ucdavis.edu>
2022-02-02 18:44:09 -0800

    52 ms

--- solutions/746. Min Cost Climbing Stairs.py
+++ solutions/746. Min Cost Climbing Stairs.py
@@ -1,7 +1,22 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        #soln 0 - dynamic programming
-        for i in range(2,len(cost)):
-            cost[i] += min(cost[i-2], cost[i-1])
-        return min(cost[-2], cost[-1])
+        #top-down DP, similalr to recursion w/ memoization
+        # @lru_cache(maxsize=None)
+        # def minimum_cost(i):
+        #     if i <= 1:
+        #         return 0
+        #     down_one = cost[i-1] + minimum_cost(i-1)
+        #     down_two = cost[i-2] + minimum_cost(i-2)
+        #     return min(down_one, down_two)
+        # return minimum_cost(len(cost))
         
+        #Alternative soln 0 - DP, time O(N), space O(1)
+        down_one = down_two = 0
+        for i in range(2, len(cost)+1):
+            down_one, down_two = min(down_one+cost[i-1], down_two+cost[i-2]), down_one
+        return down_one
+        
+        # #soln 0 - dynamic programming, time O(N), space O(1) in-place
+        # for i in range(2,len(cost)):
+        #     cost[i] += min(cost[i-2], cost[i-1])
+        # return min(cost[-2], cost[-1])
