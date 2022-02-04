7a1590bb930317efe09e5efe573167f4c2a7aee8
Karry Wong <ucdwong@ucdavis.edu>
2022-02-03 20:13:12 -0800

    67 ms

--- solutions/256. Paint House.py
+++ solutions/256. Paint House.py
@@ -3,9 +3,6 @@ class Solution:
         #DP soln, time O(len(costs)), space O(1)
         dp = costs[0]
         for i in range(1, len(costs)):
-            red, blue, green = dp
-            dp[0] = costs[i][0] + min(blue, green)
-            dp[1] = costs[i][1] + min(red, green)
-            dp[2] = costs[i][2] + min(red, blue)
+            dp[0], dp[1], dp[2] = costs[i][0] + min(dp[1], dp[2]), costs[i][1] + min(dp[0], dp[2]), costs[i][2] + min(dp[0], dp[1])
         return min(dp)
 ​
