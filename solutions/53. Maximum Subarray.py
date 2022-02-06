35984ece807000c512a889ddd9737e11c6dcfebd
Karry Wong <ucdwong@ucdavis.edu>
2022-02-05 17:12:23 -0800

    983 ms

--- solutions/53. Maximum Subarray.py
+++ solutions/53. Maximum Subarray.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # soln 3 - O(N), dynamic programming
+        # soln 3 - time O(N), dynamic programming
         for i in range(1, len(nums)):
             if nums[i-1] > 0:
                 nums[i] += nums[i-1]
@@ -34,3 +34,18 @@ class Solution:
 #             return max(combined, left_half, right_half)
         
 #         return findBestSubarray(nums, 0, len(nums)-1)
+    
+        #soln 1 - Kadane's algorithm, time O(N), space O(1)
+        #https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
+        # cur_sum = max_sum = nums[0]
+        # for i in range(1, len(nums)):
+        #     cur_sum = max(nums[i], cur_sum + nums[i])
+        #     max_sum = max(cur_sum, max_sum)
+        # return max_sum
+        
+        # #soln 0 - brute force O(N^2), time exceeded
+        # ans, n = nums[0], len(nums)
+        # for i in range(1,n+1):
+        #     for j in range(n-i+1):
+        #         ans = max(ans, sum(nums[j:i+j]))
+        # return ans
