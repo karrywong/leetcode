80ace9fa6751484f680320d9bcb4d66fd430096c
Karry Wong <ucdwong@ucdavis.edu>
2022-02-06 19:32:45 -0800

    548 ms

--- solutions/325. Maximum Size Subarray Sum Equals k.py
+++ solutions/325. Maximum Size Subarray Sum Equals k.py
@@ -1,14 +1,28 @@
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
-        #First attempt, prefix sum + hash map, Time O(N), Space O(N)
-        seen, ans = collections.defaultdict(list), 0
-        n, cur_sum = len(nums), 0
+        #Optimize first attempt, Time O(N), Space O(N)
+        seen, ans = {}, 0
+        cur_sum = 0
+        
         for i, num in enumerate(nums):
             cur_sum += num
             if cur_sum == k:
-                ans = max(ans, i+1)
-            for j in seen[cur_sum-k]:
-                ans = max(ans, i - j)
-            seen[cur_sum].append(i)
+                ans = i+1
+            if cur_sum - k in seen:
+                ans = max(ans, i-seen[cur_sum-k])
+            if cur_sum not in seen:
+                seen[cur_sum] = i
         return ans
+    
+        # #First attempt, prefix sum + hash map, Time O(N), Space O(N)
+        # seen, ans = collections.defaultdict(list), 0
+        # n, cur_sum = len(nums), 0
+        # for i, num in enumerate(nums):
+        #     cur_sum += num
+        #     if cur_sum == k:
+        #         ans = max(ans, i+1)
+        #     for j in seen[cur_sum-k]:
+        #         ans = max(ans, i - j)
+        #     seen[cur_sum].append(i)
+        # return ans
         
