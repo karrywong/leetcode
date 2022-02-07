a5d59833d312e195437a59669016ae5f216718b8
Karry Wong <ucdwong@ucdavis.edu>
2022-02-07 13:45:09 -0800

    2472 ms

--- solutions/1971. Find if Path Exists in Graph.py
+++ solutions/1971. Find if Path Exists in Graph.py
@@ -1,22 +1,34 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        #BFS approach     
         adjList = collections.defaultdict(set)
         for edge in edges:
             adjList[edge[0]].add(edge[1])
             adjList[edge[1]].add(edge[0])
             
-        dq = collections.deque([source])
-        seen = set()
+        # #BFS approach, time O(V+E)  
+        # dq = collections.deque([source])
+        # seen = set()
+        # while dq:
+        #     node = dq.popleft()
+        #     if node == destination:
+        #         return True
+        #     for nextNode in adjList[node]:
+        #         if nextNode not in seen:
+        #             seen.add(nextNode)
+        #             dq.append(nextNode)
+        # return False
         
-        while dq:
-            node = dq.popleft()
+        #DFS approach, time O(V+E)  
+        def dfs(node, seen):
             if node == destination:
                 return True
+            if node in seen:
+                return False
+            seen.add(node)
             for nextNode in adjList[node]:
-                if nextNode not in seen:
-                    seen.add(nextNode)
-                    dq.append(nextNode)
-        return False
-                    
-            
+                if dfs(nextNode, seen):
+                    return True
+            return False
+        
+        seen = set()
+        return dfs(source, seen) 
