5779ab0dc94fcc8623a0fe24b880e8898a5392f4
Karry Wong <ucdwong@ucdavis.edu>
2022-02-07 20:45:20 -0800

    2000 ms

--- solutions/309. Best Time to Buy and Sell Stock with Cooldown.py
+++ solutions/309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,21 +1,22 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ### Soln 0 - completely failed...
-        ### Soln 1 - LeetCode solution, really smart - state machine focused on "sold", "held", "reset"
-        #create list of profits
-        sold, held, reset = float('-inf'), float('-inf'), 0
-​
-        for price in prices:
-            # Alternative: the calculation is done in parallel.
-            # Therefore no need to keep temporary variables
-            #sold, held, reset = held + price, max(held, reset-price), max(reset, sold)
-​
-            pre_sold = sold
-            sold = held + price
-            held = max(held, reset - price)
-            reset = max(reset, pre_sold)
-​
-        return max(sold, reset)
+        # #LeetCode state machine
+        # #Time O(N), space O(1)
+        # sold, held, reset = float('-inf'), float('-inf'), 0
+        # for i in range(len(prices)):
+        #     sold, held, reset = held+prices[i], max(held, reset-prices[i]), max(reset, sold)
+        # return max(sold, reset)
         
-            
+        #LeetCode DP, time O(N^2), space O(N)
+        n = len(prices)
+        dp = [0]*(n+2)
         
+        for i in range(n-1, -1, -1):
+            buyNsell = 0 # Case 1). buy and sell the stock
+            for sell in range(i+1, n):
+                profit = (prices[sell] - prices[i]) + dp[sell+2]
+                buyNsell = max(profit, buyNsell)
+                
+            notBuy = dp[i+1] # Case 2). do no transaction with the stock prices[i]
+            dp[i] = max(buyNsell, notBuy)
+        return dp[0]
