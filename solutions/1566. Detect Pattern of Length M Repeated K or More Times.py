2a23e4d6b2d34b3633fc653c2310bc653599f407
Karry Wong <ucdwong@ucdavis.edu>
2022-02-08 10:19:54 -0800

    32 ms

--- solutions/1566. Detect Pattern of Length M Repeated K or More Times.py
+++ solutions/1566. Detect Pattern of Length M Repeated K or More Times.py
@@ -1,10 +1,8 @@
 class Solution:
     def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
         #First attempt, time O(N*m*k) where N = len(arr), space O(1)
-        for i in range(len(arr)-m):
+        for i in range(len(arr)-m*k+1):
             pattern = arr[i:i+m]
             if i + m*k <= len(arr) and pattern*k == arr[i:i+m*k]:
                 return True
         return False
-            
-            
