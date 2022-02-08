64d44a0139d3817c0cff5925697b355fe8669cb8
Karry Wong <ucdwong@ucdavis.edu>
2022-02-07 21:56:35 -0800

    239 ms

--- solutions/797. All Paths From Source to Target.py
+++ solutions/797. All Paths From Source to Target.py
@@ -1,21 +1,37 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        #Leetcode top-down DP
-        n = len(graph)
-        #recursion: allPathsToTarget(curNode) = curNode + allPathsToTarget(nextNode)
-        @lru_cache(maxsize=None)
-        def dp(cur): #compute all paths to target node
-            if cur == n-1:
-                return [[cur]]
+        #Recent attempt using BFS
+        path = [[0]]
+        dq = collections.deque(path)
+        ans = []
+        
+        while dq:
+            cur_path = dq.popleft()
+            if cur_path[-1] == len(graph)-1:
+                ans.append(cur_path[:])
+                continue
+            for node in graph[cur_path[-1]]:
+                temp_path = cur_path[:]
+                temp_path.append(node)
+                dq.append(temp_path)
+        return ans
+        
+#         #Leetcode top-down DP, time O(2^N*N), space O(2^N*N)
+#         n = len(graph)
+#         #recursion: allPathsToTarget(curNode) = curNode + allPathsToTarget(nextNode)
+#         @lru_cache(maxsize=None)
+#         def dp(cur): #compute all paths to target node
+#             if cur == n-1:
+#                 return [[cur]]
             
-            res = []
-            for ngh in graph[cur]:
-                for path in dp(ngh): #compute all paths to the next node of the target node
-                    res.append([cur]+path)
-            return res
-        return dp(0)
+#             res = []
+#             for ngh in graph[cur]:
+#                 for path in dp(ngh): #compute all paths to the next node of the target node
+#                     res.append([cur]+path)
+#             return res
+#         return dp(0)
 ​
-        # #Second attempt, slightly different backtracking
+        # #Second attempt, slightly different backtracking, time O(2^N*N), space O(2^N*N)
         # ans, n, A = [], len(graph), []
         # def dfs(val):
         #     A.append(val)
@@ -26,7 +42,7 @@ class Solution:
         # dfs(0)
         # return ans
     
-        # #First attempt, backtracking
+        # #First attempt, backtracking, time O(2^N*N), space O(2^N*N)
         # ans, n = [], len(graph)
         # def backtrack(val, A=[]):
         #     if val == n-1: 
@@ -37,4 +53,3 @@ class Solution:
         #         A.pop()
         # backtrack(0,[0])
         # return ans
-            
