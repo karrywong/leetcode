cbf6930df2767226734d3220ba12b5aa8cede136
Karry Wong <ucdwong@ucdavis.edu>
2022-02-09 17:06:06 -0800

    67 ms

--- solutions/139. Word Break.py
+++ solutions/139. Word Break.py
@@ -1,17 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        #soln 2 - DP, tricky idea
-        word_set = set(wordDict)
-        dp = [False] * (len(s) + 1)
-        dp[0] = True #null string
-        for i in range(1, len(s) + 1):
-            for j in range(i):
-                if dp[j] and s[j:i] in word_set:
-                    dp[i] = True
-                    break
-        return dp[len(s)]
+        # #soln 2 - DP, tricky idea, time O(N^3), space O(N)
+        # word_set = set(wordDict)
+        # dp = [False] * (len(s) + 1)
+        # dp[0] = True #null string
+        # for i in range(1, len(s) + 1):
+        #     for j in range(i):
+        #         if dp[j] and s[j:i] in word_set:
+        #             dp[i] = True
+        #             break
+        # return dp[len(s)]
         
-#         #soln 1 - recursion with memoization
+#         #soln 1 - recursion with memoization top-down, time O(N^3), space O(N)
 #         lib = {x : True for x in wordDict} #"xyz": True/False
 #         def helper(remainder):
 #             if remainder in lib:
@@ -21,8 +21,25 @@ class Solution:
 #                 if len(remainder) > len(word) and remainder.endswith(word):
 #                     x = remainder[:-len(word)]
 #                     lib[x] = helper(x)
-#                     if helper(x): return True
+#                     if helper(x): 
+#                         return True
 #             return False
         
 #         return helper(s)
                 
+        #soln 0 - second attempt, BFS intuitive 
+        n = len(s)
+        word_set = set(wordDict)
+        queue = collections.deque([0])
+        seen = set()
+        while queue: #BFS
+            start = queue.popleft()
+            if start == n:
+                return True
+            if start in seen:
+                continue
+            for end in range(start+1, n+1):
+                if s[start:end] in word_set:
+                    queue.append(end)
+            seen.add(start)
+        return False
