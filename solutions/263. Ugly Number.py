3c1f55d151aad05fa9c904c8fa9fb82170a5ecc5
Karry Wong <ucdwong@ucdavis.edu>
2022-02-11 17:58:29 -0800

    32 ms

--- solutions/263. Ugly Number.py
+++ solutions/263. Ugly Number.py
@@ -1,19 +1,35 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        #Recursion
+        # #Recursion
+        # if n <= 0:
+        #     return False
+        # elif n == 1:
+        #     return True
+        # else:
+        #     mod2 = n%2
+        #     mod3 = n%3
+        #     mod5 = n%5
+        #     if mod2 != 0 and mod3 != 0 and mod5 != 0:
+        #         return False
+        #     elif mod2 == 0:
+        #         return self.isUgly(n//2)
+        #     elif mod3 == 0:
+        #         return self.isUgly(n//3)
+        #     else:
+        #         return self.isUgly(n//5)
+        
+        #iterative
         if n <= 0:
             return False
-        elif n == 1:
+        if n == 1:
             return True
-        else:
-            mod2 = n%2
-            mod3 = n%3
-            mod5 = n%5
-            if mod2 != 0 and mod3 != 0 and mod5 != 0:
-                return False
-            elif mod2 == 0:
-                return self.isUgly(n//2)
-            elif mod3 == 0:
-                return self.isUgly(n//3)
+        while n > 1:
+            if n%2 == 0:
+                n //= 2
+            elif n%3 == 0:
+                n //= 3
+            elif n%5 == 0:
+                n //= 5
             else:
-                return self.isUgly(n//5)
+                return False
+        return True
