4b888de7886939a94affd0fb64af1439cffa10b9
Karry Wong <ucdwong@ucdavis.edu>
2022-02-12 21:55:39 -0800

    132 ms

--- solutions/323. Number of Connected Components in an Undirected Graph.py
+++ solutions/323. Number of Connected Components in an Undirected Graph.py
@@ -1,59 +1,53 @@
-class UnionFind:
-    def __init__(self, n: int):
-        self.parent = [i for i in range(n)]
-        self.rank = [1]*n #size of each set
-        self.count = n
+# class UnionFind:
+#     def __init__(self, n: int):
+#         self.parent = [i for i in range(n)]
+#         self.rank = [1]*n #size of each set
+#         self.count = n
         
-    def find(self, A: int) -> int: #Path Compression
-        if A == self.parent[A]:
-            return A
-        self.parent[A] = self.find(self.parent[A])
-        return self.parent[A]
+#     def find(self, A: int) -> int: #Path Compression
+#         if A == self.parent[A]:
+#             return A
+#         self.parent[A] = self.find(self.parent[A])
+#         return self.parent[A]
     
-    def union(self, A, B):
-        rootA = self.find(A)
-        rootB = self.find(B)
-        if rootA != rootB:
-            if self.rank[rootA] >= self.rank[rootB]:
-                self.parent[rootA] = rootB
-                self.rank[rootA] += self.rank[rootB]
-            else:
-                self.parent[rootB] = rootA
-                self.rank[rootB] += self.rank[rootA]
-            self.count -= 1
+#     def union(self, A, B):
+#         rootA = self.find(A)
+#         rootB = self.find(B)
+#         if rootA != rootB:
+#             if self.rank[rootA] >= self.rank[rootB]:
+#                 self.parent[rootA] = rootB
+#                 self.rank[rootA] += self.rank[rootB]
+#             else:
+#                 self.parent[rootB] = rootA
+#                 self.rank[rootB] += self.rank[rootA]
+#             self.count -= 1
             
-    def getCount(self):
-        return self.count
+#     def getCount(self):
+#         return self.count
 ​
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        unionFind = UnionFind(n)
-        for A, B in edges:
-            unionFind.union(A,B)
-        return unionFind.getCount()
+        # unionFind = UnionFind(n)
+        # for A, B in edges:
+        #     unionFind.union(A,B)
+        # return unionFind.getCount()
         
-        # #TBRV, soln by Zitao
-        # dic = {}
-        # for e in edges:
-        #     if e[0] not in dic:
-        #         dic[e[0]] = [e[1]]
-        #     else:
-        #         dic[e[0]].append(e[1])
-        #     if e[1] not in dic:
-        #         dic[e[1]] = [e[0]]
-        #     else:
-        #         dic[e[1]].append(e[0])
-        # seen = [0]*n
-        # count = 0
-        # for i in range(n):
-        #     if seen[i] == 0:
-        #         count += 1
-        #         stack = [i]
-        #         while stack:
-        #             u = stack.pop()
-        #             seen[u] = 1
-        #             if u in dic:
-        #                 for neighbor in dic[u]:
-        #                     if seen[neighbor] == 0:
-        #                         stack.append(neighbor)
-        # return count
+        graph = collections.defaultdict(list)
+        for v1, v2 in edges:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+        
+        count = 0
+        seen = set()
+        for i in range(n):
+            if i not in seen:
+                stack = [i]
+                while stack:
+                    node = stack.pop()
+                    seen.add(node)
+                    for nextNode in graph[node]:
+                        if nextNode not in seen:
+                            stack.append(nextNode)
+                
+                count += 1
+        return count
