fcd18e908f5f8a5f8895c8950e20e73a941e556d
Karry Wong <ucdwong@ucdavis.edu>
2022-02-17 20:35:34 -0800

    364 ms

---
+++ solutions/1231. Divide Chocolate.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        #Almost identical to problems 1011. Capacity To Ship Packages Within D Days
+        #and 1891. Cutting Ribbons
+        #Time O(len(sweetness)* O(log(sum(sweetness)//(k+1)))), space O(1)
+        def isPossible(val:int) -> bool:
+            count = 0
+            cur_sum = 0
+            i = 0
+            for sweet in sweetness:
+                cur_sum += sweet
+                if cur_sum >= val:
+                    count += 1
+                    cur_sum = 0
+            return count > k
+                
+        l, r = min(sweetness), sum(sweetness) // (k+1) + 1
+        while l < r:
+            mid = l + (r-l)//2
+            if isPossible(mid):
+                l = mid + 1
+            else:
+                r = mid
+        return l - 1
