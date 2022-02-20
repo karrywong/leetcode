d44d080e4c14c14e08eb4f807ddc68fdfe9a82cb
Karry Wong <ucdwong@ucdavis.edu>
2022-02-19 16:48:57 -0800

    32 ms

--- solutions/392. Is Subsequence.py
+++ solutions/392. Is Subsequence.py
@@ -1,11 +1,21 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s: return True
-        lst = list(s[::-1])
+#         #soln 2
+#         if not s: return True
+#         lst = list(s[::-1])
         
-        for i in range(len(t)):
-            if t[i] == lst[-1]:
-                lst.pop()
-            if not lst: return True
-                
+#         for e in t:
+#             if e == lst[-1]:
+#                 lst.pop()
+#             if not lst: return True
+#         return False
+        
+        #soln 1
+        i = 0
+        if not s: return True
+        for e in t:
+            if s[i] == e:
+                i += 1
+                if i == len(s): return True
         return False
+        
