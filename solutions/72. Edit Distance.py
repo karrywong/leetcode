b9f2ab0fd83d0f7ea29c14ed9f6b8a91111f06c9
Karry Wong <ucdwong@ucdavis.edu>
2022-02-19 19:59:58 -0800

    247 ms

--- solutions/72. Edit Distance.py
+++ solutions/72. Edit Distance.py
@@ -2,6 +2,7 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         #Totally stuck, Leetcode soln, Time O(MN), Space O(MN)
         #Remotely similar problems are 583. Delete Operation for Two Strings, 1143. Longest Common Subsequence
+        #Also try 161. One Edit Distance
         word1, word2 = '#'+word1, '#'+word2
         m, n = len(word1), len(word2)
         dp = [[0] * n for _ in range(m)]
@@ -16,5 +17,3 @@ class Solution:
                 else:
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
         return dp[-1][-1]
-        
-        
