4c0a4c1c372db7a21270e04f67c033ffd65c3273
Karry Wong <ucdwong@ucdavis.edu>
2022-02-20 18:02:37 -0800

    2326 ms

--- solutions/322. Coin Change.py
+++ solutions/322. Coin Change.py
@@ -1,32 +1,34 @@
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int: 
-        # leetcode - recursive top down
-        if amount < 1: return 0
-        count = [0]*(amount+1)
+    def coinChange(self, coins: List[int], amount: int) -> int:
+#         #Leetcode DP - recursive top down w memoization
+#         #Time O(amount*len(coins)), space O(amount)
+#         if amount < 1: return 0
+#         self.count = [0]*(amount+1)
         
-        def helper(coins, rem, count):
-            if rem < 0: return -1
-            if rem == 0: return 0
-            if count[rem]: return count[rem]
+#         def helper(coins, rem):
+#             if rem < 0: return -1
+#             if rem == 0: return 0
+#             if self.count[rem]: return self.count[rem]
 ​
-            mval = float('inf')
-            for c in coins:
-                res = helper(coins, rem-c, count);
-                if res >= 0 and res < mval:
-                    mval = 1 + res
-            count[rem] = -1 if mval == float('inf') else mval
-            return count[rem]
-        
-        ans = helper(coins, amount, count)
-        #print(count)
-        return ans
-        
-        
+#             mval = float('inf')
+#             for c in coins:
+#                 res = helper(coins, rem-c);
+#                 if res >= 0 and res < mval:
+#                     mval = 1 + res
+#             self.count[rem] = -1 if mval == float('inf') else mval
+#             return self.count[rem]
+    
+#         ans = helper(coins, amount)
+#         return ans
         
         # my solution - replicate of soln for prob #279 Perfect Squares
+        #Time O(amount*len(coins)), space O(amount)
         coins.sort()
         dp = [float('inf')]*(amount+1)
         #bottom case:
         dp[0] = 0
         for i in range(1, amount+1):
             for c in coins:
+                if i < c: break
+                dp[i] = min(dp[i], dp[i-c]+1)
+        return dp[-1] if dp[-1] != float('inf') else -1
