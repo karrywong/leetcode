608b243e5f8f97c18fcfa6efef3d9f2ccc554aad
Karry Wong <ucdwong@ucdavis.edu>
2022-02-20 21:25:32 -0800

    76 ms

--- solutions/332. Reconstruct Itinerary.py
+++ solutions/332. Reconstruct Itinerary.py
@@ -2,13 +2,9 @@ class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         #Almost identical but easier than 2097. Valid Arrangement of Pairs
         #Hierholzer's algorithm for Eulerian path
-        graph = collections.defaultdict(list)
-        degree = collections.defaultdict(int)
-        
+        graph = collections.defaultdict(list)        
         for s, e in tickets:
             graph[s].append(e)
-            degree[s] += 1
-            degree[e] -= 1
                 
         for k in graph:
             graph[k] = sorted(graph[k], reverse = True)
