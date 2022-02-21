4fd0bb0915e79fcfb8e4411911272d040ba248a8
Karry Wong <ucdwong@ucdavis.edu>
2022-02-21 11:29:31 -0800

    1178 ms

--- solutions/1584. Min Cost to Connect All Points.py
+++ solutions/1584. Min Cost to Connect All Points.py
@@ -20,26 +20,50 @@ class UnionFind:
 ​
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        #Minimum Spanning tree, Kruskal algorithm using union-find, time O(ElogE) due to sorting
-        lookup = {i:(x,y) for i, (x,y) in enumerate(points)}
-        edge_weights = []
-        n = len(points)
-        for i in range(n):
-            for j in range(i):
-                dist = abs(lookup[i][0]-lookup[j][0]) + abs(lookup[i][1]-lookup[j][1])
-                edge_weights.append((dist, i, j))
-        edge_weights.sort(reverse=True)
-        count = n-1
-        ans = 0
-        uf = UnionFind(n)
+#         #Soln 1 - MST, Kruskal algorithm using union-find, time O(ElogV) due to sorting
+#         edge_weights = []
+#         n = len(points)
+#         for i in range(n):
+#             for j in range(i):
+#                 dist = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1])
+#                 edge_weights.append((dist, i, j))
+#         edge_weights.sort(reverse=True)
+#         count = n-1
+#         ans = 0
+#         uf = UnionFind(n)
         
-        while count:
-            edge_weight, u, v = edge_weights.pop()
-            rootu = uf.find(u)
-            rootv = uf.find(v)
-            if rootu != rootv:
-                uf.union(rootu,rootv)
-                ans += edge_weight 
+#         while edge_weights and count:
+#             edge_weight, u, v = edge_weights.pop()
+#             rootu = uf.find(u)
+#             rootv = uf.find(v)
+#             if rootu != rootv:
+#                 uf.union(rootu,rootv)
+#                 ans += edge_weight 
+#                 count -= 1
+#         return ans
+​
+        #Soln 2 - MST, Prim's algorithm using heap, time O(ElogV)
+        n = len(points)
+        hp = []
+        visited = set()
+        x1, y1 = points[0]
+        for i in range(1, n):
+            dist = abs(points[i][0] - x1) + abs(points[i][1]-y1)
+            hp.append((dist, 0, i))
+        heapq.heapify(hp)
+        visited.add(0)
+        
+        ans = 0
+        count = n-1
+        while hp and count:
+            cost, u, v = heapq.heappop(hp)
+            if v not in visited:
+                x1, y1 = points[v]
+                ans += cost
+                visited.add(v)
+                for i in range(n):
+                    if i not in visited:
+                        dist = abs(points[i][0] - x1) + abs(points[i][1]-y1)
+                        heapq.heappush(hp, (dist, v, i))
                 count -= 1
-                
         return ans
