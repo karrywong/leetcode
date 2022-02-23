40bc12e3bdbfead192fcc77e5f6fa2cd8a2cd3fe
Karry Wong <ucdwong@ucdavis.edu>
2022-02-22 16:42:09 -0800

    55 ms

--- solutions/70. Climbing Stairs.py
+++ solutions/70. Climbing Stairs.py
@@ -1,21 +1,25 @@
 class Solution:
-    global memo
-    memo = collections.defaultdict(int,{1:1, 2:2})
+    # def __init__(self):
+    #     self.memo = collections.defaultdict(int,{1:1, 2:2})
+    @lru_cache(maxsize = None)
     def climbStairs(self, n: int) -> int:
-        # #soln 1 - recursion w memoization
-        # if n == 1:
-        #     return 1
-        # if n == 2:
-        #     return 2
-        # if memo[n] > 0:
-        #     return memo[n]
-        # memo[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
-        # return memo[n]
+        # # #soln 1 - recursion w memoization
+        # if n <= 2:
+        #     return n
+        # if self.memo[n] > 0:
+        #     return self.memo[n]
+        # self.memo[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
+        # return self.memo[n]
+    
+        # #soln 2 - Fibonacci formula
+        # sqrt5 = sqrt(5)
+        # m = n+1
+        # return int(0.5**m * ((1+sqrt5)**m - (1-sqrt5)**m) / sqrt5)
         
-        #soln 2 - Fibonacci formula
-        sqrt5 = sqrt(5)
-        m = n+1
-        return int(0.5**m * ((1+sqrt5)**m - (1-sqrt5)**m) / sqrt5)
+        #soln 3 - recursion w cache
+        if n <= 2:
+            return n
+        return self.climbStairs(n-1)+self.climbStairs(n-2)
         
         # #soln 0 - dynamic programming
         # if n == 1:
