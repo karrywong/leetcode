1af4e5c328994fb52087afc9ecc2a6930cad0e1d
Karry Wong <ucdwong@ucdavis.edu>
2022-02-26 11:30:35 -0800

    44 ms

--- solutions/990. Satisfiability of Equality Equations.py
+++ solutions/990. Satisfiability of Equality Equations.py
@@ -1,43 +1,52 @@
-class UnionFind:
-    def __init__(self):
-        self.root = [i for i in range(26)]
-        self.rank = [0]*26
-        
-    def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-    
-    def union(self, x, y):
-        rootx = self.find(x)
-        rooty = self.find(y)
-        if rootx != rooty:
-            if self.rank[rootx] < self.rank[rooty]:
-                self.root[rootx] = rooty
-            elif self.rank[rootx] > self.rank[rooty]:
-                self.root[rooty] = rootx
-            else:
-                self.root[rootx] = rooty
-                self.rank[rootx] += 1
         
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        #First attempt using Union Find, time O(N) w/ N = len(equations), space O(1)
-        #loop over equations and process only equal, call union(a,b)
-        #loop over equations and process only unequal, call find, if find(a) == find(b): return False
+#         #First attempt using Union Find, time O(len(equations)), space O(1)
+#         #loop over equations and process only equal, call union(a,b)
+#         #loop over equations and process only unequal, call find, if find(a) == find(b): return False
         
-        uf = UnionFind()
+#         uf = UnionFind()
+#         for equation in equations:
+#             if equation[1] == "=":
+#                 u = ord(equation[0]) - ord('a')
+#                 v = ord(equation[3]) - ord('a')
+#                 uf.union(u,v)
+        
+#         for equation in equations:
+#             if equation[1] == "!":        
+#                 u = ord(equation[0]) - ord('a')
+#                 v = ord(equation[3]) - ord('a')
+#                 if uf.find(u) == uf.find(v):
+#                     return False
+#         return True
+​
+        #Second attempt using DFS, time O(len(equations)), space O(len(equations))
+        graph = collections.defaultdict(set)
         for equation in equations:
             if equation[1] == "=":
                 u = ord(equation[0]) - ord('a')
                 v = ord(equation[3]) - ord('a')
-                uf.union(u,v)
+                graph[u].add(v)
+                graph[v].add(u)
+        
+        colors = [None] * 26
+        cnt = 0
+        
+        def dfs(node: int, cnt: int):
+            colors[node] = cnt
+            for ngh in graph[node]:
+                if colors[ngh] is None:
+                    dfs(ngh, cnt)
+        
+        for i in range(26):
+            if colors[i] is None:
+                dfs(i, cnt)
+                cnt += 1
         
         for equation in equations:
-            if equation[1] == "!":        
+            if equation[1] == "!":
                 u = ord(equation[0]) - ord('a')
                 v = ord(equation[3]) - ord('a')
-                if uf.find(u) == uf.find(v):
+                if colors[u] == colors[v]:
                     return False
         return True
