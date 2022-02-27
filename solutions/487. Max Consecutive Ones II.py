e9a47e7eccc5a16e06b3fc0b2ddd626663a7e241
Karry Wong <ucdwong@ucdavis.edu>
2022-02-26 16:47:19 -0800

    806 ms

--- solutions/487. Max Consecutive Ones II.py
+++ solutions/487. Max Consecutive Ones II.py
@@ -39,3 +39,26 @@ class Solution:
 #         return max(ans, count1+count2+1)
     
         ### Old attempts
+        ### Soln 1 - brute force O(n^2), time exceeded
+        # res = 0
+        # for i in range(len(nums)):
+        #     count = 0
+        #     for j in range(i, len(nums)):
+        #         if count == 2:
+        #             break
+        #         if nums[j] == 0:
+        #             count += 1
+        #         if count <= 1: 
+        #             res = max(res, j - i + 1)
+        # return res
+​
+#         ### Soln 2 - fast and elegant solution from discussion by redSand
+#         k = result = 0
+#         prev_zero_seen = -1
+#         for i, num in enumerate(nums):
+#             if num == 0:
+#                 result = max(result, i - k)
+#                 k = prev_zero_seen + 1
+#                 prev_zero_seen = i
+        
+#         return max(result, len(nums) - k)
