8cb00d5beab0f0f1848c7626f949dddf69983b3f
Karry Wong <ucdwong@ucdavis.edu>
2022-02-26 16:51:18 -0800

    2374 ms

--- solutions/2149. Rearrange Array Elements by Sign.py
+++ solutions/2149. Rearrange Array Elements by Sign.py
@@ -1,16 +1,13 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         #Weekly contest
-        neg_num = []
-        pos_num = []
+        neg_num, pos_num = [], []
         for num in nums:
             if num < 0:
                 neg_num.append(num)
             else:
                 pos_num.append(num)
-        
-        ans = []
+        ans = []        
         for pos, neg in zip(pos_num, neg_num):
             ans += [pos, neg]
-        
         return ans
