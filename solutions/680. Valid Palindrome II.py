207d5f3744ce7847b7b2e7e90fe323b99a018e98
Karry Wong <ucdwong@ucdavis.edu>
2022-02-27 16:50:01 -0800

    125 ms

--- solutions/680. Valid Palindrome II.py
+++ solutions/680. Valid Palindrome II.py
@@ -1,28 +1,11 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        ### Soln 3 Greedy algorithm, simpler from discussion by MAaRNG
-        # if s == s[::-1]:
-        #     return True
-        # for i in range(len(s)-1):
-        #     if s[i] != s[-i-1]:
-        #         return s[i: -i-2] == s[i+1: -i-1][::-1] or s[i+1: -i-1] == s[i+2: len(s)-i][::-1]
-        # return False     
-        
-#         ### Soln 2 Greedy algorithm, LeetCode solution
-        # def is_pali_range(i,j):
-        #     return all(s[k] == s[j-k+i] for k in range(i,j))
-​
-        for i in range(len(s)//2):
-            if s[i] != s[~i]:
-                j = len(s) - 1 - i
-                # return is_pali_range(i+1,j) or is_pali_range(i, j-1)
-                return s[i+1:j+1] == s[i+1:j+1][::-1] or s[i:j] == s[i:j][::-1]
+        #Time O(N), space O(1)
+        l, r = 0, len(s)-1 #left & right pointers
+        while l < r:
+            if s[l] == s[r]:
+                l += 1 
+                r -= 1
+            else: 
+                return s[l+1:r+1] == s[l+1:r+1][::-1] or s[l:r] == s[l:r][::-1]
         return True
-        
-        ### Soln 1 - brute force, time exceeded
-        # for i in range(len(s)):
-        #     temp = [l for j, l in enumerate(s) if j != i]
-        #     if temp == temp[::-1]: 
-        #         return True
-        # return False
-        
