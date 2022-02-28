bee893098be3613b0ba6f7120120ff71ed8f6aed
Karry Wong <ucdwong@ucdavis.edu>
2022-02-28 09:21:07 -0800

    1256 ms

---
+++ solutions/1746. Maximum Subarray Sum After One Operation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSumAfterOperation(self, nums: List[int]) -> int:
+        #First attempt using DP, time O(N), space (1)
+        #dp = [0] * len(nums)
+        cur_sum = nums[0]
+        cur_sq = dp = nums[0] * nums[0]
+        
+        for i in range(1, len(nums)):
+            val = nums[i] * nums[i]
+            #update cur_sq - either perform square or not
+            cur_sq = max(val, cur_sum+val, cur_sq+nums[i])
+            #update cur_sum from Kadane's algorithm
+            cur_sum = max(nums[i], cur_sum+nums[i])            
+            dp = max(dp, cur_sq)
+            
+        return dp
