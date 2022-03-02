917c388720c64392bcbe37aa8a5a9b7722d1dc01
Karry Wong <ucdwong@ucdavis.edu>
2022-03-01 22:49:32 -0800

    77 ms

--- solutions/1114. Print in Order.py
+++ solutions/1114. Print in Order.py
@@ -3,25 +3,24 @@ from threading import Lock
 #Recently learned concurrent computing and concepts, i.e. race conditions, deadlocks, and resource starvation. Idea is to set critical sections to ensure partial order
 class Foo:
     def __init__(self):
-        self.firstJobDone = Lock()
-        self.secondJobDone = Lock()
-        self.firstJobDone.acquire()
-        self.secondJobDone.acquire()
+        self.locks = (Lock(),Lock())
+        self.locks[0].acquire()
+        self.locks[1].acquire()
 ​
     def first(self, printFirst: 'Callable[[], None]') -> None:
         # printFirst() outputs "first". Do not change or remove this line.
         printFirst()
-        self.firstJobDone.release()
+        self.locks[0].release()
 ​
 ​
     def second(self, printSecond: 'Callable[[], None]') -> None:
-        with self.firstJobDone:
+        with self.locks[0]:
             # printSecond() outputs "second". Do not change or remove this line.
             printSecond()
-        self.secondJobDone.release()
+            self.locks[1].release()
 ​
 ​
     def third(self, printThird: 'Callable[[], None]') -> None:
-        with self.secondJobDone:
+        with self.locks[1]:
             # printThird() outputs "third". Do not change or remove this line.
             printThird()
