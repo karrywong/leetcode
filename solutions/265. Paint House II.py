9a84ca98035fc4eab7ce2b88ef28c7b126c035db
Karry Wong <ucdwong@ucdavis.edu>
2022-03-12 12:30:09 -0800

    344 ms

--- solutions/265. Paint House II.py
+++ solutions/265. Paint House II.py
@@ -1,7 +1,30 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
+#         #Almost identical to 256. Paint House
+#         #Top-down DP with recursion w memo, time O(n*k^2), space O(n)
+#         memo = {} #(house, color)
+#         def paint_house(house:int, color:int) -> int:
+#             if (house, color) in memo:
+#                 return memo[(house, color)]
+            
+#             if house == len(costs) - 1:
+#                 return costs[house][color]
+            
+#             temp = float('inf')
+#             for j in range(len(costs[0])):
+#                 if j != color:
+#                     temp = min(temp, paint_house(house+1, j))
+#             memo[(house, color)] = costs[house][color] + temp
+#             return memo[(house, color)]
+        
+#         ans = float('inf')
+#         for j in range(len(costs[0])):
+#             ans = min(ans, paint_house(0,j))
+#         return ans
+        
         #DP with optimized space, dp[i][j]: minimal cost of painting the ith house with jth color
-        #Time: O(n*k), space: O(1)
+        #Time: O(n*k), space: O(k)
+        #Follow-up: can be further optimized to Time: O(n*k), space O(1)
         dp_cur = costs[0]
         for i in range(1,len(costs)):
             dp_prev = dp_cur[:]
