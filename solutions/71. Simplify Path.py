15ec346f9ed5d21b794bb902198f4a1761209d1f
Karry Wong <ucdwong@ucdavis.edu>
2022-03-12 13:30:59 -0800

    33 ms

---
+++ solutions/71. Simplify Path.py
@@ -0,0 +1,38 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        #LeetCode soln, clean, time O(N), space O(N)
+        stack = []
+        for portion in path.split("/"):
+            if portion == "..":
+                if stack:
+                    stack.pop()
+            elif portion == '.' or not portion:
+                continue
+            else:
+                stack.append(portion)
+        return "/" + "/".join(stack)
+        
+#         #First attempt, code messy, time O(N), space O(N)
+#         stack = ['/']
+#         i = 0
+        
+#         while i < len(path):
+#             if path[i] == '/':
+#                 if stack and stack[-1] != '/':
+#                     stack.append(path[i])
+#             elif path[i] == "." and stack[-1] == "/":
+#                 count = 1
+#                 while i+1 < len(path) and path[i+1] == ".":
+#                     count += 1
+#                     i += 1
+#                 if (i+1 < len(path) and path[i+1] != "/") or count >= 3:
+#                     stack.append("."*count)
+#                 elif count == 2:
+#                     if len(stack) > 1 and stack[-1] == "/":
+#                         stack.pop()
+#                         while stack and stack[-1] != '/':
+#                             stack.pop()
+#             else:
+#                 stack.append(path[i])
+#             i += 1
+#         return ''.join(stack[:-1]) if len(stack) > 1 and stack[-1] == '/' else ''.join(stack)
