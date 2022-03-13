c7a162eca3a368c29bc0b479c613db28430f68f1
Karry Wong <ucdwong@ucdavis.edu>
2022-03-13 16:32:43 -0700

    100 ms

--- solutions/387. First Unique Character in a String.py
+++ solutions/387. First Unique Character in a String.py
@@ -1,22 +1,20 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        # leetcode soln
-        count = collections.Counter(s)
-        for idx, ch in enumerate(s):
-            if count[ch] == 1:
-                return idx  
+        #two sweeps, first, arr[char] += 1
+        #second sweep return first non-repeating
+        #Time O(N), space O(1)
+        arr = [0] * 26
+        a_ascii = ord('a')
+        for char in s:
+            arr[ord(char)-a_ascii] += 1
+        for i, char in enumerate(s):
+            if arr[ord(char)-a_ascii] == 1:
+                return i
         return -1
     
-        # soln 1
-        seen = {}
-        for i, e in enumerate(s):
-            if e in seen:
-                seen[e] = float('inf')
-            else:
-                seen[e] = i  
-        ans = min(seen.values())
-        return ans if ans != float('inf') else -1
-            
-                
-        
-​
+        # #Old attempt 
+        # count = collections.Counter(s)
+        # for idx, ch in enumerate(s):
+        #     if count[ch] == 1:
+        #         return idx  
+        # return -1        
