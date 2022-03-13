41ed48c033316fb11c4382bfa7e50ade342b64b5
Karry Wong <ucdwong@ucdavis.edu>
2022-03-13 14:30:32 -0700

    96 ms

---
+++ solutions/1239. Maximum Length of a Concatenated String with Unique Characters.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        #Time: O(2**N), n = length of all strings with unique character
+        #arr2 = ["abcd", "efg", "hij", "kl", "m"]
+        lookup = {}
+        for string in arr:
+            checker = 0
+            for char in string:
+                val = 1 << (ord(char) - ord('a'))
+                if checker & val != 0:
+                    break
+                checker |= val
+            else: #string consists of unique characters
+                lookup[string] = checker
+        
+        arr2 = sorted(lookup.keys(),reverse = True, key = len) #at most arr
+        ans = 0 
+        
+        def dfs(ind, S):
+            nonlocal ans
+            ans = max(ans, len(S))
+            
+            for i in range(ind+1, len(arr2)):
+                if lookup[S] & lookup[arr2[i]] == 0:
+                    lookup[S+arr2[i]] = lookup[S] | lookup[arr2[i]]
+                    dfs(i, S+arr2[i])
+            
+        for i, string in enumerate(arr2): #O(n)
+            dfs(i, string) #O(n)
+        return ans
