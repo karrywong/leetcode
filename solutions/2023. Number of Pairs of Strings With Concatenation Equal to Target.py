7b1b3e12ad34183dd4a664adaf5eabe71f1b40ff
Karry Wong <ucdwong@ucdavis.edu>
2022-03-24 18:08:58 -0700

    40 ms

--- solutions/2023. Number of Pairs of Strings With Concatenation Equal to Target.py
+++ solutions/2023. Number of Pairs of Strings With Concatenation Equal to Target.py
@@ -1,24 +1,36 @@
 class Solution:
     def numOfPairs(self, nums: List[str], target: str) -> int:
         #Brute force, time O(N^2 * len(target))
-        
+        #First attempt, time O(N * len(target)), space O(N)
         #1st sweep:construct complements in htb
         #htb = {"7":1, "777":1, "77":2} 
         #2nd sweep
         #Counter = {"777":1, "7":1, "77":2} #O(N)
-        htb = collections.defaultdict(int)
-        for num in nums:
-            if target.startswith(num):
-                htb[target[len(num):]] += 1
+#         htb = collections.defaultdict(int)
+#         for num in nums:
+#             if target.startswith(num):
+#                 htb[target[len(num):]] += 1
         
-        counter = collections.Counter(nums)
+#         counter = collections.Counter(nums)
+#         ans = 0
+#         for num in counter.keys():
+#             if num in htb:
+#                 if num * 2 == target: 
+#                     ans += counter[num] * (counter[num] - 1)
+#                 else:
+#                     ans += htb[num] * counter[num]
+#         return ans
+    
+        d = collections.defaultdict(int)
+        for i in nums:
+            d[i] += 1
+       
         ans = 0
-        for num in counter.keys():
-            if num in htb:
-                if num * 2 == target: 
-                    ans += counter[num] * (counter[num] - 1)
-                else:
-                    ans += htb[num] * counter[num]
+        for k, v in d.items():
+            if target.startswith(k) and target[len(k):] in d:
+                r = target[len(k):]
+                ans += (d[k] * d[k] - d[k]) if k == r else (d[k] * d[r])
+               
         return ans
         
         #nums = {"1", "1", "1", "2", "2"}, target = "12"
