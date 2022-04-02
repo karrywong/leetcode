edac565d4e487e45dfcf20ee2123b7dc5c83d184
Karry Wong <ucdwong@ucdavis.edu>
2022-04-02 16:52:50 -0700

    32 ms

--- solutions/1790. Check if One String Swap Can Make Strings Equal.py
+++ solutions/1790. Check if One String Swap Can Make Strings Equal.py
@@ -1,22 +1,23 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        if len(s1) != len(s2): return False
-        letters1 = collections.Counter(s1)
-        letters2 = collections.Counter(s2)
-        if len(letters1) != len(letters2): return False
+        #Two-liner by ye15, time O(N), space O(1)
+        diff = [[x, y] for x, y in zip(s1, s2) if x != y]
+        return not diff or len(diff) == 2 and diff[0][::-1] == diff[1]
         
-        once = False
-        swap = []
-        for char1, char2 in zip(s1, s2):
-            if char1 != char2:
-                if once: return False
-                if not swap:
-                    swap.extend([char1,char2])
-                elif swap[0] != char2 or swap[1] != char1:
-                    return False
-                else:
-                    once = True
-                    swap = []
-        return False if swap else True
+        # #First attempt w/ one swap, time: O(N), space O(1)
+        # if len(s1) != len(s2): return False        
+        # once = False
+        # swap = []
+        # for char1, char2 in zip(s1, s2):
+        #     if char1 != char2:
+        #         if once: return False
+        #         if not swap:
+        #             swap.extend([char1,char2])
+        #         elif swap[0] != char2 or swap[1] != char1:
+        #             return False
+        #         else:
+        #             once = True
+        #             swap = []
+        # return False if swap else True
             
         
