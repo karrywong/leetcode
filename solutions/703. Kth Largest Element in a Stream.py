79caf81267e1065ea12263b84c74a70f34aace6c
Karry Wong <ucdwong@ucdavis.edu>
2022-04-03 22:35:26 -0700

    Update 703. Kth Largest Element in a Stream.py

--- solutions/703. Kth Largest Element in a Stream.py
+++ solutions/703. Kth Largest Element in a Stream.py
@@ -1,36 +1,58 @@
-        elif root.val > key:
-            root.left = self.insert(root.left, key)
-        else:
-            root.cnt += 1
-            root.right = self.insert(root.right, key)
-        
-        return root
-    
-    def findKthLargest(self, root, k):
-        if not root: return -1
-        
-        elif root.cnt == k: return root.val
-        
-        elif root.cnt > k:
-            return self.findKthLargest(root.right, k)
-        else:
-            return self.findKthLargest(root.left, k - root.cnt -1)
-    
-#     def __init__(self, k: int, nums: List[int]):        
-#         #Leetcode, much cleaner
-#         self.k = k 
-#         self.heap = nums
-#         heapq.heapify(self.heap)
-#         while len(self.heap) > k:
-#             heapq.heappop(self.heap)
-        
-#     def add(self, val: int) -> int:
-#         heapq.heappush(self.heap, val)
-#         if len(self.heap) > self.k:
-#             heapq.heappop(self.heap)
-#         # print(self.heap)
-#         return self.heap[0]
-        
+class KthLargest:
+    class node():
+        def __init__(self, val, cnt):
+            self.val = val
+            self.right = None
+            self.left = None
+            self.cnt = cnt
+    
+    def __init__(self, k: int, nums: List[int]):
+        self.root = None
+        for num in nums:
+            self.root = self.insert(self.root, num)
+        self.k = k-1
+        
+    def add(self, val: int) -> int:
+        self.root = self.insert(self.root, val)
+        return self.findKthLargest(self.root, self.k)
+    
+    def insert(self, root, key):
+        if not root:
+            return self.node(key, 0)
+        
+        elif root.val > key:
+            root.left = self.insert(root.left, key)
+        else:
+            root.cnt += 1
+            root.right = self.insert(root.right, key)
+        
+        return root
+    
+    def findKthLargest(self, root, k):
+        if not root: return -1
+        
+        elif root.cnt == k: return root.val
+        
+        elif root.cnt > k:
+            return self.findKthLargest(root.right, k)
+        else:
+            return self.findKthLargest(root.left, k - root.cnt -1)
+    
+#     def __init__(self, k: int, nums: List[int]):        
+#         #Leetcode, much cleaner
+#         self.k = k 
+#         self.heap = nums
+#         heapq.heapify(self.heap)
+#         while len(self.heap) > k:
+#             heapq.heappop(self.heap)
+        
+#     def add(self, val: int) -> int:
+#         heapq.heappush(self.heap, val)
+#         if len(self.heap) > self.k:
+#             heapq.heappop(self.heap)
+#         # print(self.heap)
+#         return self.heap[0]
+        
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
