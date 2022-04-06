0426b6af3a223c8e8d1b575bde1ff6c842f3c9d4
Karry Wong <ucdwong@ucdavis.edu>
2022-04-05 20:52:22 -0700

    42 ms

--- solutions/953. Verifying an Alien Dictionary.py
+++ solutions/953. Verifying an Alien Dictionary.py
@@ -1,8 +1,22 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        ### Soln - sorting with key as library
-        lib = {v: i for i, v in enumerate(order)}
-        return words == sorted(words, key = lambda x : [lib[x[i]] for i in range(len(x))])
-​
+        #First atteempt, straightforward
+        lookup = {}
+        for i, char in enumerate(order):
+            lookup[char] = i
         
-        
+        for j in range(len(words)-1):
+            word1, word2 = words[j], words[j+1]
+            x = 0
+            n = min(len(word1), len(word2))
+            while x < n:
+                if lookup[word1[x]] > lookup[word2[x]]:
+                    return False
+                elif lookup[word1[x]] < lookup[word2[x]]:
+                    break
+                x += 1
+            if x < n:
+                continue
+            elif word1[n:]:
+                return False
+        return True
