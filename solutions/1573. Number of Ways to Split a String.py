ad06add2b187d42ddf7d523bb1f95218c0b50e7c
Karry Wong <ucdwong@ucdavis.edu>
2022-04-07 19:43:29 -0700

    453 ms

---
+++ solutions/1573. Number of Ways to Split a String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numWays(self, s: str) -> int:
+        s_int = [int(char) for char in s]
+        total_val = sum(s_int)
+        if total_val % 3 != 0:
+            return 0
+        elif total_val == 0:
+            return (len(s)-1)*(len(s)-2)//2 % (10**9+7)
+        else:
+            one_third_total_val = total_val//3
+            val, check1, check2 = 0, one_third_total_val, 2*one_third_total_val
+            count1, count2 = 0, 0
+            i = 0
+            while i < len(s_int) and val <= check2:
+                val += s_int[i]
+                if val == check1:
+                    count1 += 1
+                elif val == check2:
+                    count2 += 1
+                i += 1
+            return (count1 * count2) % (10**9+7)
