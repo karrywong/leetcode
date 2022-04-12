a6ccb22f8c7fa693b098985c59da7c3b48936f41
Karry Wong <ucdwong@ucdavis.edu>
2022-04-12 10:33:45 -0700

    170 ms

--- solutions/1367. Linked List in Binary Tree.py
+++ solutions/1367. Linked List in Binary Tree.py
@@ -11,6 +11,26 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        # #KMP algorithm, too convoluted, time O(N+L), space O(L+H)
+        # A, dp = [head.val], [0]
+        # i = 0
+        # node = head.next
+        # while node:
+        #     while i and node.val != A[i]:
+        #         i = dp[i - 1]
+        #     i += node.val == A[i]
+        #     A.append(node.val)
+        #     dp.append(i)
+        #     node = node.next
+        # # print(A, dp)
+        # def dfs(root, i):
+        #     if not root: return False
+        #     while i and root.val != A[i]:
+        #         i = dp[i - 1]
+        #     i += root.val == A[i]
+        #     return i == len(dp) or dfs(root.left, i) or dfs(root.right, i)
+        # return dfs(root, 0)
+        
         #Brute force, inspired by lee215
         #Time O(N*min(L,H)), Space O(H), w/ N = tree size, L = len(head), H = tree height
         def helper(linklst: ListNode, node: TreeNode) -> bool:
