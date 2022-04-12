fb8d4387a9f015dcd14f4711e8f2611311b5507f
Karry Wong <ucdwong@ucdavis.edu>
2022-04-12 10:44:00 -0700

    242 ms

--- solutions/43. Multiply Strings.py
+++ solutions/43. Multiply Strings.py
@@ -1,6 +1,7 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        #Leetcode - sum products from all pairs of digits
+        #Leetcode (Approach 3) - sum products from all pairs of digits
+        #Time O(M*N), space O(M+N)
         if num1 == "0" or num2 == "0": return "0"
         ans = [0]* (len(num1) + len(num2))
         num1, num2 = num1[::-1], num2[::-1] #reverse num1 and num2 in place
@@ -15,14 +16,20 @@ class Solution:
         if ans[-1] == 0: ans.pop()
         return ''.join(str(digit) for digit in reversed(ans))
         
-        #First attempt, write out num2 (more digits) and compute sum([(j*10**n)*val2 for j in num1])
-        #Time O(N + M*N), Space O(1)
-        if len(num1) > len(num2):
-            num1, num2 = num2, num1
-        m, n = len(num1), len(num2) #m <= n
-        val2, order = 0, 1
-        for i in range(n-1, -1, -1):
-            val2 += int(num2[i]) * order
-            order *= 10
+#         #First attempt, write out num2 (more digits) and compute sum([(j*10**n)*val2 for j in num1])
+#         #Time O(N + M*N), Space O(1), where N = len(num1), M = len(num2)
+#         if len(num1) > len(num2):
+#             num1, num2 = num2, num1
+#         m, n = len(num1), len(num2) #m <= n
+#         val2, order = 0, 1
+#         for i in range(n-1, -1, -1):
+#             val2 += int(num2[i]) * order
+#             order *= 10
 ​
-        val1, order = 0, 1
+#         val1, order = 0, 1
+#         ans = 0
+#         for i in range(m-1, -1,-1):
+#             val1 = int(num1[i]) * order
+#             ans += val1 * val2
+#             order *= 10
+#         return str(ans)    
