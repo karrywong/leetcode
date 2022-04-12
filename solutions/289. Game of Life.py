7ded14df3d49df49b448563ce4141300ea18daaf
Karry Wong <ucdwong@ucdavis.edu>
2022-04-12 08:00:19 -0700

    61 ms

--- solutions/289. Game of Life.py
+++ solutions/289. Game of Life.py
@@ -3,6 +3,19 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+#         ###Bonus - Stefan Pochmann's soln for sparse matrix
+#         def gameOfLifeInfinite(self, live: set) -> set:
+#             ctr = collections.Counter((I, J) for i, j in live for I in range(i-1, i+2) for J in range(j-1, j+2) \
+#                                       if I != i or J != j)
+#             return {ij for ij in ctr if ctr[ij] == 3 or ctr[ij] == 2 and ij in live}
+        
+#         def gameOfLife(self, board):
+#             live = {(i, j) for i, row in enumerate(board) for j, live in enumerate(row) if live}
+#             live = self.gameOfLifeInfinite(live)
+#             for i, row in enumerate(board):
+#                 for j in range(len(row)):
+#                     row[j] = int((i, j) in live)        
+        
         #Soln 2 - Use tricks -1: live->dead and 2: dead->live, time O(M*N), space O(1)
         moves = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
         m, n = len(board), len(board[0])
