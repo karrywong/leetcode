c797d20797ab3118f649937ba539466c27b9b83d
Karry Wong <ucdwong@ucdavis.edu>
2022-04-16 12:57:54 -0700

    37 ms

--- solutions/54. Spiral Matrix.py
+++ solutions/54. Spiral Matrix.py
@@ -1,26 +1,20 @@
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        #Second attempt, code cleaner, time O(N), space O(1) excluding the output array
-        m, n = len(matrix), len(matrix[0])
-        ans, size = [], m*n
-        left, right = 0, n-1 #j
-        up, down = 0, m-1 #i
-        while len(ans) < size:
-            for j in range(left, right+1):
-                ans.append(matrix[up][j]) #left to right
-            for i in range(up+1, down+1):
-                ans.append(matrix[i][right]) #up to down
-            if up != down: #make sure that we're on a different row
-                for j in range(right-1, left-1,-1):
-                    ans.append(matrix[down][j])
-            if left != right: #make sure that we're on a different col
-                for i in range(down-1, up, -1):
-                    ans.append(matrix[i][left])
-            left += 1
-            up += 1
-            right -= 1
-            down -= 1
-        return ans
+        # up, down = 0, m-1 #i
+        # while len(ans) < size:
+        #     for j in range(left, right+1):
+        #         ans.append(matrix[up][j]) #left to right
+        #     for i in range(up+1, down+1):
+        #         ans.append(matrix[i][right]) #up to down
+        #     if up != down: #make sure that we're on a different row
+        #         for j in range(right-1, left-1,-1):
+        #             ans.append(matrix[down][j])
+        #     if left != right: #make sure that we're on a different col
+        #         for i in range(down-1, up, -1):
+        #             ans.append(matrix[i][left])
+        #     left += 1
+        #     up += 1
+        #     right -= 1
+        #     down -= 1
+        # return ans
         
         # #First attempt, direct solution, time O(N), space O(1) excluding the output array
         # m, n = len(matrix), len(matrix[0])
