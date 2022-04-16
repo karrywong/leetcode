5ce0afc7320804de25116b891b39a6be69f19811
Karry Wong <ucdwong@ucdavis.edu>
2022-04-16 11:32:40 -0700

    196 ms

--- solutions/344. Reverse String.py
+++ solutions/344. Reverse String.py
@@ -3,13 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        ##Soln 1
-        l = 0 
-        r = len(s) - 1
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
-        
-        # ###Soln 2
-        # s[:] = s[::-1]
+        # #soln 1 - n/2 swaps
+        l = len(s)
+        for i in range(0, l//2):
+            s[i], s[~i] = s[~i], s[i]
+            
+        # #soln 0 - built-in ft
+        # s.reverse()
+            
