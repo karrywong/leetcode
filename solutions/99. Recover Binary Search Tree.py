9f06840d2c1950e1e12248b5cf387716e728ff6d
Karry Wong <ucdwong@ucdavis.edu>
2022-04-18 17:20:36 -0700

    70 ms

--- solutions/99. Recover Binary Search Tree.py
+++ solutions/99. Recover Binary Search Tree.py
@@ -9,25 +9,44 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        #Trivial w/ space O(N), time O(N)
-        def helper(node):
-            if not node: return []
-            return helper(node.left) + [node.val] + helper(node.right)
+#         #soln 1 - Trivial (first flatten tree->find two swapped nodes->dfs) w/ space O(N), time O(N)
+#         def helper(node):
+#             if not node: return []
+#             return helper(node.left) + [node.val] + helper(node.right)
         
-        def dfs(node):
-            if not node: return
-            if node.val in diff:
-                node.val = diff[1] if node.val == diff[0] else diff[0]
-            dfs(node.left)
-            dfs(node.right)
+#         def dfs(node):
+#             if not node: return
+#             if node.val in diff:
+#                 node.val = diff[1] if node.val == diff[0] else diff[0]
+#             dfs(node.left)
+#             dfs(node.right)
             
-        lst = helper(root)
-        diff = [None, None]
-        for i in range(len(lst)-1):
-            if lst[i] > lst[i+1]:
-                diff[1] = lst[i+1]
-                if diff[0] is None:
-                    diff[0] = lst[i]
+#         lst = helper(root)
+#         diff = [None, None]
+#         for i in range(len(lst)-1):
+#             if lst[i] > lst[i+1]:
+#                 diff[1] = lst[i+1]
+#                 if diff[0] is None:
+#                     diff[0] = lst[i]
+#                 else:
+#                     break
+#         dfs(root)
+​
+        #soln 2 - LeetCode's iterative inorder, time O(N), space (N)
+        stack = []
+        x = y = pred = None
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if pred and root.val < pred.val:
+                y = root
+                if x is None:
+                    x = pred
                 else:
                     break
-        dfs(root)
+            pred = root
+            root = root.right
+        
+        x.val, y.val = y.val, x.val
