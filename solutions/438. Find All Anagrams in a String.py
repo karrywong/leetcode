ab958c9db5a0f922f3fbf571455d4213f9069b22
Karry Wong <ucdwong@ucdavis.edu>
2022-04-20 11:52:22 -0700

    109 ms

--- solutions/438. Find All Anagrams in a String.py
+++ solutions/438. Find All Anagrams in a String.py
@@ -6,15 +6,16 @@ class Solution:
         if np > ns: return []        
         
         count_p, count_s = [0]*26, [0]*26
-        for i in range(np):
-            count_p[ord(p[i])-ord('a')] += 1
-            count_s[ord(s[i])-ord('a')] += 1
             
-        for i in range(ns-np):
-            if count_p == count_s:
-                ans.append(i) 
-            count_s[ord(s[np+i])-ord('a')] += 1
-            count_s[ord(s[i])-ord('a')] -= 1
+        for i in range(ns):
+            if i < np:
+                count_p[ord(p[i])-ord('a')] += 1
+                count_s[ord(s[i])-ord('a')] += 1  
+            else:                      
+                if count_p == count_s: 
+                    ans.append(i-np)
+                count_s[ord(s[i])-ord('a')] += 1
+                count_s[ord(s[i-np])-ord('a')] -= 1
         
         if count_p == count_s: ans.append(ns-np)
         return ans
@@ -30,3 +31,13 @@ class Solution:
 #         for i in range(ns-np):
 #             if lib1 == lib2:
 #                 ans.append(i)
+            
+#             lib2[s[np+i]] += 1
+#             lib2[s[i]] -= 1
+#             if lib2[s[i]] == 0:
+#                 del lib2[s[i]]
+            
+#         if lib1 == lib2:
+#             ans.append(ns-np)
+        
+#         return ans
