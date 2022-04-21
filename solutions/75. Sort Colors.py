a2bb0bffa5b16fbfba2f5a93a47a8c6e92842766
Karry Wong <ucdwong@ucdavis.edu>
2022-04-20 20:42:31 -0700

    50 ms

--- solutions/75. Sort Colors.py
+++ solutions/75. Sort Colors.py
@@ -3,23 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-#         #soln 1 - Dutch national flag problem
-#         # for all idx < p0 : nums[idx < p0] = 0
-#         # curr is an index of element under consideration
-#         p0 = curr = 0
-#         # for all idx > p2 : nums[idx > p2] = 2
-#         p2 = len(nums) - 1
-​
-#         while curr <= p2:
-#             if nums[curr] == 0:
-#                 nums[p0], nums[curr] = nums[curr], nums[p0]
-#                 p0 += 1
-#                 curr += 1
-#             elif nums[curr] == 2:
-#                 nums[curr], nums[p2] = nums[p2], nums[curr]
-#                 p2 -= 1
-#             else:
-#                 curr += 1
+        # #soln 1 - Dutch national flag problem
+        # # for all idx < p0 : nums[idx < p0] = 0
+        # # curr is an index of element under consideration
+        # p0 = curr = 0
+        # # for all idx > p2 : nums[idx > p2] = 2
+        # p2 = len(nums) - 1
+        # while curr <= p2:
+        #     if nums[curr] == 0:
+        #         nums[p0], nums[curr] = nums[curr], nums[p0]
+        #         p0 += 1
+        #         curr += 1
+        #     elif nums[curr] == 2:
+        #         nums[curr], nums[p2] = nums[p2], nums[curr]
+        #         p2 -= 1
+        #     else:
+        #         curr += 1
 ​
         #counting sort
         count =  [0 for _ in range(3)]
@@ -32,12 +31,12 @@ class Solution:
         for i in range(1, len(count)):
             count[i] += count[i-1]
         # Build the output character array
-        for i in range(n-1, -1, -1):
-            output[count[nums[i]] - 1] = nums[i]
-            count[nums[i]] -= 1
-        # Copy the output array to arr, so that arr now contains sorted characters
         for i in range(n):
-            nums[i] = output[i]   
+            count[nums[i]] -= 1
+            output[count[nums[i]]] = nums[i]
+            
+        # Copy the output array to arr, so that arr now contains sorted characters
+        nums[:] = output[:]   
                 
 #         #soln 0 - Jake's solution
 #         i,j,k = 0,0,len(nums) - 1
