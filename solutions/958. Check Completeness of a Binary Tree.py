364a86770d70e3126d238308210e8013cc94e2a5
Karry Wong <ucdwong@ucdavis.edu>
2022-04-24 18:29:41 -0700

    49 ms

---
+++ solutions/958. Check Completeness of a Binary Tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:        
+        #Soln 1 - my original idea
+        queue = collections.deque([root])
+        target = 1
+        while queue:
+            size = len(queue)
+            bo_end = False #denote end of level
+            for _ in range(size):
+                node = queue.popleft()
+            
+                if bo_end and (node.left or node.right):
+                    return False
+​
+                if node.left and node.right:
+                    queue.append(node.left)
+                    queue.append(node.right)
+                elif not node.left and node.right:
+                    return False
+                else:  #(node.left and not node.right), (not node.left, not node.right)
+                    if node.left: queue.append(node.left)
+                    bo_end = True
+            
+            if size != target and queue:
+                return False
+            target *= 2
+        return True
