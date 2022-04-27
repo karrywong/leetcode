debd9b6565200a98d91325b85ed37c02d099d91c
Karry Wong <ucdwong@ucdavis.edu>
2022-04-27 12:00:38 -0700

    112 ms

--- solutions/1232. Check If It Is a Straight Line.py
+++ solutions/1232. Check If It Is a Straight Line.py
@@ -2,8 +2,10 @@ class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
         #Discussion
         (x0, y0), (x1, y1) = coordinates[:2]
+        x_diff = x1 - x0
+        y_diff = y1 - y0
         for x, y in coordinates:
-            if (x1 - x0) * (y - y1) != (x - x1) * (y1 - y0):
+            if x_diff * (y - y1) != (x - x1) * y_diff:
                 return False
         return True
         
