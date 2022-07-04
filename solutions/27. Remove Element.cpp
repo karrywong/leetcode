f599780d0d51ab6086d49682445397cc9660e35e
Karry Wong <ucdwong@ucdavis.edu>
2022-07-04 12:48:28 -0700

    8 ms

---
+++ solutions/27. Remove Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // int ans = 0, j = 0;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] != val) {
+        //         nums[j] = nums[i];
+        //         j++, ans++;
+        //     }
+        // }
+        // return ans;
+        
+        //one-liner by dreyri
+        return std::distance(nums.begin(), std::remove(nums.begin(), nums.end(), val));
+    }
+};
