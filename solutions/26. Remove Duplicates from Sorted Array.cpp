b756a7f27da004b0a21ca160dfbd79b7ca6cca05
Karry Wong <ucdwong@ucdavis.edu>
2023-01-01 12:41:35 -0800

    14 ms

---
+++ solutions/26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // Soln 1 - two pointer, time O(N), space O(1)
+        // int j=0;
+        // for (int i = 1; i < nums.size(); ++i) {
+        //     if (nums[j] != nums[i]) {
+        //         j++;
+        //         nums[j] = nums[i];
+        //     }
+        // }
+        // return j+1;
+        
+        //Soln 2 - reverse
+        const auto it = std::unique(nums.begin(), nums.end());
+        nums.erase(it, nums.end());
+        return nums.size();
+    }
+};
