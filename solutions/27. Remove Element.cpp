63db84f3024a1a0933e970361759741c90908f34
Karry Wong <ucdwong@ucdavis.edu>
2023-01-01 12:06:46 -0800

    0 ms

--- solutions/27. Remove Element.cpp
+++ solutions/27. Remove Element.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        // int ans = 0, j = 0;
-        // for (int i = 0; i < nums.size(); i++) {
-        //     if (nums[i] != val) {
-        //         nums[j] = nums[i];
-        //         j++, ans++;
-        //     }
-        // }
-        // return ans;
+        int ans = 0, j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++, ans++;
+            }
+        }
+        return ans;
         
         //one-liner by dreyri
-        return std::distance(nums.begin(), std::remove(nums.begin(), nums.end(), val));
+        // return std::distance(nums.begin(), std::remove(nums.begin(), nums.end(), val));
     }
 };
