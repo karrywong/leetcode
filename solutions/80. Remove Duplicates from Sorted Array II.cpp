1a0e3a49d654e8635634851569ff2639b6493466
Karry Wong <ucdwong@ucdavis.edu>
2023-01-07 16:07:56 -0800

    7 ms

--- solutions/80. Remove Duplicates from Sorted Array II.cpp
+++ solutions/80. Remove Duplicates from Sorted Array II.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // first attempt, time O(N), space O(N)
-        if (nums.size() == 0) return 0;
-        
-        vector<pair<int, int>> record;
-        int temp = nums[0], count = 1;
-        for (int i = 1; i < nums.size(); i++){
-            if (temp == nums[i]) count++;
+        //2nd attempt, time O(N), space O(1)
+        int count=1, j=1;
+        for (int i=1; i<nums.size(); i++) {
+            if (nums[i-1]==nums[i]) 
+                count++;
             else {
-                record.push_back(make_pair(temp,count));
-                temp = nums[i];
                 count = 1;
             }
-        }
-        record.push_back(make_pair(temp,count));
-        int i = 0;
-        for (auto tp:record) {
-            if (tp.second > 2) tp.second = 2;
-            for (int j = 0; j < tp.second; j++){
-                nums[i++] = tp.first;
+            
+            if (count <= 2) {
+                nums[j++] = nums[i];
             }
         }
-        return i;
+        return j;
+        
+//         // first attempt, time O(N), space O(N)
+//         if (nums.size() == 0) return 0;
+        
+//         vector<pair<int, int>> record;
+//         int temp = nums[0], count = 1;
+//         for (int i = 1; i < nums.size(); i++){
+//             if (temp == nums[i]) count++;
+//             else {
+//                 record.push_back(make_pair(temp,count));
+//                 temp = nums[i];
+//                 count = 1;
+//             }
+//         }
+//         record.push_back(make_pair(temp,count));
+//         int i = 0;
+//         for (auto tp:record) {
+//             if (tp.second > 2) tp.second = 2;
+//             for (int j = 0; j < tp.second; j++){
+//                 nums[i++] = tp.first;
+//             }
+//         }
+//         return i;
     }
 };
