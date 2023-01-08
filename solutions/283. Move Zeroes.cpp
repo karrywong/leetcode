f74b8f7376cdcb3de8e46c54e62741926dc85fda
Karry Wong <ucdwong@ucdavis.edu>
2023-01-08 12:42:44 -0800

    24 ms

---
+++ solutions/283. Move Zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // first attempt, time O(N), space O(1)
+        int j=0, temp;
+        for (int i=0; i<nums.size(); i++){
+            if (nums[i] != 0) {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+        
+    }
+};
