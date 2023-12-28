5dad37ec10488cc665a07d11f4fad6283cf96cf1
Karry Wong <ucdwong@ucdavis.edu>
2023-12-27 20:18:11 -0800

    0 ms

--- solutions/374. Guess Number Higher or Lower.cpp
+++ solutions/374. Guess Number Higher or Lower.cpp
@@ -9,17 +9,33 @@
 ​
 class Solution {
 public:
+//     better
     int guessNumber(int n) {
-        // initialization
-        int left = 1, right = n, mid=left+(right-left)/2;
-        int guessAns = guess(mid);
-        while (guessAns != 0) {
-            if (guessAns == 1) left = mid+1;
-            else if (guessAns == -1) right = mid;
-            mid = left+(right-left)/2;
-            guessAns = guess(mid);
-            
-        }
-        return mid;
+        int l = 1, r = n;
+        while (l < r) {
+            int mid = l + (r-l)/2;
+            int guessAns = guess(mid);
+            if (guessAns == 0) 
+                return mid;
+            else if (guessAns == 1)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return l;
     }
+//     bad
+//     int guessNumber(int n) {
+//         // initialization
+//         int left = 1, right = n, mid=left+(right-left)/2;
+//         int guessAns = guess(mid);
+//         while (guessAns != 0) {
+//             if (guessAns == 1) left = mid+1;
+//             else if (guessAns == -1) right = mid;
+//             mid = left+(right-left)/2;
+//             guessAns = guess(mid);
+            
+//         }
+//         return mid;
+//     }
 };
