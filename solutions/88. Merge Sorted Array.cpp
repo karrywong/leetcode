63c052c51b4b101543658a9a99a48caae2606900
Karry Wong <ucdwong@ucdavis.edu>
2024-01-06 14:53:34 -0800

    0 ms

--- solutions/88. Merge Sorted Array.cpp
+++ solutions/88. Merge Sorted Array.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        //time O(m+n), space O(1)
-        int ptr1 = m-1, ptr2 = n-1, ptr3 = m+n-1;
-        while (ptr1 >= 0 && ptr2 >= 0) {
-            if (nums1[ptr1] < nums2[ptr2]) {
-                nums1[ptr3] = nums2[ptr2];
-                ptr2--;
+        int p1 = m-1, p2 = n-1;
+        for (int p=m+n-1; p > -1; p--) {
+            if (p2 < 0) break;
+            else if (p1 >= 0 && nums1[p1] > nums2[p2]){
+                nums1[p] = nums1[p1];
+                p1--;
             }
             else {
-                nums1[ptr3] = nums1[ptr1];
-                ptr1--;
+                nums1[p] = nums2[p2];
+                p2--;
             }
-            ptr3--;
         }
-        while (ptr2 >= 0) {
-            nums1[ptr2] = nums2[ptr2];
-            ptr2--;
-        }
+        
+        // //time O(m+n), space O(1)
+        // int ptr1 = m-1, ptr2 = n-1, ptr3 = m+n-1;
+        // while (ptr1 >= 0 && ptr2 >= 0) {
+        //     if (nums1[ptr1] < nums2[ptr2]) {
+        //         nums1[ptr3] = nums2[ptr2];
+        //         ptr2--;
+        //     }
+        //     else {
+        //         nums1[ptr3] = nums1[ptr1];
+        //         ptr1--;
+        //     }
+        //     ptr3--;
+        // }
+        // while (ptr2 >= 0) {
+        //     nums1[ptr2] = nums2[ptr2];
+        //     ptr2--;
+        // }
     }
 };
