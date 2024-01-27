b7724493490728a5392edfa31a0286331e8d2889
Karry Wong <ucdwong@ucdavis.edu>
2024-01-27 14:03:12 -0800

    34 ms

--- solutions/1980. Find Unique Binary String.py
+++ solutions/1980. Find Unique Binary String.py
@@ -5,10 +5,18 @@ class Solution:
         # nums = ["111","011","001"] -> [7,3,1], range: 0-7, 000
         
         # defintion: n = len(binary string) = len(nums)
-        #Goedos
-        
-        #diagonal argument: nums = ["01","10"] ans = 11
+        # binary to base 10
+        # time O(n^2), space O(n)
+        n = len(nums)
+        lst = set([int(num,2) for num in nums])
+        for i in range(2**n):
+            if i not in lst:
+                return bin(i)[2:].zfill(n)
+    
+        # diagonal argument: nums = ["01","10"] ans = 11
+        # time O(n), space O(n)
         # ["00","01"] = 10
         # ["111","011","001"] = [000]
-        return "".join(["1" if num[idx] == "0" else "0" for idx, num in enumerate(nums)])
+        # return "".join(["1" if num[idx] == "0" else "0" for idx, num in enumerate(nums)])
+    
         
