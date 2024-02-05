026ff5e32bc7c565c017614a4ec201a2f29d6554
Karry Wong <ucdwong@ucdavis.edu>
2024-02-04 17:16:27 -0800

    1295 ms

--- solutions/2024. Maximize the Confusion of an Exam.py
+++ solutions/2024. Maximize the Confusion of an Exam.py
@@ -26,4 +26,28 @@ class Solution:
     
     # answerKey = "TTFTTFTT", k = 1
     # (idx, ans): (0,1)->(1,2)->
+​
+#         # binary search, O(N*logN)
+#         n = len(answerKey)
+#         left, right = k, n
         
+#         def isValid(size):
+#             counter = collections.Counter(answerKey[:size])
+#             if min(counter['T'], counter['F']) <= k:
+#                 return True
+#             for i in range(size, n):
+#                 counter[answerKey[i]] += 1
+#                 counter[answerKey[i - size]] -= 1
+#                 if min(counter['T'], counter['F']) <= k:
+#                     return True
+#             return False
+        
+#         while left < right:
+#             mid = (left + right + 1) // 2
+            
+#             if isValid(mid):
+#                 left = mid
+#             else:
+#                 right = mid - 1
+        
+#         return left
