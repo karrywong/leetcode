9ce315d2b473275ac2fb9011076041dd5d68a158
Karry Wong <ucdwong@ucdavis.edu>
2024-02-10 10:56:22 -0800

    285 ms

--- solutions/487. Max Consecutive Ones II.py
+++ solutions/487. Max Consecutive Ones II.py
@@ -1,45 +1,31 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int: 
-        # Runtime O(N), space O(1)
-        k = -1 
-        j = 0 # last seen 0 position
-        cnt_zero = 0
-        ans = 0
-        
+        # 1004. Max Consecutive Ones III, runtime O(N), space O(1)
+        k, ans = -1, 0
+        deq = deque([])
         for i in range(len(nums)):
             if nums[i] == 0:
-                cnt_zero += 1
-                if cnt_zero > 1:
-                    k = j
-                j = i
+                deq.append(i)
+            if len(deq) > 1:
+                k = deq.popleft()
             ans = max(ans, i-k)
         return ans
+        
+#         # two pointer, Runtime O(N), space O(1)
+#         k = -1 
+#         j = 0 # last seen 0 position
+#         cnt_zero = 0
+#         ans = 0
+        
+#         for i in range(len(nums)):
+#             if nums[i] == 0:
+#                 cnt_zero += 1
+#                 if cnt_zero > 1:
+#                     k = j
+#                 j = i
+#             ans = max(ans, i-k)
+#         return ans
     
     #  (i, k, j, ans)
     # [1,0,1,1,0] 
     # (0, -1, 0, 1) -> (1, -1, 1, 2) -> (2,-1,1,3) -> (3,-1,1,4) -> (4,1,4,4)
-    
-    # [1,0,1,1,0,1,1,1,1,1]
-    # ans = 8
-    
-    # [1,0,1,1,0,1]
-    # (0,-1,0,1) -> (1,-1,0,1) -> ... ->(3,-1,0,4) -> (4,1,4,4) -> (5,1,4,4)
-​
-# ## Soln 2 - fast and elegant solution from discussion by redSand
-#         k = result = 0
-#         prev_zero_seen = -1
-#         for i, num in enumerate(nums):
-#             if num == 0:
-#                 result = max(result, i - k)
-#                 k = prev_zero_seen + 1
-#                 prev_zero_seen = i
-        
-#         return max(result, len(nums) - k)
-    
-        ### Old attempts
-        ### Soln 1 - brute force O(n^2), time exceeded
-        # res = 0
-        # for i in range(len(nums)):
-        #     count = 0
-        #     for j in range(i, len(nums)):
-        #         if count == 2:
