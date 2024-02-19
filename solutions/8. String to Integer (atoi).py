4e3a1a2813263e90a68880e9a5e7f2d7e98a048f
Karry Wong <ucdwong@ucdavis.edu>
2024-02-19 01:01:42 -0800

    45 ms

--- solutions/8. String to Integer (atoi).py
+++ solutions/8. String to Integer (atoi).py
@@ -1,3 +1,24 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        # Max and Min values for the integers
+        INT_MAX = 2147483647
+        INT_MIN = -2147483648
+        # Trimmed string
+        s = s.lstrip()
+        # Counter
+        i = 0
+        # Flag to indicate if the number is negative
+        isNegative = len(s) > 1 and s[0] == '-'
+        # Flag to indicate if the number is positive
+        isPositive = len(s) > 1 and s[0] == '+'
+        if isNegative:
+            i += 1
+        elif isPositive:
+            i += 1
+        # This will store the converted number
+        number = 0
+        # Loop for each numeric character in the string iff numeric characters are leading
+        # characters in the string
         while i < len(s) and '0' <= s[i] <= '9':
             number = number * 10 + (ord(s[i]) - ord('0'))
             i += 1
