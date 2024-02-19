533bf89e54c41266199a2f82c2a736b78cd8261c
Karry Wong <ucdwong@ucdavis.edu>
2024-02-18 17:00:57 -0800

    61 ms

--- solutions/34. Find First and Last Position of Element in Sorted Array.py
+++ solutions/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,29 +1,45 @@
+# import bisect
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        #soln 0 - modified binary search, time O(N), space O(1)
-        n = len(nums)
-        if n == 0: return [-1,-1]
-        if nums[0] == nums[-1]:
-            if nums[0] == target:
-                return [0, n-1]
+        # # soln 2 - use built-in function
+        # if len(nums) == 0:
+        #     return [-1,-1]
+        # left = bisect.bisect_left(nums, target)
+        # left = -1 if left == len(nums) or nums[left] != target else left
+        # right = bisect.bisect_right(nums, target)
+        # right = -1 if right == 0 or nums[right-1] != target else right-1
+        # return [left,right]
+        
+        #soln 1 - binary search, both upper and lower bounds
+        #for repetitious numbers
+        if len(nums) == 0:
+            return [-1,-1]
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = left + (right-left)//2
+            if nums[mid] < target:
+                left = mid+1
+            else: 
+                right = mid
+        left = left if nums[left] == target else -1
+        ans = [left]
+        
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + (right-left)//2
+            if nums[mid] <= target:
+                left = mid+1
             else:
-                return [-1,-1]
-        l, r = 0, n - 1
-        while l <= r:
-            mid = l + (r-l) // 2
-            if nums[mid] == target:
-                ans = [mid,mid]
-                i,j = mid-1, mid+1
-                while i >= 0 and nums[i] == target:
-                    i -= 1
-                ans[0] = i + 1
-                while j <= n - 1 and nums[j] == target:
-                    j += 1
-                ans[1] = j - 1
-                return ans
-                    
-            if nums[mid] > target:
-                r = mid - 1
-            else:
-                l = mid + 1
-        return [-1,-1]
+                right = mid
+        right = right-1 if nums[right-1] == target else -1
+        ans.append(right)
+        return ans
+    
+        # Testing
+        # nums = [5,7,7,8,8,10], target = 8
+        # (0,5) -> (3,5) -> (3,4) -> (3,3) -> 3
+        # (0,6) -> (4,6) -> (4,5) -> (5,5) -> 4
+        
+        # nums = [5,7,7,8,8,10], target = 6
+        # (0,5) -> (0,2) -> (0,1) -> (1,1) -> -1
+        # (0,6) -> (0,3) -> (0,1) -> (1,1) -> -1        
