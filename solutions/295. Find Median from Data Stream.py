4304e58d75d0101130888c74f64a5b984f56542c
Karry Wong <ucdwong@ucdavis.edu>
2024-02-19 13:13:03 -0800

    410 ms

--- solutions/295. Find Median from Data Stream.py
+++ solutions/295. Find Median from Data Stream.py
@@ -1,29 +1,27 @@
 class MedianFinder:
-​
-    def __init__(self):
-        # self.lst = [] #trivial list sort, time O(NlogN)
-        
+    def __init__(self):        
         self.maxHeap = [] #first half of all numbers
         self.minHeap = [] #second half of all numbers
 ​
     def addNum(self, num: int) -> None:
-        #self.lst.append(num)
-        
         #Add num to maxHeap
         #balancing two heaps
-        if not self.maxHeap:
+        if len(self.maxHeap) == 0:
             heapq.heappush(self.maxHeap, -1*num)
-        else:
-            heapq.heappush(self.maxHeap, -1*num)
-            heapq.heappush(self.minHeap, -1*heapq.heappop(self.maxHeap))
+            return
+        
+        temp = -1*heapq.heappushpop(self.maxHeap, -1*num)
+        heapq.heappush(self.minHeap, temp)
         
         if len(self.maxHeap) < len(self.minHeap):
             heapq.heappush(self.maxHeap, -1*heapq.heappop(self.minHeap))
-    def findMedian(self) -> float:
-        # self.lst.sort()
-        # return self.lst[len(self.lst)//2] if len(self.lst) % 2 == 1 else 0.5*(self.lst[len(self.lst)//2-1] + self.lst[len(self.lst)//2])
         
-        return -1*self.maxHeap[0] if len(self.maxHeap) > len(self.minHeap) else 0.5*(-1*self.maxHeap[0]+self.minHeap[0])
+            
+    def findMedian(self) -> float:
+        if len(self.maxHeap) > len(self.minHeap):
+            return -1*self.maxHeap[0] 
+        else:
+            return 0.5*(-1*self.maxHeap[0]+self.minHeap[0])
 ​
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
