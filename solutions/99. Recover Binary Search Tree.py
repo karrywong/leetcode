980df24c9e46163f2d4467c41d443d08bf25337c
Karry Wong <ucdwong@ucdavis.edu>
2024-02-22 22:31:22 -0800

    54 ms

--- solutions/99. Recover Binary Search Tree.py
+++ solutions/99. Recover Binary Search Tree.py
@@ -1,3 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        # soln 3 - Recursive inorder, time O(N), space O(N)
+        def dfs(node: Optional[TreeNode]) -> None:
+            if node is None:
+                return
+            
+            dfs(node.left)
+            if self.prev_node and node.val < self.prev_node.val:
+                if self.x is None:
+                    self.x = self.prev_node
+                self.y = node
+            self.prev_node = node
+            dfs(node.right)
+        
+        self.x = self.y = self.prev_node = None
+        dfs(root)
+        self.x.val, self.y.val = self.y.val, self.x.val
+        
+#         #soln 2 - Iterative inorder, time O(N), space (N)
 #         stack = []
 #         node = root
 #         x = y = prev_node = None
@@ -36,10 +65,3 @@
 #             dfs(node.right)
             
 #         lst = helper(root)
-#         x, y = None, None
-#         for i in range(len(lst)-1):
-#             if lst[i] > lst[i+1]:
-#                 if x is None:
-#                     x = lst[i]
-#                 y = lst[i+1]
-#         dfs(root)
