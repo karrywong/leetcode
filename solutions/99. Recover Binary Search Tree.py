78d585645e36b438715dd28fbc706cf5f4501e8c
Karry Wong <ucdwong@ucdavis.edu>
2024-02-22 22:31:15 -0800

    54 ms

--- solutions/99. Recover Binary Search Tree.py
+++ solutions/99. Recover Binary Search Tree.py
@@ -1,52 +1,45 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def recoverTree(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
+#         stack = []
+#         node = root
+#         x = y = prev_node = None
+#         while True:
+#             if node is not None:
+#                 stack.append(node)
+#                 node = node.left
+#             elif len(stack) > 0:
+#                 node = stack.pop()
+                
+#                 if prev_node and node.val < prev_node.val:
+#                     if x is None:
+#                         x = prev_node
+#                     y = node
+    
+#                 prev_node = node 
+#                 node = node.right
+#             else:
+#                 break
+#         x.val, y.val = y.val, x.val
+​
 #         #soln 1 - Trivial (first flatten tree->find two swapped nodes->dfs) w/ space O(N), time O(N)
-#         def helper(node):
-#             if not node: return []
+#         def helper(node: Optional[TreeNode]) -> List[int]:
+#             if not node:
+#                 return []
 #             return helper(node.left) + [node.val] + helper(node.right)
         
-#         def dfs(node):
-#             if not node: return
-#             if node.val in diff:
-#                 node.val = diff[1] if node.val == diff[0] else diff[0]
+#         def dfs(node: Optional[TreeNode]) -> None:
+#             if not node:
+#                 return 
+#             if node.val == x:
+#                 node.val = y
+#             elif node.val == y:
+#                 node.val = x
 #             dfs(node.left)
 #             dfs(node.right)
             
 #         lst = helper(root)
-#         diff = [None, None]
+#         x, y = None, None
 #         for i in range(len(lst)-1):
 #             if lst[i] > lst[i+1]:
-#                 diff[1] = lst[i+1]
-#                 if diff[0] is None:
-#                     diff[0] = lst[i]
-#                 else:
-#                     break
+#                 if x is None:
+#                     x = lst[i]
+#                 y = lst[i+1]
 #         dfs(root)
-​
-        #soln 2 - LeetCode's iterative inorder, time O(N), space (N)
-        stack = []
-        x = y = pred = None
-        while stack or root:
-            while root:
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
-            if pred and root.val < pred.val:
-                y = root
-                if x is None:
-                    x = pred
-                else:
-                    break
-            pred = root
-            root = root.right
-        
-        x.val, y.val = y.val, x.val
