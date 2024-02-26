28d07eb7c4a9c4b3c2acd4845d0b44e5a9e3e99c
Karry Wong <ucdwong@ucdavis.edu>
2024-02-25 17:01:29 -0800

    43 ms

--- solutions/88. Merge Sorted Array.py
+++ solutions/88. Merge Sorted Array.py
@@ -1,34 +1,44 @@
 class Solution:
+    # m_0, .., m_(k-1)
+    # List[List[int]], [[1,2,4], [2,3,5], [3,3,3,3,3], [1,4,5], ...]
+    # each step, O(k)
+    # time O([m_0+m_1+...+m_(k-1)]*k)
+    # mergesort
+    
+    # Time O(m+n), space O(1)
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        """
-        #soln 2 - LeetCode backward, Time O(log(M+N)), Space O(1)
-        p1, p2 = m-1, n-1
-        for p in range(n+m-1, -1, -1):
-            if p2 < 0:
-                break
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
+        """     
+        ptr1, ptr2, ptr = m-1, n-1, n+m-1
+        while ptr1 >= 0 or ptr2 >= 0:
+            if ptr1 == -1:
+                nums1[ptr] = nums2[ptr2]
+                ptr2 -= 1
+            elif ptr2 == -1:
+                nums1[ptr] = nums1[ptr1]
+                ptr1 -= 1
+            elif nums2[ptr2] <= nums1[ptr1]:
+                nums1[ptr] = nums1[ptr1]
+                ptr1 -= 1
             else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
+                nums1[ptr] = nums2[ptr2]
+                ptr2 -= 1
+            ptr -= 1
+    
+#         ptr1, ptr2 = 0, 0
+#         ans = []
+#         while ptr1 < m or ptr2 < n:
+#             if ptr1 == m:
+#                 ans.append(nums2[ptr2])
+#                 ptr2 += 1
+#             elif ptr2 == n:
+#                 ans.append(nums1[ptr1])
+#                 ptr1 += 1
+#             elif nums1[ptr1] <= nums2[ptr2]:
+#                 ans.append(nums1[ptr1])
+#                 ptr1 += 1
+#             else: 
+#                 ans.append(nums2[ptr2])
+#                 ptr2 += 1
         
-        # # soln 1 - Time O(log(M+N)), Space O(1), my second attempt
-        # if n == 0: return nums1
-        # i, j = 0, 0
-        # while j < n and i < m+n:
-        #     if nums1[i] > nums2[j]:
-        #         for k in range(len(nums1)-1,i-1,-1):
-        #             nums1[k] = nums1[k-1]
-        #         nums1[i] = nums2[j]
-        #         j += 1
-        #     i += 1 
-        # if j != n: nums1[j-n:] = nums2[j:]
-        
-        # # soln 0 - Time O(log(M+N)), Space O(N), my first attempt
-        # if n == 0: return nums1
-        # nums1[-n:] = nums2[:]
-        # nums1.sort()
-            
