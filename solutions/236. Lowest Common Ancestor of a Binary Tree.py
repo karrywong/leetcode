664ee6a94b8d27f67f72c52b6ab702b4639ffbef
Karry Wong <ucdwong@ucdavis.edu>
2024-03-02 15:19:08 -0800

    49 ms

--- solutions/236. Lowest Common Ancestor of a Binary Tree.py
+++ solutions/236. Lowest Common Ancestor of a Binary Tree.py
@@ -6,72 +6,48 @@
 #         self.right = None
 ​
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':         
-        # Karry's solution w Haotian's
-        htb = {} #key=cur.val, value=prev.val
-        queue = collections.deque([root])
-        pval, qval = p.val, q.val
-        bo1, bo2 = False, False
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode': 
+        #One pass with DFS
+        self.ans = None
+        def dfs(node: 'TreeNode', t1: int, t2: int) -> bool:
+            if node is None:
+                return False
+            left_bo = dfs(node.left, t1, t2)
+            right_bo = dfs(node.right, t1, t2)
+            cur = True if node.val == t1 or node.val == t2 else False
+            if left_bo+right_bo+cur >= 2: 
+                self.ans = node
+            return left_bo or right_bo or cur
         
-        while queue and (not bo1 or not bo2):
-            node = queue.popleft()
-            if node.val == pval: bo1 = True
-            elif node.val == qval: bo2 = True
-            if node.left:
-                htb[node.left] = node
-                queue.append(node.left)
-            if node.right:
-                htb[node.right] = node
-                queue.append(node.right)
-        #existing while loop, either queue is empty or p and q are both in htb
+        dfs(root, p.val, q.val)
+        return self.ans
         
-        pset = set([p])
-        while p.val != root.val:
-            p = htb[p]
-            pset.add(p)
+#         #Two-pass DFS, Time O(N), space O(N)
+#         def dfs(node: 'TreeNode', target: int) -> List['TreeNode']:
+#             if node is None:
+#                 return []
+#             if node.val == target:
+#                 return [node]
+            
+#             left_lst = dfs(node.left, target)
+#             right_lst = dfs(node.right, target) 
+            
+#             if len(left_lst) > 0:
+#                 left_lst.append(node)
+#                 return left_lst 
+#             if len(right_lst) > 0:
+#                 right_lst.append(node)
+#                 return right_lst
+#             return []
+            
+#         p_lst = dfs(root, p.val)
+#         q_lst = dfs(root, q.val)
         
-        while q.val != root.val:
-            if q in pset: return q
-            q = htb[q]
-        return q
+#         ptr = -1
+#         prev = None
+#         while abs(ptr) < len(p_lst)+1 and abs(ptr) < len(q_lst)+1 and p_lst[ptr].val == q_lst[ptr].val:
+#             prev = p_lst[ptr]                
+#             ptr -= 1
+#         return prev
 ​
-        # #Leetcode - DFS w recursion
-        # self.ans = None
-        # global pval, qval
-        # def dfs(node):
-        #     if not node: 
-        #         return False
-        #     left = dfs(node.left)
-        #     right = dfs(node.right)
-        #     mid = True if node.val == pval or node.val == qval else False
-        #     if left + right + mid >= 2:
-        #         self.ans = node
-        #     return mid or left or right
-        # pval, qval = p.val, q.val
-        # dfs(root)
-        # return self.ans
-​
-#         #soln 0 - Jake's soln 
-#         pval = p.val
-#         qval = q.val
-#         memo = {None:False} # memo[subtree_root] = True if subtree contains p or q
-        
-#         def helper(subtree):
-#             if subtree in memo: return memo[subtree]
-#             if subtree.val in [pval,qval]:
-#                 memo[subtree] = True
-#             else:
-#                 memo[subtree] = helper(subtree.left) or helper(subtree.right)
-#             return memo[subtree]
-#         curnode = root
-#         while True:
-#             if curnode.val in [pval,qval]:
-#                 return curnode
-#             left = helper(curnode.left)
-#             right = helper(curnode.right)
-#             if left and right:
-#                 return curnode
-#             if left:
-#                 curnode = curnode.left
-#             else:
-#                 curnode = curnode.right
+#         # Karry's solution w Haotian's
