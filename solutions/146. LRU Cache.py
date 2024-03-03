080ec9bc2e16a89ff914ab69acab4717b7c41d3c
Karry Wong <ucdwong@ucdavis.edu>
2024-03-03 15:59:55 -0800

    560 ms

--- solutions/146. LRU Cache.py
+++ solutions/146. LRU Cache.py
@@ -1,3 +1,25 @@
+# double linked list solution
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+​
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dict = {}
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+    def get(self, key: int) -> int:
+        if key not in self.dict:
+            return -1
+        node = self.dict[key]
+        self.remove(node)
         self.add(node)
         return node.val
 ​
@@ -51,23 +73,3 @@
 #             return -1
 #         self.dict.move_to_end(key)
 #         return self.dict[key]
-​
-#     def put(self, key: int, value: int) -> None:
-#         """
-#         :type key: int
-#         :type value: int
-#         :rtype: void
-#         """
-#         if key in self.dict:
-#             self.dict[key] = value #update
-#             self.dict.move_to_end(key)
-#             return
-​
-#         if len(self.dict) >= self.upper_bound:
-#             self.dict.popitem(last=False)
-#         self.dict[key] = value        
-​
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
