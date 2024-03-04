091cb74670be9a13931fb92b14fbcd6294ffa7d3
Karry Wong <ucdwong@ucdavis.edu>
2024-03-03 16:00:21 -0800

    560 ms

--- solutions/146. LRU Cache.py
+++ solutions/146. LRU Cache.py
@@ -1,3 +1,26 @@
+# double linked list solution
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+​
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dict = {}
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+    def get(self, key: int) -> int:
+        if key not in self.dict:
+            return -1
+        node = self.dict[key]
+        self.remove(node)
+        self.add(node)
         return node.val
 ​
     def put(self, key: int, value: int) -> None:
@@ -24,49 +47,3 @@
         prev_end = self.tail.prev
         prev_end.next = node
         node.prev = prev_end
-        node.next = self.tail
-        self.tail.prev = node
-    
-    def remove(self, node) -> None:
-        node.next.prev = node.prev
-        node.prev.next = node.next
-​
-# Use built-in Linked Hash map solution
-# from collections import OrderedDict
-# class LRUCache(object):
-#     def __init__(self, capacity: int):
-#         """
-#         :type capacity: int
-#         """
-#         self.upper_bound = capacity
-#         self.dict = OrderedDict() #{4:4, 3:3}
-​
-#     def get(self, key: int) -> int:
-#         """
-#         :type key: int
-#         :rtype: int
-#         """
-#         if key not in self.dict:
-#             return -1
-#         self.dict.move_to_end(key)
-#         return self.dict[key]
-​
-#     def put(self, key: int, value: int) -> None:
-#         """
-#         :type key: int
-#         :type value: int
-#         :rtype: void
-#         """
-#         if key in self.dict:
-#             self.dict[key] = value #update
-#             self.dict.move_to_end(key)
-#             return
-​
-#         if len(self.dict) >= self.upper_bound:
-#             self.dict.popitem(last=False)
-#         self.dict[key] = value        
-​
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
