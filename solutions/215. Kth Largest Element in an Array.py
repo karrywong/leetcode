51a753b720266fbfd8b5ec0a4b71cd1a2b5a9ab2
Karry Wong <ucdwong@ucdavis.edu>
2024-03-11 20:17:41 -0700

    468 ms

--- solutions/215. Kth Largest Element in an Array.py
+++ solutions/215. Kth Largest Element in an Array.py
@@ -47,3 +47,25 @@ class Solution:
 ​
 #         store_index = left
 #         for i in range(left, right):
+#             if nums[i] < pivot:
+#                 nums[store_index], nums[i] = nums[i], nums[store_index]
+#                 store_index += 1
+#         nums[right], nums[store_index] = nums[store_index], nums[right]
+#         return store_index
+​
+#     def quick_select(left, right, k_smallest) -> None:
+#         if left == right:
+#             return
+#         pivot_index = random.randint(left, right)
+#         pivot_index = partition(left, right, pivot_index)
+​
+#         if k_smallest == pivot_index:
+#             return
+#         elif k_smallest < pivot_index: #go left
+#             quick_select(left, pivot_index - 1, k_smallest)
+#         else: #go right
+#             quick_select(pivot_index + 1, right, k_smallest)
+​
+#     n = len(nums)
+#     quick_select(0, n - 1, n - k)
+#     return nums[n-k]
