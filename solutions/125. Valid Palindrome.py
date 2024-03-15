c2a000cee2eabf7c23708142d38999e95a7fa8a6
Karry Wong <ucdwong@ucdavis.edu>
2024-03-15 14:31:16 -0700

    39 ms

--- solutions/125. Valid Palindrome.py
+++ solutions/125. Valid Palindrome.py
@@ -1,13 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #Two pointers, time O(N), space O(N)
+        #Two pointers, time O(N), space O(1)
         i, j = 0, len(s)-1
         while i < j:
-            while i < j and not s[i].isalnum():
+            if not s[i].isalnum():
                 i += 1
-            while i < j and not s[j].isalnum():
+                continue
+        
+            if not s[j].isalnum():
                 j -= 1
-            
+                continue
+                
             if s[i].lower() != s[j].lower():
                 return False
             
@@ -15,7 +18,7 @@ class Solution:
             j -= 1
         return True
         
-        # #Direct reversal, time O(N), space O(N)
-        # temp = [l for l in s.lower() if l.isalnum()]
-        # # temp = "".join(temp)
-        # return temp[::-1] == temp
+        #Direct reversal, time O(N), space O(N)
+        temp = [l for l in s.lower() if l.isalnum()]
+        # temp = "".join(temp)
+        return temp[::-1] == temp
