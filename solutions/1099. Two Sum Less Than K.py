8b33801d16ef2859d86c8aa7b0a0e73d6e0d0191
Karry Wong <ucdwong@ucdavis.edu>
2024-03-27 12:05:47 -0700

    38 ms

--- solutions/1099. Two Sum Less Than K.py
+++ solutions/1099. Two Sum Less Than K.py
@@ -1,14 +1,33 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
-        #Sort + two pointers, time O(NlogN), Space O(1)
-        ans, diff, n = -1, float('inf'), len(nums)
+        # simpler binary search, time O(NlogN), space O(N)
         nums.sort()
-        l, r = 0, n-1
+        ans = -1
+        l, r = 0, len(nums)-1
         while l < r:
-            temp = nums[l] + nums[r]
-            if temp >= k:
-                r -= 1
-            else: #temp < k
+            temp_val = nums[l] + nums[r]
+            if temp_val < k:
                 l += 1
-                ans = max(ans, temp)
+                ans = max(ans, temp_val)
+            else:
+                r -= 1
         return ans
+        
+        # # # binary search, tricky, time O(NlogN), space O(N)
+        # nums.sort()
+        # ans = -1
+        # for i in range(len(nums)):
+        #     if nums[i] >= k: break
+        #     #for every Ai, find the largest Aj with Ai+Aj<K
+        #     l, r, res = i+1, len(nums)-1, i+1
+        #     target = k - nums[i]
+        #     while l <= r : 
+        #         mid = l + (r-l)//2
+        #         if nums[mid] < target:
+        #             res = mid
+        #             l = mid+1
+        #         else:
+        #             r = mid-1
+        #     if res < len(nums) and nums[res] < target:
+        #         ans = max(ans,nums[i] + nums[res])
+        # return ans
