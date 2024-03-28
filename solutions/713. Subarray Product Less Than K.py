4acb19db12f428cd98925f01583ea0ca249940b0
Karry Wong <ucdwong@ucdavis.edu>
2024-03-27 21:13:25 -0700

    860 ms

--- solutions/713. Subarray Product Less Than K.py
+++ solutions/713. Subarray Product Less Than K.py
@@ -1,29 +1,35 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        # #first attempt, prefix product + sliding windows
-        # #time O(N), space O(1)
-        n, ans = len(nums), 0
-        j, cur_prod = 0, 1
-        for i in range(n):
-            cur_prod *= nums[i]                
+        #first attempt, prefix product + sliding windows
+        #time O(N), space O(1)
+        ans = 0
+        cur_prod, j = 1, 0
+        for i, num in enumerate(nums):
+            cur_prod *= num
             while j <= i and cur_prod >= k:
                 cur_prod /= nums[j]
                 j += 1
             ans += i-j+1
         return ans
         
-#         #Leetcode Soln using binary search, idea: take log, consider prefix sum, prefix[i+1] = nums[0] + .. + nums[i]
-#         #for each i, the largest j so that nums[i] + ... + nums[j] = prefix[j] - prefix[i] < k
-#         #time O(NlogN), space O(1)
+#         # Lang like C++ that have overflow issue, use log
+#         # binary search + prefix sum
 #         if k == 0: return 0
-#         k = math.log(k)
-#         prefixSum = [0] * (len(nums)+1)
-#         for i in range(1, len(nums)+1):
-#             prefixSum[i] += prefixSum[i-1] + math.log(nums[i-1]) #prefix[i+1] = nums[0] + .. + nums[i]
-​
+#         log_k = math.log(k)
+#         logs_prefix_sum = [0]
+#         for num in nums:
+#             logs_prefix_sum.append(logs_prefix_sum[-1] + math.log(num)) #prefix[i+1] = nums[0] + .. + nums[i]
+            
 #         ans = 0 
-#         for i, p in enumerate(prefixSum):
-#             j = bisect.bisect(prefixSum, p+k-1e-9,i+1)
-#             ans += j-i-1
-#             # print(i,p, j, j-i-1)
+#         for i, log_sum in enumerate(logs_prefix_sum):
+#             sum_val = log_k + log_sum - 1e-9 
+#             l, r = i+1, len(logs_prefix_sum)
+#             while l < r:
+#                 mid = l + (r-l)//2
+#                 if logs_prefix_sum[mid] < sum_val:
+#                     l = mid+1
+#                 else: 
+#                     r = mid
+#             # j = bisect.bisect(logs_prefix_sum, log_sum + log_k - 1e-9, i+1)
+#             ans += l - i - 1
 #         return ans
