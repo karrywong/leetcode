7bc4779089b28bebd77f16d44b98baf71ac0bcb5
Karry Wong <ucdwong@ucdavis.edu>
2024-03-27 21:30:08 -0700

    50 ms

--- solutions/543. Diameter of Binary Tree.py
+++ solutions/543. Diameter of Binary Tree.py
@@ -6,16 +6,27 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # soln 0 - LeetCode DFS, time O(N), space O(N)
-        self.diameter = 0 
-        def helper(node):
-            if not node:
+        #Mock w/ Haotian 8/28/2022
+        #1. ans = max(ans, left_len + right_len + 1)
+        #2. return max(left_len, right_len)
+        ans = 0 #longestLen
+        #longest_path = []
+        def helper(node: TreeNode) -> int:
+            nonlocal ans
+            if not node: 
                 return 0
+            left_len = helper(node.left)
+            right_len = helper(node.right)
+            # left_len, left_path = helper(node.left)
+            # right_len, right_path = helper(node.right)
+            ans = max(ans, left_len+right_len)
             
-            left_path = helper(node.left)
-            right_path = helper(node.right)
-            self.diameter = max(self.diameter, left_path + right_path)
+            #path = left_path + [node.val] if left_len > right_len else right_path + [node.val]
+            # if left_len+right_len > longestLen:
+            #    longest_path = left_path + [node.val] + right_path[::-1]
             
-            return max(left_path, right_path) + 1
+            return max(left_len, right_len) + 1
+            #return longestLen, path
+        
         helper(root)
-        return self.diameter
+        return ans
