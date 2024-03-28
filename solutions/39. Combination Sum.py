d6eb93d21cf6c5a58d414c428ea2bba5940f107b
Karry Wong <ucdwong@ucdavis.edu>
2024-03-27 21:22:11 -0700

    51 ms

--- solutions/39. Combination Sum.py
+++ solutions/39. Combination Sum.py
@@ -1,31 +1,31 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        #Soln 2 - DP
-        # Initialize storage from 0 to target
-        dp = [[[]]] + [[] for _ in range(target)]
+#         #Soln 2 - DP
+#       # Initialize storage from 0 to target
+#         dp = [[[]]] + [[] for _ in range(target)]
 ​
-        # For each candidate, create valid lists from candidate to target
-        for c in candidates:
-            for i in range(c, target + 1):
-                dp[i] += [l + [c] for l in dp[i - c]]       
-            # print(c, dp)
-        return dp[target]
+#         # For each candidate, create valid lists from candidate to target
+#         for c in candidates:
+#             for i in range(c, target + 1):
+#                 dp[i] += [l + [c] for l in dp[i - c]]       
+#             print(c, dp)
+#         return dp[target]
         
-        # #soln 1 - Leetcode backtracking, optimized
-        # #Time O(N**(T/M+1)), space O(T/M)
-        # ans, n = [], len(candidates)
-        # def backtrack(num=target, start=0, A=[]):
-        #     if num == 0:
-        #         ans.append(A.copy())
-        #         return
-        #     if num < 0:
-        #         return
-        #     for i in range(start, n):
-        #         A.append(candidates[i])
-        #         backtrack(num-candidates[i], i, A)
-        #         A.pop()
-        # backtrack()
-        # return ans
+        #soln 1 - Leetcode backtracking, optimized
+        #Time O(N**(T/M+1)), space O(T/M), where T is the target value & M = min(candidates)
+        ans, n = [], len(candidates)
+        def backtrack(num=target, start=0, A=[]):
+            if num == 0:
+                ans.append(A[:])
+                return
+            if num < 0:
+                return
+            for i in range(start, n):
+                A.append(candidates[i])
+                backtrack(num-candidates[i], i, A)
+                A.pop()
+        backtrack()
+        return ans
         
         #soln 0 - first attempt, backtracking with hashtable, slow
 #         ans, seen, n = [], set(), len(candidates)
