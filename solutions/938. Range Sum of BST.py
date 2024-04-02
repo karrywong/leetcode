dc41ebfb8203a94e31c221c1dd5d4351f1391f49
Karry Wong <ucdwong@ucdavis.edu>
2024-04-02 15:46:59 -0700

    114 ms

--- solutions/938. Range Sum of BST.py
+++ solutions/938. Range Sum of BST.py
@@ -4,33 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:    
-    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
-        ### First attempt
-        if not root:
-            return 0
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+#         # bottom-to-top, time O(N), space O(N)
+#         ans = 0
+#         if root is None:
+#             return ans
         
-        answer = 0
-        if root.val <= high and root.val >= low:
-            answer += root.val
-        if root.val > low:
-            answer += self.rangeSumBST(root.left, low, high)
-        if root.val < high:
-            answer += self.rangeSumBST(root.right, low, high)
-        
-        return answer
+#         if root.val >= low:
+#             ans += self.rangeSumBST(root.left, low, high)
+#         if root.val <= high:
+#             ans += self.rangeSumBST(root.right, low, high)
+#         if low <= root.val <= high:
+#             ans += root.val
+#         return ans
 ​
-        ### Recursion - LeetCode answer
-        # def dfs(node):
-        #     if node:
-        #         if low <= node.val <= high:
-        #             self.ans += node.val
-        #         if low < node.val:
-        #             dfs(node.left)
-        #         if node.val < high:
-        #             dfs(node.right)
-        # self.ans = 0
-        # dfs(root)
-        # return self.ans
-                
-    
+        # top-to-bottom, time O(N), space O(N)
+        ans = 0
+        def dfs(node: Optional[TreeNode]) -> None:
+            nonlocal ans
+            if node is None:
+                return
+            if low <= node.val <= high:
+                ans += node.val
+            
+            if node.val > low:
+                dfs(node.left)
+            if node.val < high:
+                dfs(node.right)
+        dfs(root)
+        return ans
