0d9bc228a24825e824c74e6e7f4320f9e11c5232
Karry Wong <ucdwong@ucdavis.edu>
2024-04-01 17:19:00 -0700

    45 ms

--- solutions/206. Reverse Linked List.py
+++ solutions/206. Reverse Linked List.py
@@ -5,18 +5,18 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]: 
-        if (not head) or (not head.next):
-            return head
-        cur = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return cur
+#         if (not head) or (not head.next):
+#             return head
+#         cur = self.reverseList(head.next)
+#         head.next.next = head
+#         head.next = None
+#         return cur
     
-        # soln 2 - more elegant
-        prev, cur = None, head
-        while cur:
-            cur.next, prev, cur = prev, cur, cur.next
-        return prev
+#         # soln 2 - more elegant
+#         prev, cur = None, head
+#         while cur:
+#             cur.next, prev, cur = prev, cur, cur.next
+#         return prev
         
 #         # soln 1 - one pass, time O(N), space O(1)
 #         prev = None
@@ -28,15 +28,17 @@ class Solution:
 #             cur = next_temp
 #         return prev
     
-        # naive, time O(N), space O(N)
-#         lst = []
-#         cur = head
-#         while cur:
-#             lst.append(cur.val)
-#             cur = cur.next 
-#         ans = ListNode(None)
-#         cur = ans
-#         for i in range(len(lst)-1, -1, -1):
-#             cur.next = ListNode(lst[i])
-#             cur = cur.next
-#         return ans.next
+        # stack, time O(N), space O(N)
+        stack = []
+        cur = head 
+        while cur:
+            stack.append(cur)
+            cur = cur.next
+        if not stack: return None
+        ans = stack.pop()
+        cur = ans
+        while stack:
+            cur.next = stack.pop()
+            cur = cur.next
+        cur.next = None
+        return ans
