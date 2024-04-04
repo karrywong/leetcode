814c5ecf6cd2a50fad7c010201cc8116c01d818b
Karry Wong <ucdwong@ucdavis.edu>
2024-04-03 21:18:11 -0700

    80 ms

--- solutions/253. Meeting Rooms II.py
+++ solutions/253. Meeting Rooms II.py
@@ -1,8 +1,6 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # #First attempt, use sort, Time O(N^2), Space(N)
-        # if not intervals: 
-        #     return 0
+        # #Brute force, use sort, Time O(N^2), Space(N)
         # intervals.sort()
         # rooms = [intervals[0]]
         # for interval in intervals[1:]:
@@ -14,15 +12,30 @@ class Solution:
         #         rooms.append(interval)
         # return len(rooms)
         
-        #Priority Queue using Heap, Time O(NlogN), Space O(N)
-        if not intervals: 
-            return 0
-        free_rooms = []
-        intervals.sort()
-        heapq.heappush(free_rooms, intervals[0][1])
+        # # Heap, time O(NlogN), space O(N)
+        # intervals.sort()
+        # free_rooms = [intervals[0][1]]
+        # for interval in intervals[1:]:
+        #     if free_rooms[0] <= interval[0]:
+        #         heapq.heappop(free_rooms)
+        #     heapq.heappush(free_rooms, interval[1])
+        # return len(free_rooms)
         
-        for interval in intervals[1:]:
-            if free_rooms[0] <= interval[0]:
-                heapq.heappop(free_rooms)
-            heapq.heappush(free_rooms, interval[1])
-        return len(free_rooms)
+        # Two pointers, time O(NlogN), space O(N)
+        start_times = []
+        end_times = []
+        for s, e in intervals:
+            start_times.append(s)
+            end_times.append(e)
+        start_times.sort()
+        end_times.sort()
+        
+        s_ptr, e_ptr = 0, 0
+        used_rooms = 0
+        while s_ptr < len(start_times):
+            if start_times[s_ptr] >= end_times[e_ptr]:
+                used_rooms -= 1
+                e_ptr += 1
+            used_rooms += 1
+            s_ptr += 1
+        return used_rooms
