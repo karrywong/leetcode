321cca61b88536e1be29f05f491ccc7f09af9ceb
Karry Wong <ucdwong@ucdavis.edu>
2024-04-04 15:02:57 -0700

    41 ms

--- solutions/206. Reverse Linked List.py
+++ solutions/206. Reverse Linked List.py
@@ -5,40 +5,34 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]: 
-#         if (not head) or (not head.next):
-#             return head
-#         cur = self.reverseList(head.next)
-#         head.next.next = head
-#         head.next = None
-#         return cur
-    
-#         # soln 2 - more elegant
-#         prev, cur = None, head
-#         while cur:
-#             cur.next, prev, cur = prev, cur, cur.next
-#         return prev
+        # # recursive, time O(N), space O(N)
+        # if head is None or head.next is None:
+        #     return head
+        # cur = self.reverseList(head.next)
+        # head.next.next = head
+        # head.next = None
+        # return cur
         
-#         # soln 1 - one pass, time O(N), space O(1)
-#         prev = None
-#         cur = head
-#         while cur:
-#             next_temp = cur.next
-#             cur.next = prev
-#             prev = cur
-#             cur = next_temp
-#         return prev
-    
-        # stack, time O(N), space O(N)
-        stack = []
-        cur = head 
+        # iterative, time O(N), space O(1)
+        prev, cur = None, head
         while cur:
-            stack.append(cur)
-            cur = cur.next
-        if not stack: return None
-        ans = stack.pop()
-        cur = ans
-        while stack:
-            cur.next = stack.pop()
-            cur = cur.next
-        cur.next = None
-        return ans
+            # temp_next = cur.next
+            # cur.next = prev
+            # prev = cur
+            # cur = temp_next
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
+        
+        # # stack, time O(N), space O(N)
+        # stack = []
+        # cur = head
+        # while cur:
+        #     stack.append(cur)
+        #     cur = cur.next
+        # ans = ListNode(None)
+        # cur = ans
+        # while stack:
+        #     cur.next = stack.pop()
+        #     cur = cur.next
+        # cur.next = None
+        # return ans.next
