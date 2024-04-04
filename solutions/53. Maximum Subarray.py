1e8f0451fe4b80f0aeca165dedf0b424e1c7501f
Karry Wong <ucdwong@ucdavis.edu>
2024-04-04 16:39:53 -0700

    557 ms

--- solutions/53. Maximum Subarray.py
+++ solutions/53. Maximum Subarray.py
@@ -8,8 +8,9 @@ class Solution:
         
         #soln 1 - Kadane's algorithm, time O(N), space O(1)
         #https://en.wikipedia.org/wiki/Maximum_subarray_problem#Kadane's_algorithm
-        cur_sum = max_sum = nums[0]
-        for num in nums[1:]:
+        cur_sum = 0
+        max_sum = nums[0]
+        for num in nums:
             cur_sum = max(num, cur_sum+num)
             max_sum = max(max_sum, cur_sum)
         return max_sum
