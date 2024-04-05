6f06054d4e82f36f2b3ad4f12c4d7615d397226e
Karry Wong <ucdwong@ucdavis.edu>
2024-04-04 23:49:34 -0700

    105 ms

--- solutions/787. Cheapest Flights Within K Stops.py
+++ solutions/787. Cheapest Flights Within K Stops.py
@@ -1,35 +1,3 @@
-class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-#         #Soln 4 - Bellman-Ford algorithm using a DP approach, time O(k*E), space O(V)
-#         prev = [float('inf')] * n
-#         cur = [float('inf')] * n
-#         prev[src] = 0
-        
-#         for i in range(1,k+2):
-#             cur[src] = 0
-#             for u,v,cost in flights:
-#                 if prev[u] < float('inf'):
-#                     cur[v] = min(cur[v], prev[u]+cost)
-#             prev = cur[:]
-#         return -1 if cur[dst] == float('inf') else cur[dst]
-​
-#         # #Adjacency list for solns 1 -3 
-        graph = collections.defaultdict(set)
-        edge_weights = {}
-        for u,v,cost in flights:
-            graph[u].add(v)
-            edge_weights[(u,v)] = cost
-        
-#         #Soln 3 - Modified Dijstra's Algorithm, time O(ElogV), space O(E)
-        costs = [float("inf") for _ in range(n)]
-        stops_count = [float("inf") for _ in range(n)]
-        
-        hp = [(0,0,src)]
-        heapq.heapify(hp)
-        while hp:
-            cost, stops, node = heapq.heappop(hp)
-            if node == dst:
-                return cost
             
             if stops == k+1:
                 continue
@@ -68,3 +36,28 @@ class Solution:
 #         res = dfs(src, k)
 #         return -1 if res == float('inf') else res
             
+        #Soln 1 - First attempt DFS - LTE
+#         seen = {} #key: node, values: tuple(cost, stops)
+#         def dfs(node, stops=0, cost=0):
+#             if stops > k:
+#                 return
+            
+#             if node == dst:
+#                 if dst in seen:
+#                     seen[dst][0] = min(seen[dst][0], cost)
+#                 else:
+#                     seen[dst] = [cost,stops]
+#                 return
+            
+#             if node in seen:
+#                 if seen[node][0] <= cost and seen[node][1] <= stops:
+#                     return
+#             seen[node] = [cost, stops]
+            
+#             for ngh in graph[node]:
+#                 if ngh != dst:
+#                     dfs(ngh, stops+1, cost+edge_weights[(node, ngh)])
+#                 else:
+#                     dfs(ngh, stops, cost+edge_weights[(node, ngh)])
+#         dfs(src)
+#         return seen[dst][0] if dst in seen else -1
