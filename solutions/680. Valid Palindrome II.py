6eb6795cf84f976ffd07329bf8b53031dd2942da
Karry Wong <ucdwong@ucdavis.edu>
2024-04-04 21:37:13 -0700

    103 ms

--- solutions/680. Valid Palindrome II.py
+++ solutions/680. Valid Palindrome II.py
@@ -1,30 +1,21 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def check_palindrome(substr:str, l:int, r:int) -> bool:
+        def check_palindrome(l:int, r:int) -> bool:
             while l < r:
-                if substr[l] != substr[r]:
+                if s[l] != s[r]:
                     return False
                 l += 1
                 r -= 1
             return True
         
         left, right = 0, len(s)-1
+        while left < right and s[left] == s[right]:
+            left += 1
+            right -= 1
+        if left == right:
+            return True
+        else:
+            return check_palindrome(left+1, right) or check_palindrome(left, right-1)
         
-        while left < right:
-            if s[left] == s[right]:
-                left += 1
-                right -= 1
-            else:
-                return check_palindrome(s, left+1, right) or  check_palindrome(s, left, right-1)
-        return True
-        
-        # #Time O(N), space O(N)
-        # l, r = 0, len(s)-1 #left & right pointers
-        # while l < r:
-        #     if s[l] == s[r]:
-        #         l += 1 
-        #         r -= 1
-        #     else: 
-        #         return s[l+1:r+1] == s[l+1:r+1][::-1] or s[l:r] == s[l:r][::-1]
-        # return True
+                    
     
