b35cfb792a10d88b56eccbbf4536a56ef37781d2
Karry Wong <ucdwong@ucdavis.edu>
2024-04-20 14:36:12 -0700

    83 ms

--- solutions/1249. Minimum Remove to Make Valid Parentheses.py
+++ solutions/1249. Minimum Remove to Make Valid Parentheses.py
@@ -1,47 +1,54 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        #Leetcode two-pass string builder
-        # Pass 1: Remove all invalid ")"
-        first_pass_chars = []
-        balance = 0
-        open_seen = 0
-        for c in s:
-            if c == "(":
-                balance += 1
-                open_seen += 1
-            if c == ")":
-                if balance == 0:
-                    continue
-                balance -= 1
-            first_pass_chars.append(c)
-​
-        # Pass 2: Remove the rightmost "("
-        result = []
-        open_to_keep = open_seen - balance
-        for c in first_pass_chars:
-            if c == "(":
-                open_to_keep -= 1
-                if open_to_keep < 0:
-                    continue
-            result.append(c)
-​
-        return "".join(result)
-        
-#         #First attempt, mock interview on 10/24
-#         #Three passes, time O(N), space O(N)
-#         # eg1, "lee(t(c)o)de)" -> "(()))" -> "(())"
-#         stack = []
-#         tbremoved = []
-#         for i, char in enumerate(s): 
+#         #Two-pass string builder
+#         # Pass 1: compute balance; remove extra rightmost ")"
+#         first_pass_chars = []
+#         balance = 0
+#         for char in s:
 #             if char == "(":
-#                 stack.append((char,i))
-#             if char == ")":
-#                 if not stack or stack[-1][0] != "(":
-#                     tbremoved.append(i)
-#                 else:
-#                     stack.pop()
+#                 balance += 1
+#             elif char == ")":
+#                 if balance == 0:
+#                     continue
+#                 balance -= 1
+#             first_pass_chars.append(char)
+#         # balance >= 0     
         
-#         while stack: 
-#             tbremoved.append(stack.pop()[1])
+#         second_pass_chars = []
+#         for i in range(len(first_pass_chars)-1, -1, -1):
+#             c = first_pass_chars[i]
+#             if balance > 0 and c == "(":
+#                 balance -= 1
+#                 continue
+#             second_pass_chars.append(c)
+                
+#         return "".join(second_pass_chars[::-1])
         
-#         return ''.join([char for i, char in enumerate(s) if i not in tbremoved])        
+        # mock practice on 4/20
+        # time O(N), space O(N), N = len(s)
+        # pass with stack on (,), unbalance note index into removed index
+        # while stack ->into removed index
+        
+        to_removed = []
+        stack = [] # record ( index
+        
+        for i, char in enumerate(s):
+            if char == "(":
+                stack.append(i)
+            elif char == ")":
+                if stack:
+                    stack.pop()
+                else:
+                    to_removed.append(i)
+        
+        to_removed.extend(stack) # sorted
+        # return "".join([s[i] for i in range(len(s)) if i not in to_removed])
+        
+        ptr = 0
+        res = []
+        for i,char in enumerate(s):
+            if ptr < len(to_removed) and i == to_removed[ptr]:
+                ptr += 1
+                continue
+            res.append(char)
+        return "".join(res)
