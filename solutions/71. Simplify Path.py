b1b39fc5e617ad4ed0582be9f2b0e6820c773bc8
Karry Wong <ucdwong@ucdavis.edu>
2024-04-28 16:46:46 -0700

    41 ms

--- solutions/71. Simplify Path.py
+++ solutions/71. Simplify Path.py
@@ -1,38 +1,34 @@
 class Solution:
-    def simplifyPath(self, path: str) -> str:
-        #LeetCode soln, clean, time O(N), space O(N)
+    def simplifyPath(self, path: str) -> str:    
+        # time O(N), space O(N)
         stack = []
-        for p in path.split("/"):
-            if p == "..":
+        for ele in path.split("/"):
+            if ele == "..":
                 if stack:
-                    stack.pop()
-            elif p == "." or not p:
+                    stack.pop()          
+            elif ele == "" or ele == ".":
                 continue
             else:
-                stack.append(p)
-        return "/"+"/".join(stack)
+                stack.append(ele)
+                
+            # #TODO: 
+            # # not begin with digit
+            # if ele[0].isdigit() or ele[0] == "_":
+            #     continue
         
-#         #First attempt, code messy, time O(N), space O(N)
-#         stack = ['/']
-#         i = 0
-        
-#         while i < len(path):
-#             if path[i] == '/':
-#                 if stack and stack[-1] != '/':
-#                     stack.append(path[i])
-#             elif path[i] == "." and stack[-1] == "/":
-#                 count = 1
-#                 while i+1 < len(path) and path[i+1] == ".":
-#                     count += 1
-#                     i += 1
-#                 if (i+1 < len(path) and path[i+1] != "/") or count >= 3:
-#                     stack.append("."*count)
-#                 elif count == 2:
-#                     if len(stack) > 1 and stack[-1] == "/":
-#                         stack.pop()
-#                         while stack and stack[-1] != '/':
-#                             stack.pop()
-#             else:
-#                 stack.append(path[i])
-#             i += 1
-#         return ''.join(stack[:-1]) if len(stack) > 1 and stack[-1] == '/' else ''.join(stack)
+        return "/" + ("/").join(stack)
+    
+# Testing
+# path = "/home/"
+# path.split("/") = ["home"]
+# stack = ["home"] -> "/home"
+​
+# path = "/../"
+# path.split("/") = [".."]
+# stack = [] -> "/"
+​
+#  path = "/home//foo/"
+# path.split("/") = ["home", "", "foo"]
+# stack = ["home", "foo"] -> "/home/foo"
+​
+# path = "/home karry/" -> "/home karry"
