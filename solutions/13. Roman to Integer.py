5792781184dc77f1da8a931cf88ae28602b7e290
Karry Wong <ucdwong@ucdavis.edu>
2024-05-01 11:15:25 -0700

    38 ms

--- solutions/13. Roman to Integer.py
+++ solutions/13. Roman to Integer.py
@@ -1,20 +1,32 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        #soln 3 - backward
-        table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}     
+#         lookup = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+#         ans = lookup[s[-1]]
+        
+#         for i in range(len(s)-2, -1, -1):
+#             bo1 = s[i]=="I" and s[i+1] in ("V", "X")
+#             bo2 = s[i]=="X" and s[i+1] in ("L", "C")
+#             bo3 = s[i]=="C" and s[i+1] in ("D", "M")
+            
+#             val = -1 * lookup[s[i]] if bo1 or bo2 or bo3 else lookup[s[i]]
+#             ans += val
+#         return ans
+​
+        # soln 3 - backward
+        table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         ans = table[s[-1]]
         ptr = -2
         pre = s[-1]
         while ptr > -len(s)-1:
             val = table[s[ptr]]
-            if val < table[pre]: 
+            if val < table[pre]:
                 ans -= val
             else:
                 ans += val
             pre = s[ptr]
             ptr -= 1
         return ans
-        
+    
         # soln 2 - check set and if the next value is larger
         #table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         #check = set(['I','X','C'])
@@ -26,12 +38,3 @@ class Solution:
         #    else:
         #        ans += table[cur]
         #return ans
-        
-        # soln 1 - most primitive
-        #ans = 0
-        #htb = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
-        #n = len(s)
-        #ptr = 0
-        #while ptr < n:
-        #    if ptr == n-1:
-        #        ans += htb[s[ptr]]
