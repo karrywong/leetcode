e886b3e9975ca19949ec80571c658101d55224f3
Karry Wong <ucdwong@ucdavis.edu>
2024-05-01 11:47:16 -0700

    48 ms

--- solutions/3. Longest Substring Without Repeating Characters.py
+++ solutions/3. Longest Substring Without Repeating Characters.py
@@ -1,51 +1,35 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+#         # time O(N), space O(N)
+#         # dict = {a:0, b:1}
+#         # ans = max(ans, i-dict['b'])
+        
+#         # s="pwwkew"
+#         # (0,1,-1), (1,2,-1), (2,2,1),..., (4,3,1)
+        
+#         lookup = defaultdict(lambda:-1)
+#         ans = 0
+#         j = -1 # left bound
+#         for i, c in enumerate(s):
+#             if c in lookup:
+#                 j = max(j,lookup[c])
+#             ans = max(ans, i-j)
+#             lookup[c] = i
+#             # print(i,ans,j)
+#         return ans
+#     # (0,1,-1), (1,2,-1), (2,2,1), (3,2,)
+    
+​
         ### Soln 2 - two pointers + hashtable, optimized
         # sliding window, s[l:r+1] always no repeated character
-        n = len(s)
-        l = 0
-        seen = {} # dictionary time O(1); space O(n)
+        # time O(N), N = len(s), space O(N)
+        n, l = len(s), 0
+        seen = {}
         ans = 0
         
         for r, char in enumerate(s):
             if char in seen and l <= seen[char]:
                 l = seen[char] + 1
             seen[char] = r
-            ans = max(ans, r - l + 1)   
-        
+            ans = max(ans, r-l+1)
         return ans
-        
-#         ### Soln 1 - two pointers + hashtable O(N)
-#         lib = {}
-#         res = 0
-#         head = 0
-#         tail = 0
-​
-#         while tail < len(s):
-#             l = s[tail]
-#             if l not in lib:
-#                 lib[l] = ""
-#                 tail += 1
-#                 res = max(res, tail - head)
-#             else:
-#                 del lib[s[head]]
-#                 head += 1
-#         return res
-            
-    
-#         ### Soln 0 - brute force O(N^3), LeetCode solution
-#         n = len(s)
-#         ans = 0
-#         for i in range(n):
-#             for j in range(i+1, n+1):
-#                 if self.allUnique(s,i,j):
-#                     ans = max(ans, j-i)
-#         return ans
-    
-#     def allUnique(self, s, start, end) -> bool:
-#         set_letter = set()
-#         for i in range(start, end):
-#             if s[i] in set_letter:
-#                 return False
-#             set_letter.add(s[i])
-#         return True
