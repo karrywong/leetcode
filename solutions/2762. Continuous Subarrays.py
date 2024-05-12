567a0b9df07018a5c3a836b6eeb06e0431c7cc35
Karry Wong <ucdwong@ucdavis.edu>
2024-05-12 15:53:01 -0700

    570 ms

--- solutions/2762. Continuous Subarrays.py
+++ solutions/2762. Continuous Subarrays.py
@@ -8,41 +8,42 @@ class Solution:
         # [5,4,5,4,5,4,5,3,2,4]
         # [5,4,4,5,4,4,3,1]
         # [5,4,1,8]
-        
-        left = 0
+​
+        # shorter sliding window soln, inspired by votrubac
+        hash_map = {} #max size of 5 elements
+        left_ind = 0
         ans = 0
-        lower_bound = float('-inf')
-        upper_bound = float('inf')
-        hash_map = {}
-        
-        for right in range(len(nums)):
-            num = nums[right]
-            hash_map[num] = right
+        for right_ind, num in enumerate(nums):
+            hash_map[num] = right_ind
             
-            if lower_bound <= num <= upper_bound:
-                ans += right-left+1
-                lower_bound = max(num-2, lower_bound)
-                upper_bound = min(num+2, upper_bound)
-            else:
-                #find left
-                candidates = set(range(lower_bound, upper_bound+1))
-                candidates = candidates.difference(set(range(num-2, num+3)))
-                left = -1
-                for c in candidates:
-                    left = max(hash_map.get(c,-1), left)
-                if left == -1:
-                    left = right
-                    lower_bound = nums[right]-2
-                    upper_bound = nums[right]+2
-                else:
-                    left += 1
-                    temp = set()
-                    for x in range(nums[right]-2, nums[right]+3):
-                        if x in hash_map and hash_map[x] >= left:
-                            temp.add(x)
-                    lower_bound = max(temp)-2 #4-2=2
-                    upper_bound = min(temp)+2 #2+2=4
-                ans += right-left+1
-                
-            # print(num, left, right, ans, lower_bound, upper_bound, hash_map)
+            deleted_list = []
+            for k, idx in hash_map.items():
+                if abs(num-k) > 2:
+                    left_ind = max(left_ind, idx+1)
+                    deleted_list.append(k)
+            
+            for d in deleted_list:
+                del hash_map[d]
+                    
+            ans += right_ind - left_ind + 1
         return ans
+​
+        
+# long long continuousSubarrays(vector<int>& nums) {
+#     map<int, int> m;
+#     long long res = 0;
+#     for (int i = 0, j = 0; i < nums.size(); ++i) {
+#         auto [it, inserted] = m.insert({nums[i], i});
+#         if (!inserted)
+#             it->second = i;
+#         else {
+#             for (auto it1 = begin(m); nums[i] - it1->first > 2;) {
+#                 j = max(j, it1->second + 1);
+#                 m.erase(it1++);
+#             }
+#             for (auto it1 = prev(end(m)); it1->first - nums[i] > 2;) {
+#                 j = max(j, it1->second + 1);
+#                 m.erase(it1--);
+#             }
+#         }
+#         res += i - j + 1;
