5867440f7d00d88c90e284c11a5b6cc8d6c1c0d0
Karry Wong <ucdwong@ucdavis.edu>
2024-08-04 14:00:47 -0700

    52 ms

--- solutions/9. Palindrome Number.py
+++ solutions/9. Palindrome Number.py
@@ -1,3 +1,18 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x)[::-1] == str(x)
+        # return str(x)[::-1] == str(x)
+        
+        # Time O(log(x)), space O(1)
+        if x < 0:
+            return False
+        x_str = str(x)
+        left, right = 0, len(x_str)-1
+        
+        while left < right:
+            if x_str[left] != x_str[right]:
+                return False
+            left += 1
+            right -= 1
+            
+        return True
+            
