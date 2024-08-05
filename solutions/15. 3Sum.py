c2381636c2d9c54a55613617d9385e08c9b65b63
Karry Wong <ucdwong@ucdavis.edu>
2024-08-05 15:20:25 -0700

    865 ms

--- solutions/15. 3Sum.py
+++ solutions/15. 3Sum.py
@@ -1,19 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-    ## soln 3 - my solution
-        res, dups = set(), set()
-        def _is_two_sum(arr: List[int], target: int, j: int) -> bool: 
-            lookup = set() # target - num
-            for i, a in enumerate(arr[j+1:]):
-                if a in lookup:
-                    res.add(tuple(sorted([arr[j], a, target-a])))
-                lookup.add(target-a)
-​
+        ## soln 3 - my solution, time O(N^2), space O(N^2)
+        ans, seen = set(), set()
+        def _is_two_sum(nums: List[int], idx:int) -> None:
+            target = -nums[idx]
+            lookup = set() # target - x
+            for num in nums[idx+1:]:
+                if num in lookup:
+                    ans.add(tuple(sorted([num, target-num, nums[idx]])))
+                lookup.add(target-num)
+            return
+        
         for idx, num in enumerate(nums):
-            if num not in dups:
-                _is_two_sum(nums, -1*num, idx)
-                dups.add(num)
-        return list(res)
+            if num not in seen:
+                _is_two_sum(nums, idx)
+                seen.add(num)
+        return list(ans)
         
         # #soln 2 - Leetcode w no-sorting
         # res, dups = set(), set()
@@ -40,15 +42,3 @@ class Solution:
 #                 else:
 #                     ans.append([nums[i], nums[l], nums[r]])
 #                     l += 1
-#                     r -= 1
-#                     while l < r and nums[l] == nums[l-1]:
-#                         l += 1
-        
-#         ans = []
-#         nums.sort()
-#         for i in range(len(nums)-2):
-#             if nums[i] > 0:
-#                 break
-#             if i == 0 or nums[i] != nums[i-1]:
-#                 twoSum(i, ans)
-#         return ans
