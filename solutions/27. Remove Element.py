7fcae2ac7959146322396124ce854a4ab393fe46
Karry Wong <ucdwong@ucdavis.edu>
2024-08-05 10:46:01 -0700

    47 ms

--- solutions/27. Remove Element.py
+++ solutions/27. Remove Element.py
@@ -1,10 +1,18 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        #Recent attempt - reverse order
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i] == val:
-                nums.pop(i)
-        return len(nums)
+        # time O(N), space O(1)
+        ptr = len(nums)-1
+        for idx in range(len(nums)-1, -1, -1):
+            if nums[idx] == val:
+                nums[idx], nums[ptr] = nums[ptr], nums[idx]
+                ptr -= 1
+        return ptr+1
+​
+        # #Recent attempt - reverse order
+        # for i in range(len(nums)-1, -1, -1):
+        #     if nums[i] == val:
+        #         nums.pop(i)
+        # return len(nums)
         
         ### Soln 1 - two pointer
         # pointer = 0
@@ -13,17 +21,6 @@ class Solution:
         #         nums[pointer] = nums[i]
         #         pointer += 1
         # return pointer
-    
-        # ### Soln 2 - two pointer, optimal
-        # i = 0
-        # temp_length = len(nums)
-        # while i < temp_length:
-        #     if nums[i] == val:
-        #         nums[i] = nums[temp_length-1];
-        #         temp_length -= 1
-        #     else:
-        #         i += 1
-        # return temp_length
-        
+​
         # # Soln 3 - oneliner, clever idea from discussion
         # while val in nums: nums.remove(val)
