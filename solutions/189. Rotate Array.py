ec1382b46493d9fa6fbb8436219645cedb67a5f0
Karry Wong <ucdwong@ucdavis.edu>
2024-08-05 14:24:36 -0700

    163 ms

--- solutions/189. Rotate Array.py
+++ solutions/189. Rotate Array.py
@@ -3,26 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #soln 2 - cyclic replacements
-        n = len(nums)
-        k %= n
+        # soln 2 - cyclic replacements
+        # Time O(N), Space O(1)
         start, count = 0, 0
-        while count < n:
-            cur, prev = start, nums[start]
+        k %= len(nums)
+        
+        while count < len(nums):
+            cur, prev  = start, nums[start]
             while True:
-                next_id = (cur+k)%n
-                nums[next_id], prev = prev, nums[next_id]
-                cur = next_id
+                next_idx = (cur+k) % len(nums)
+                nums[next_idx], prev = prev, nums[next_idx]
+                cur = next_idx
                 count += 1
-                if start == cur: break
+                if start == cur:
+                    break
             start += 1
-            
-        # #soln 1 - reversal time O(n), space O(1)
+        return
+        
+        # # #soln 1 - reversal time O(n), space O(1)
         # nums.reverse()
         # k %= len(nums)
         # nums[:k] = nums[:k][::-1]
         # nums[k:] = nums[k::][::-1]
         
         # #soln 0 - violation of the in-place rule
+        #Time O(N * k), Space O(1)
         # for i in range(k):
         #     nums.insert(0, nums.pop())
