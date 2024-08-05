edabc0ba1acfac422b1c110ae6cf25a0f1b5351e
Karry Wong <ucdwong@ucdavis.edu>
2024-08-05 10:01:40 -0700

    49 ms

--- solutions/13. Roman to Integer.py
+++ solutions/13. Roman to Integer.py
@@ -12,29 +12,29 @@ class Solution:
 #             ans += val
 #         return ans
 ​
-        # soln 3 - backward
-        table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        ans = table[s[-1]]
-        ptr = -2
-        pre = s[-1]
-        while ptr > -len(s)-1:
-            val = table[s[ptr]]
-            if val < table[pre]:
-                ans -= val
-            else:
-                ans += val
-            pre = s[ptr]
-            ptr -= 1
-        return ans
+        # # soln 3 - backward
+        # table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        # ans = table[s[-1]]
+        # ptr = -2
+        # pre = s[-1]
+        # while ptr > -len(s)-1:
+        #     val = table[s[ptr]]
+        #     if val < table[pre]:
+        #         ans -= val
+        #     else:
+        #         ans += val
+        #     pre = s[ptr]
+        #     ptr -= 1
+        # return ans
     
-        # soln 2 - check set and if the next value is larger
-        #table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        #check = set(['I','X','C'])
-        #ans = 0;
-        #for i in range(0,len(s)):
-        #    cur = s[i]
-        #    if i < len(s) - 1 and table[s[i+1]] > table[cur]:
-        #        ans -= table[cur]
-        #    else:
-        #        ans += table[cur]
-        #return ans
+    # soln 2 - check set and if the next value is larger
+    # Time O(N), space O(1)
+        table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        check = set('IXC')
+        ans = 0
+        for i in range(0,len(s)):
+            if i < len(s) - 1 and table[s[i+1]] > table[s[i]]:
+                ans -= table[s[i]]
+            else:
+                ans += table[s[i]]
+        return ans
