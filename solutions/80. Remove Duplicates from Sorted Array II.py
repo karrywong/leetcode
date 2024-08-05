174dd65abf6b18466a3ef262f6a3926b504b45d9
Karry Wong <ucdwong@ucdavis.edu>
2024-08-05 11:58:40 -0700

    53 ms

--- solutions/80. Remove Duplicates from Sorted Array II.py
+++ solutions/80. Remove Duplicates from Sorted Array II.py
@@ -1,25 +1,18 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        #Recent attempt
-        for i in range(len(nums)-1, 1, -1):
-            if nums[i] == nums[i-1] == nums[i-2]:
-                nums.pop(i)
-        return len(nums)
+        ptr = 0
+        for idx, num in enumerate(nums):
+            if ptr <= 1 or nums[ptr-2] != num:
+                nums[ptr] = num
+                ptr += 1
+        return ptr
+​
+        # #Recent attempt
+        # for i in range(len(nums)-1, 1, -1):
+        #     if nums[i] == nums[i-1] == nums[i-2]:
+        #         nums.pop(i)
+        # return len(nums)
     
-        ### Soln 1 - two pointers, not optimal
-#         i = 1
-#         count = 1
-#         while i < len(nums):
-#             if nums[i-1] == nums[i]:
-#                 count += 1   
-#                 if count > 2: 
-#                     nums.pop(i)
-#                     i -= 1
-#             else:
-#                 count = 1
-#             i += 1 
-#         return len(nums)
-        
 #         ### Soln 2 - two pointers, optimal
 #         j = 1
 #         count = 1
