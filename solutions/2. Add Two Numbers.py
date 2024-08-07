885e482bd7fc66a1845db6ad285cbf3419a73740
Karry Wong <ucdwong@ucdavis.edu>
2024-08-07 07:58:09 -0700

    42 ms

--- solutions/2. Add Two Numbers.py
+++ solutions/2. Add Two Numbers.py
@@ -5,30 +5,30 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # time O(N), space O(N)
-        ans = ListNode(0)
+        ptr1, ptr2 = l1, l2
+        ans = ListNode(None)
         cur = ans
         carry = 0
+        
         while l1 or l2:
+            val1 = val2 = 0
             if l1:
-                x = l1.val 
-                l1 = l1.next 
-            else:
-                x = 0
+                val1 = l1.val
+                l1 = l1.next
                 
             if l2:
-                y = l2.val
+                val2 = l2.val
                 l2 = l2.next
-            else:
-                y = 0
-            total = x + y + carry
-            carry, val = divmod(total, 10)
-            cur.next = ListNode(val)
+            
+            carry, node_val = divmod(val1+val2+carry,10)
+            cur.next = ListNode(node_val)
             cur = cur.next
+        
         if carry:
             cur.next = ListNode(carry)
-        return ans.next
-    
+        
+        return ans.next 
+​
         # naive
         #n = 0
         #i = 0
