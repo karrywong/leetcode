cb3fdf94923bbde1e8f55b314c5c01321c54b2b8
Karry Wong <ucdwong@ucdavis.edu>
2024-08-07 09:17:20 -0700

    29 ms

--- solutions/138. Copy List with Random Pointer.py
+++ solutions/138. Copy List with Random Pointer.py
@@ -8,60 +8,36 @@ class Node:
 """
 ​
 class Solution:    
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        lookup = {} #key: old node, value: new node
-        ans = Node(0) #sentinel
-        ans_ptr = ans
-        cur = head
-        
-        while cur:
-            ans_ptr.next = Node(cur.val)
-            lookup[cur] = ans_ptr.next
-            cur = cur.next
-            ans_ptr = ans_ptr.next
-        
-        cur = head
-        ans_ptr = ans.next
-        while ans_ptr:
-            ans_ptr.random = lookup.get(cur.random)
-            cur = cur.next
-            ans_ptr = ans_ptr.next
-            
-        return ans.next
-​
-    # def __init__(self):
-    #         self.visitedHash = {}
-    
     # def copyRandomList(self, head: 'Node') -> 'Node':
-#         ## Soln 2 - Leetcode iterative w O(N) space
-#         def getClonedNode(node):
-#             if node:
-#                 if node in self.visitedHash:
-#                     return self.visitedHash[node]
-#                 else:
-#                     self.visitedHash[node] = Node(node.val, None, None)
-#                     return self.visitedHash[node]
-#             return None
+#         ## Soln 2 - my iterative w O(N) space, time O(N)
+#         hash_map = {} #key: old node, value: new node
+#         cur = head
+#         ans = ListNode(None)
+#         ans_ptr = ans
         
-#         if not head: return head
-#         old_node = head
-#         new_node = Node(old_node.val, None, None)
-#         self.visitedHash[old_node] = new_node
+#         while cur:
+#             ans_ptr.next = ListNode(cur.val)
+#             hash_map[cur] = ans_ptr.next
+#             cur = cur.next
+#             ans_ptr = ans_ptr.next
         
-#         while old_node:
-#             new_node.random = getClonedNode(old_node.random)
-#             new_node.next = getClonedNode(old_node.next)
-#             old_node = old_node.next
-#             new_node = new_node.next
-            
-#         return self.visitedHash[head]
-    
-        # # Soln 1 - Leetcode recursive w DFS
-        # if not head: return head
-        # if head in self.visitedHash:
-        #     return self.visitedHash[head]
-        # node = Node(head.val, None, None)
-        # self.visitedHash[head] = node
-        # node.next = self.copyRandomList(head.next)
-        # node.random = self.copyRandomList(head.random)
-        # return node
+#         cur = head
+#         ans_ptr = ans.next
+#         while cur:
+#             ans_ptr.random = hash_map.get(cur.random)
+#             cur = cur.next
+#             ans_ptr = ans_ptr.next
+#         return ans.next 
+​
+    def __init__(self):
+            self.visitedHash = {}
+    def copyRandomList(self, head: 'Node') -> 'Node':
+       # Soln 1 - Leetcode recursive w DFS, time O(N), space O(N)
+        if not head: return head
+        if head in self.visitedHash:
+            return self.visitedHash[head]
+        node = ListNode(head.val)
+        self.visitedHash[head] = node
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+        return node
