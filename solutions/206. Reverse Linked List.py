2469778562a041f98913bd41ce9af7e43feeb8e9
Karry Wong <ucdwong@ucdavis.edu>
2024-08-07 09:35:51 -0700

    43 ms

--- solutions/206. Reverse Linked List.py
+++ solutions/206. Reverse Linked List.py
@@ -5,24 +5,25 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]: 
-        # # recursive, time O(N), space O(N)
+        # iterative, time O(N), space O(1)
+        prev = None
+        cur = head
+        while cur:
+            # temp = cur.next
+            # cur.next = prev
+            # prev = cur
+            # cur = temp
+            cur.next, prev, cur = prev, cur, cur.next # one-step 
+        return prev
+    
+        # # recursion, time O(N), space O(N)
         # if head is None or head.next is None:
         #     return head
         # cur = self.reverseList(head.next)
         # head.next.next = head
         # head.next = None
         # return cur
-        
-        # iterative, time O(N), space O(1)
-        prev, cur = None, head
-        while cur:
-            # temp_next = cur.next
-            # cur.next = prev
-            # prev = cur
-            # cur = temp_next
-            cur.next, prev, cur = prev, cur, cur.next
-        return prev
-        
+    
         # # stack, time O(N), space O(N)
         # stack = []
         # cur = head
@@ -36,3 +37,5 @@ class Solution:
         #     cur = cur.next
         # cur.next = None
         # return ans.next
+    
+            
