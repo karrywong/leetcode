d99d4c9feee93d0d62406ba93e4211800b11c3ef
Karry Wong <ucdwong@ucdavis.edu>
2024-08-07 08:14:55 -0700

    41 ms

--- solutions/21. Merge Two Sorted Lists.py
+++ solutions/21. Merge Two Sorted Lists.py
@@ -5,31 +5,30 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # soln 0 - first attempt, iteration, Time O(n+m), Space O(n+m)
-        ans_ptr = ListNode(0)
-        ans = ans_ptr
-        l1_ptr, l2_ptr = l1, l2
-        while l1_ptr and l2_ptr:
-            if l1_ptr.val <= l2_ptr.val:
-                ans_ptr.next = ListNode(l1_ptr.val)
-                l1_ptr = l1_ptr.next
-            else:    
-                ans_ptr.next = ListNode(l2_ptr.val)
-                l2_ptr = l2_ptr.next
-            ans_ptr = ans_ptr.next
-        ans_ptr.next = l1_ptr if l1_ptr else l2_ptr
-        return ans.next
-    
-        # # soln 1 - Leetcode recursion, Time O(n+m), Space O(n+m)
-        # if l1 is None:
-        #     return l2
-        # elif l2 is None:
-        #     return l1
-        # elif l1.val < l2.val:
-        #     l1.next = self.mergeTwoLists(l1.next, l2)
-        #     return l1
-        # else:
-        #     l2.next = self.mergeTwoLists(l1, l2.next)
-        #     return l2
-            
+#         # Time O(n+m), n=len(l1), m=len(l2), space O(n+m)
+#         ans = ListNode(None)
+#         cur = ans
         
+#         while l1 and l2:
+#             if l1.val <= l2.val:
+#                 cur.next  = ListNode(l1.val)
+#                 l1 = l1.next
+#             else:
+#                 cur.next  = ListNode(l2.val)
+#                 l2 = l2.next
+#             cur = cur.next
+            
+#         cur.next = l1 if l1 else l2
+#         return ans.next 
+    
+        # Leetcode, recursion, time O(n+m), space O(n+m)
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+        elif l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
