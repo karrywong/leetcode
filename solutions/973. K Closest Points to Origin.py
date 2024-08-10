d89725c836bbf3de0875eb061bd7dccd11a38561
Karry Wong <ucdwong@ucdavis.edu>
2024-08-10 14:52:34 -0700

    577 ms

--- solutions/973. K Closest Points to Origin.py
+++ solutions/973. K Closest Points to Origin.py
@@ -1,13 +1,23 @@
+import heapq
+​
+​
 class Solution:
+    # time O(Nlogk), space O(k)
+    # k is small or N ~ k, heap is useful
+    # k ~ N/2, heap is similar as brute-force sorting O(NlogN)
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        #Heap, Time O(Nlogk), Space O(k)
-        return heapq.nsmallest(k, points, key=self.squared_distance)
-        
-#         # Sort the list with a custom comparator function, Time O(NlogN), Space O(N)
-#         points.sort(key=self.squared_distance)
-#         # Return the first k elements of the sorted list
-#         return points[:k]
+        hp = [] #[dist,x,y]; max heaap
+        heapq.heapify(hp)
+        for point in points:            
+            dist = -1*self.squared_distance(point)
+            if len(hp) < k:
+                heapq.heappush(hp,[dist,*point]) #O(logk)
+                continue
+            heapq.heappushpop(hp, [dist,*point]) #O(logk)
+         
+        return [[h[1], h[2]] for h in hp]
     
     def squared_distance(self, point: List[int]) -> int:
         """Calculate and return the squared Euclidean distance."""
-        return point[0] ** 2 + point[1] ** 2
+        return point[0]**2 + point[1]**2
+​
