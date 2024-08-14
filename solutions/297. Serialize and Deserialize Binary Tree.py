c80f17dadb8613059bef8d60e3ede0f5f3000aed
Karry Wong <ucdwong@ucdavis.edu>
2024-08-14 10:22:14 -0700

    104 ms

--- solutions/297. Serialize and Deserialize Binary Tree.py
+++ solutions/297. Serialize and Deserialize Binary Tree.py
@@ -4,45 +4,44 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque 
+​
 ​
 class Codec:
     #Leetcode soln - DFS, preorder transversal
     #Time: O(N), Space: O(N)
-    def serialize(self, root):
+    def serialize(self, root:Optional[TreeNode]) -> str:
         """Encodes a tree to a single string.
         
         :type root: TreeNode
         :rtype: str
         """
-        def preorder(node, string) -> str: #preorder traversal
-            if not node:
-                string += 'None,'
-            else:
-                string += str(node.val) + ','
-                string = preorder(node.left, string)
-                string = preorder(node.right, string)
-            return string
-        return preorder(root, '')
+        def preorder(node:Optional[TreeNode]) -> list[Optional[int]]:
+            if node is None:
+                return ['None']
+            return [str(node.val)] + preorder(node.left) + preorder(node.right)
+        return ",".join(preorder(root))
 ​
-    def deserialize(self, data):
+    def deserialize(self, data:str) -> Optional[TreeNode]:
         """Decodes your encoded data to tree.
         
         :type data: str
         :rtype: TreeNode
         """
-        def btFromPreorder(dq) -> Optional[TreeNode]:
-            if dq[0] == 'None':
-                dq.popleft()
+        def get_data(datas: list[str]) -> str:
+            i = datas.popleft()
+            yield i
+            
+        def helper() -> Optional[TreeNode]:
+            val = next(get_data(dq))
+            if val == 'None':
                 return None
-            
-            root = TreeNode(dq[0])
-            dq.popleft()
-            root.left = btFromPreorder(dq)
-            root.right = btFromPreorder(dq)
-            return root
-            
-        data_queue = deque(data.split(','))
-        return btFromPreorder(data_queue)
+            node = TreeNode(int(val))
+            node.left = helper()
+            node.right = helper()
+            return node
+        dq = deque(data.split(','))
+        return helper()
         
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
