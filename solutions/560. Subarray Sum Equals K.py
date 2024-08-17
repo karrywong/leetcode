ad1634ef5de8e1599ef40c33b3fd635005db25c7
Karry Wong <ucdwong@ucdavis.edu>
2024-08-17 14:31:46 -0700

    N/A

--- solutions/560. Subarray Sum Equals K.py
+++ solutions/560. Subarray Sum Equals K.py
@@ -3,35 +3,40 @@ from collections import defaultdict
 ​
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # Two ideas:
-        # cur_sum == target, ans += 1
-        # bookkeep # occurence of cur_sum
-        # if cur_sum-target in lookup, cur_sum - (cur_sum-target) = target
-        # ans += lookup[cur_sum-target]
+#         prefix sum + hash_table lib, time O(N), space O(N)
+#         x = cur_sum - k <> k = cur_sum - x = (a+...+f) - (a+b) = c+e=f
+#         [a, b, c, e, f, g]
         
-        # brute-force, compute all possible subarray, time: O(N^3), space O(1)
+        ans, cur_sum = 0, 0
+        lib = defaultdict(int)
+        for num in nums: 
+            cur_sum += num # 3
+            if cur_sum == k: ans += 1
         
-        # Time O(N), space O(N), N = len(nums)
-        ans = cur_sum = 0
-        lookup = defaultdict(int)
-        for num in nums:
-            cur_sum += num
-            if cur_sum == k:
-                ans += 1
-                
-            ans += lookup[cur_sum-k]
-            lookup[cur_sum] += 1
+            ans += lib[cur_sum-k] # 
+            lib[cur_sum] += 1
         return ans
-    
-    # [3,2,1], target = 3
-    # a = 1, cur_sum = 6, ans = 1
-    # ans += lookup[3]
-    # {3:1, 5:1, 6:1}
-    
-    # [1,1,1], target = 2
-    #      | 
-    # ans = 2, cur_sum = 3
-    # lookup = {1:1, 2:1, 3:1}
+​
+#         # failed attempt, time O(N)
+#         # two ptr = sliding window, time O(N), space O(1)
+#         ans, cur_sum = 0, 0
+#         left = 0
+#         for right, num in enumerate(nums):  # 2,1
+#             cur_sum += num
+#             if cur_sum == k: ans += 1
             
+#             # while left < right and cur_sum >= k:
+#             while left < right and abs(cur_sum-nums[left]-k) < abs(cur_sum-k):
+#                 cur_sum -= nums[left]
+#                 left += 1
+#                 if cur_sum == k: ans += 1
+            
+#         return ans
+    
+    # testing
+    # nums = [1,1,1], k = 2, cur_sum = 1
+    #             |
+    # left = 2
+    # ans = 1+1
         
         
