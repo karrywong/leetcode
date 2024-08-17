6536c13be93eaab37e0edfb67aa097a83523bf2c
Karry Wong <ucdwong@ucdavis.edu>
2024-08-16 18:05:51 -0700

    441 ms

--- solutions/215. Kth Largest Element in an Array.py
+++ solutions/215. Kth Largest Element in an Array.py
@@ -1,16 +1,21 @@
-# import heapq
-import random
+import heapq
+# import random
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-    # # start time: 7:40pm
-    # # min heap, time O(Nlogk), space O(k)
+        # min heap, time O(Nlogk), space O(k)
         hp = []
         heapq.heapify(hp)
-        for num in nums: #num = 6
-            if len(hp) >= k:
-                heapq.heappushpop(hp, num) 
-            else:
+​
+        for num in nums:
+            if len(hp) < k:
                 heapq.heappush(hp, num)
+            elif hp[0] < num:
+                heapq.heappushpop(hp, num)
+# OR 
+            # if len(hp) >= k:
+            #     heapq.heappushpop(hp, num) 
+            # else:
+            #     heapq.heappush(hp, num)  
         return hp[0]
 ​
     # # time O(N), space O(len(cnt) = (max_val - min_val) / step_size)
@@ -22,11 +27,11 @@ class Solution:
     #         return i
     # return 
 ​
-    #Heap, time O(NlogK), Space O(k)
-    # return heapq.nlargest(k, nums)[-1]
+    # Heap, time O(NlogK), Space O(k)
+        # return heapq.nlargest(k, nums)[-1]
+        # return heapq.nsmallest(len(nums)-k+1,nums)[-1]
     
-    # num = [499, 1, 5, 40,....] 
-    
+    # num = [499, 1, 5, 40,....]
     # nums = [3,2,3,1,2,4,5,5,6], k = 4
     # hp = [4,5,5,6]
     # <- decreasing order
@@ -37,27 +42,3 @@ class Solution:
 #             # all elements less than pivot are on the left. Otherwise, on the right
 #             pivot = lst[p]
 #             lst[p], lst[r] = lst[r], lst[p]
-​
-#             store_ind = l
-#             for i in range(l,r):
-#                 if lst[i] < pivot:
-#                     lst[store_ind], lst[i] = lst[i], lst[store_ind]
-#                     store_ind += 1
-#             lst[store_ind], lst[r] = lst[r], lst[store_ind]
-#             return store_ind
-​
-#         def quick_select(left:int, right:int, k_smallest:int, lst:List[int]) -> None:
-#             if left == right:
-#                 return
-#             pivot_ind = random.randint(left, right)
-#             pivot_ind = partition(left, right, pivot_ind, lst)
-​
-#             if pivot_ind < k_smallest: #go right
-#                 quick_select(pivot_ind+1, right, k_smallest, lst)
-#             elif pivot_ind > k_smallest: #go left
-#                 quick_select(left, pivot_ind-1, k_smallest, lst)
-#             return
-​
-#         n = len(nums)
-#         quick_select(0, n-1, n-k, nums)
-#         return nums[n-k]
