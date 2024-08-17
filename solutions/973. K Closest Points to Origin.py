c707e67f47f8393a6d3c7f0fc4ea9cbf184dc2f0
Karry Wong <ucdwong@ucdavis.edu>
2024-08-16 17:50:44 -0700

    586 ms

--- solutions/973. K Closest Points to Origin.py
+++ solutions/973. K Closest Points to Origin.py
@@ -12,8 +12,8 @@ class Solution:
             dist = -1*self.squared_distance(point)
             if len(hp) < k:
                 heapq.heappush(hp,[dist,*point]) #O(logk)
-                continue
-            heapq.heappushpop(hp, [dist,*point]) #O(logk)
+            else:
+                heapq.heappushpop(hp, [dist,*point]) #O(logk)
         return [[h[1], h[2]] for h in hp]
     
     def squared_distance(self, point: List[int]) -> int:
