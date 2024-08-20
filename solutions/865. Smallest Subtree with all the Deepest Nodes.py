4a0a04ff0588c4ce4dec7b220fc5c6223dfba0d6
Karry Wong <ucdwong@ucdavis.edu>
2024-08-20 10:06:19 -0700

    39 ms

--- solutions/865. Smallest Subtree with all the Deepest Nodes.py
+++ solutions/865. Smallest Subtree with all the Deepest Nodes.py
@@ -6,32 +6,45 @@
 #         self.right = right
 class Solution:
     def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
-#         #Leetcode recursion, cleanest but hard to understand
-#         Result = collections.namedtuple("Result", ("node", "dist"))
-#         def dfs(node):
-#             # Return the result of the subtree at this node.
-#             if not node: return Result(None, 0)
-#             L, R = dfs(node.left), dfs(node.right)
-#             if L.dist > R.dist: return Result(L.node, L.dist + 1)
-#             if L.dist < R.dist: return Result(R.node, R.dist + 1)
-#             return Result(node, L.dist + 1)
-​
-#         return dfs(root).node
-​
-        def dfs(node) -> Tuple[Optional[TreeNode], int]:
-            if node is None:
-                return None, 0
-            left_node, left_dist = dfs(node.left)
-            right_node, right_dist = dfs(node.right)
-            if left_dist > right_dist: 
-                return left_node, left_dist+1
-            elif left_dist < right_dist:
-                return right_node, right_dist+1
-            else:
-                return node, left_dist+1
-        ans, _ = dfs(root)
-        # print(depth)
-        return ans
+        # two-pass, time O(N), space O(N)
+        lookup = {None:-1}
+        def helper(node, parent=None):
+            lookup[node] = lookup.get(parent) + 1
+            if node.left: 
+                helper(node.left, node)
+            if node.right:
+                helper(node.right, node)
+            return
+        helper(root)
+        depth = max(lookup.values())
+        
+        def get_ans(node) -> TreeNode:
+            if not node:
+                return None
+            if lookup[node] == depth:
+                return node
+            
+            left_ans = get_ans(node.left)
+            right_ans = get_ans(node.right)
+            return node if left_ans and right_ans else left_ans or right_ans
+        
+        return get_ans(root)
+        
+        # #Leetcode recursion, time O(N), space O(N)
+        # def dfs(node) -> Tuple[Optional[TreeNode], int]:
+        #     if node is None:
+        #         return None, 0
+        #     left_node, left_dist = dfs(node.left)
+        #     right_node, right_dist = dfs(node.right)
+        #     if left_dist > right_dist: 
+        #         return left_node, left_dist+1
+        #     elif left_dist < right_dist:
+        #         return right_node, right_dist+1
+        #     else:
+        #         return node, left_dist+1
+        # ans, _ = dfs(root)
+        # # print(depth)
+        # return ans
         
 #         #Karry's first attempt with Jake's help, DFS with memoization on depth, two passes
 #         htb = {} #key = node, value = depth
@@ -42,17 +55,3 @@ class Solution:
 #                 left = dfs(node.left)
 #                 right = dfs(node.right)
 #                 depth = max(left, right) + 1
-#                 htb[node] = depth
-#                 return depth
-#             return htb[node]
-#         dfs(root) #htb built
-        
-#         while root:
-#             left_depth = htb[root.left] if root.left else 0
-#             right_depth = htb[root.right] if root.right else 0
-#             if left_depth == right_depth:
-#                 return root
-#             elif left_depth > right_depth:
-#                 root = root.left
-#             else: #left_depth < right_depth
-#                 root = root.right
