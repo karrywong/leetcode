2dc09be448862b63c0f4465ff993dfb7bb3d17c7
Karry Wong <ucdwong@ucdavis.edu>
2024-08-20 11:21:05 -0700

    43 ms

--- solutions/270. Closest Binary Search Tree Value.py
+++ solutions/270. Closest Binary Search Tree Value.py
@@ -6,10 +6,16 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        # soln 1 - first attempt
-        ans = float('inf')
+        # soln 1 - time O(N), space O(N)
+        ans: float('inf')
+        diff = float('inf')
         while root:
-            ans = min(ans, root.val, key = lambda x: abs(target - x))
+            # ans = min(ans, root.val, key = lambda x: abs(target - x))
+            val = abs(root.val-target)
+            if val < diff or (val == diff and root.val < ans):
+                ans = root.val
+                diff = val
+                
             if root.val > target:
                 root = root.left
             elif root.val < target:
