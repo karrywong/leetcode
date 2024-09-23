592575c39dcddf504fb514af34cdfc35895ab08f
Karry Wong <ucdwong@ucdavis.edu>
2024-09-23 10:49:00 -0700

    44 ms

--- solutions/13. Roman to Integer.py
+++ solutions/13. Roman to Integer.py
@@ -1,5 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        # time O(N), space O(1)
+        lookup = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        ans = 0
+        for idx, char in enumerate(s):
+            if idx < len(s)-1 and lookup[char] < lookup[s[idx+1]]:
+                ans -= lookup[char]
+            else:
+                ans += lookup[char]
+        return ans
+​
 #         lookup = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 #         ans = lookup[s[-1]]
         
@@ -26,15 +36,3 @@ class Solution:
         #     pre = s[ptr]
         #     ptr -= 1
         # return ans
-    
-    # soln 2 - check set and if the next value is larger
-    # Time O(N), space O(1)
-        table = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        check = set('IXC')
-        ans = 0
-        for i in range(0,len(s)):
-            if i < len(s) - 1 and table[s[i+1]] > table[s[i]]:
-                ans -= table[s[i]]
-            else:
-                ans += table[s[i]]
-        return ans
