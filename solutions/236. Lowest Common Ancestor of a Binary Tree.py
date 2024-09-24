2664c2d79aed9902e9597c258f5b71585f16ae29
Karry Wong <ucdwong@ucdavis.edu>
2024-09-24 09:15:54 -0700

    53 ms

--- solutions/236. Lowest Common Ancestor of a Binary Tree.py
+++ solutions/236. Lowest Common Ancestor of a Binary Tree.py
@@ -1,30 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-​
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':         
-        # time O(N), space O(N)
-        self.ans = TreeNode()
-        def helper(node: 'TreeNode') -> bool:
-            if not node:
-                return False
-            left_val = helper(node.left)
-            right_val = helper(node.right)
-            val = True if node == p or node == q else False
-            if left_val + right_val + val == 2:
-                self.ans = node
-            return val or left_val or right_val
-        
-        helper(root)
-        return self.ans
-​
-​
-#         #Two-pass DFS, Time O(N), space O(N)
-#         def dfs(node: 'TreeNode', target: int) -> List['TreeNode']:
 #             if node is None:
 #                 return []
 #             if node.val == target:
@@ -54,3 +27,26 @@ class Solution:
 #         htb = {} #key=cur.val, value=prev.val
 #         queue = collections.deque([root])
 #         pval, qval = p.val, q.val
+#         bo1, bo2 = False, False
+        
+#         while queue and (not bo1 or not bo2):
+#             node = queue.popleft()
+#             if node.val == pval: bo1 = True
+#             elif node.val == qval: bo2 = True
+#             if node.left:
+#                 htb[node.left] = node
+#                 queue.append(node.left)
+#             if node.right:
+#                 htb[node.right] = node
+#                 queue.append(node.right)
+#         #existing while loop, either queue is empty or p and q are both in htb
+        
+#         pset = set([p])
+#         while p.val != root.val:
+#             p = htb[p]
+#             pset.add(p)
+        
+#         while q.val != root.val:
+#             if q in pset: return q
+#             q = htb[q]
+#         return q
