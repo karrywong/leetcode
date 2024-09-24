b9bd7fc6fcf476396f4b6b5919155c14e586236b
Karry Wong <ucdwong@ucdavis.edu>
2024-09-24 09:15:52 -0700

    53 ms

--- solutions/236. Lowest Common Ancestor of a Binary Tree.py
+++ solutions/236. Lowest Common Ancestor of a Binary Tree.py
@@ -7,23 +7,21 @@
 ​
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':         
-        # Time O(N), space O(N)
-        self.ans: 'TreeNode'
-        check = {p.val, q.val}
-        def dfs(node: 'TreeNode') -> bool:
-            if node is None:
+        # time O(N), space O(N)
+        self.ans = TreeNode()
+        def helper(node: 'TreeNode') -> bool:
+            if not node:
                 return False
-            
-            left_bo = dfs(node.left)
-            right_bo = dfs(node.right)
-            bo = node.val in check
-            if left_bo + right_bo + bo == 2: 
+            left_val = helper(node.left)
+            right_val = helper(node.right)
+            val = True if node == p or node == q else False
+            if left_val + right_val + val == 2:
                 self.ans = node
-                return True
-            return left_bo or right_bo or bo 
+            return val or left_val or right_val
         
-        dfs(root)
+        helper(root)
         return self.ans
+​
 ​
 #         #Two-pass DFS, Time O(N), space O(N)
 #         def dfs(node: 'TreeNode', target: int) -> List['TreeNode']:
@@ -45,38 +43,14 @@ class Solution:
             
 #         p_lst = dfs(root, p.val)
 #         q_lst = dfs(root, q.val)
-        
+​
 #         ptr = -1
-#         prev = None
-#         while abs(ptr) < len(p_lst)+1 and abs(ptr) < len(q_lst)+1 and p_lst[ptr].val == q_lst[ptr].val:
-#             prev = p_lst[ptr]                
+#         while abs(ptr-1) <= len(p_lst) and abs(ptr-1) <= len(q_lst) and p_lst[ptr-1].val == q_lst[ptr-1].val:
 #             ptr -= 1
-#         return prev
+#         return p_lst[ptr]
+​
 ​
 #         # Karry's solution w Haotian's
 #         htb = {} #key=cur.val, value=prev.val
 #         queue = collections.deque([root])
 #         pval, qval = p.val, q.val
-#         bo1, bo2 = False, False
-        
-#         while queue and (not bo1 or not bo2):
-#             node = queue.popleft()
-#             if node.val == pval: bo1 = True
-#             elif node.val == qval: bo2 = True
-#             if node.left:
-#                 htb[node.left] = node
-#                 queue.append(node.left)
-#             if node.right:
-#                 htb[node.right] = node
-#                 queue.append(node.right)
-#         #existing while loop, either queue is empty or p and q are both in htb
-        
-#         pset = set([p])
-#         while p.val != root.val:
-#             p = htb[p]
-#             pset.add(p)
-        
-#         while q.val != root.val:
-#             if q in pset: return q
-#             q = htb[q]
-#         return q
