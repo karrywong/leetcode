26fcd0052b91044c97b172055c044aec8c5f0403
Karry Wong <ucdwong@ucdavis.edu>
2024-09-24 13:14:52 -0700

    118 ms

--- solutions/1326. Minimum Number of Taps to Open to Water a Garden.py
+++ solutions/1326. Minimum Number of Taps to Open to Water a Garden.py
@@ -7,44 +7,37 @@ class Solution:
 #             for j in range(max(i - a + 1, 0), min(i + a, n) + 1):
 #                 dp[j] = min(dp[j], dp[max(0, i - a)] + 1)
 #         return dp[n] if dp[n] < float('inf') else -1
+​
+        # Alternative
+        max_reach = [0] * (n+1)
+        for i, w in enumerate(ranges):
+            l, r = max(i-w,0), min(i+w,n)
+            max_reach[l] = max(max_reach[l], r)
         
-        #Soln by liketheflower, time O(N), space O(N)
-        # For all location of taps, store the largest right reach point
-        max_right_end = list(range(n+1))
-        for i, a in enumerate(ranges):
-            # left, right = max(0, i-a), min(n, i+a)
-            # max_right_end[left] = max(max_right_end[left], right)
-            max_right_end[max(0, i-a)] = min(n, i+a) #optimized since from left to right, 2nd time always wider coverage
+        ans, l, r = 1, 0, max_reach[0]
+        while r < n:
+            new_r = max(max_reach[l:r+1])
+            if r == new_r:
+                return -1
+            l, r = r+1, new_r
+            ans += 1
+        return ans 
+            
+#         # Leetcode suggested greedy, time O(N), space O(N)
+#         max_reach = [0] * (n+1)
+#         for i, w in enumerate(ranges):
+#             l, r = max(i-w,0), min(i+w,n)
+#             max_reach[l] = max(max_reach[l], r)
         
-        count, l, r = 1, 0, max_right_end[0]
-        while True:
-            if n <= r : return count
-            new_r = max(max_right_end[l:r+1])
-            if new_r == r: return -1
-            l, r = r, new_r
-            count += 1
-        
-        # #Failed attempt but feel close to the answer in terms of the logic...
-        # intervals = []
-        # for i, dist in enumerate(ranges):
-        #     if dist > 0:
-        #         intervals.append([i-dist, i+dist])
-        # if not intervals:
-        #     return -1
-        # intervals.sort()
-        # # print(intervals)
-        # prev_end = intervals[0][1]
-        # ind = 1
-        # while ind < len(intervals) and intervals[ind][0] <= 0:
-        #     prev_end = max(prev_end, intervals[ind][1])
-        #     ind += 1
-        # count = 1
-        # # print(ind, prev_end)
-        # for interval in intervals[ind:]:
-        #     if n <= prev_end:
-        #         break
-        #     start, end = interval
-        #     if start <= prev_end < end:
-        #         count += 1
-        #         prev_end = end
-        # return count if n <= prev_end else -1
+#         ans = 0
+#         cur_end = 0 #current rightmost position 
+#         next_end = 0 #next rightmost position 
+#         for i in range(n+1):
+#             if i > next_end: 
+#                 return -1
+#             if i > cur_end:
+#                 ans += 1
+#                 cur_end = next_end
+#             next_end = max(next_end, max_reach[i])
+            
+#         return ans
