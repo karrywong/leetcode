3445b00d953099834f306084375feafec3ef664a
Karry Wong <ucdwong@ucdavis.edu>
2024-09-25 14:52:14 -0700

    453 ms

--- solutions/1091. Shortest Path in Binary Matrix.py
+++ solutions/1091. Shortest Path in Binary Matrix.py
@@ -28,3 +28,28 @@ class Solution:
                 continue
             _find_next_step(x,y,step,n)
         return ans if ans < upper_bound else -1
+​
+#         # DFS, time O(N), space O(N)
+#         if grid[0][0]==1: return -1
+#         n = len(grid)
+#         moves = [(1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1), (0,-1), (1,-1)]
+#         max_int = 10**4+1
+        
+#         def helper(x, y, cnt) -> int:
+#             if x == n-1 and y == n-1:
+#                 return cnt
+​
+#             if x<0 or x>=n or y<0 or y>=n or grid[x][y] != 0:
+#                 return max_int
+            
+#             grid[x][y] = 2
+#             cost = max_int
+#             for dx, dy in moves:
+#                 next_cnt = helper(x+dx, y+dy,cnt+1)
+#                 cost = min(cost, next_cnt)
+#             grid[x][y] = 0
+            
+#             return cost
+        
+#         ans = helper(0,0,1)
+#         return -1 if ans == max_int else ans
