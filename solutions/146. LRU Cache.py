861e7031e8b84efb1d7f0de2ec2ac40a855e497b
Karry Wong <ucdwong@ucdavis.edu>
2024-09-25 16:31:51 -0700

    514 ms

--- solutions/146. LRU Cache.py
+++ solutions/146. LRU Cache.py
@@ -1,49 +1,31 @@
-# double linked list solution
-class ListNode:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.next = None
-        self.prev = None
+from collections import OrderedDict
+​
 ​
 class LRUCache:
     def __init__(self, capacity: int):
+        self.ordered_dict = OrderedDict()
         self.capacity = capacity
-        self.dict = {}
-        self.head = ListNode(-1,-1)
-        self.tail = ListNode(-1,-1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
         
     def get(self, key: int) -> int:
-        if key not in self.dict:
-            return -1
-        node = self.dict[key]
-        self.remove(node)
-        self.add(node)
-        return node.val
+        if key in self.ordered_dict:
+            ans = self.ordered_dict[key] 
+            self.ordered_dict.move_to_end(key)
+        else: 
+            ans = -1
+        return ans
 ​
     def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            old_node = self.dict[key] 
-            self.remove(old_node)
-            # add new node
-            node = ListNode(key, value)
-            self.dict[key] = node
-            self.add(node)
+        if key in self.ordered_dict:
+            self.ordered_dict[key] = value
+            self.ordered_dict.move_to_end(key)
             return
-            
-        # Check capacity before inserting node
-        if len(self.dict) >= self.capacity:
-            node_to_delete = self.head.next
-            self.remove(node_to_delete)
-            del self.dict[node_to_delete.key]
-        # add new node
-        node = ListNode(key, value)
-        self.dict[key] = node
-        self.add(node)
-                
-    def add(self, node) -> None:
-        prev_end = self.tail.prev
-        prev_end.next = node
-        node.prev = prev_end
+        
+        if len(self.ordered_dict) >= self.capacity:
+            self.ordered_dict.popitem(last=False)
+        self.ordered_dict[key] = value
+        
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+​
