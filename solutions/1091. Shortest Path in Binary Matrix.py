af9249fbf4a80d22f1b44d3d98f0ab52f5e33b09
Karry Wong <ucdwong@ucdavis.edu>
2024-09-25 14:50:08 -0700

    453 ms

--- solutions/1091. Shortest Path in Binary Matrix.py
+++ solutions/1091. Shortest Path in Binary Matrix.py
@@ -1,47 +1,30 @@
-class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:        
-        #Optimized with overwrite, BFS, Time O(N), Space O(N)
-        #similar but easier than 752. Open the Lock
-        n = len(grid)
-        if grid[0][0]: return -1
-        queue = collections.deque([(0,0,1)])
-        grid[0][0] = 2
-        # seen = set() # can be optimized
-        moves = [(1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1), (0,-1), (1,-1)]
-        while queue:
-            r, c, count = queue.popleft()
-            if r == n-1 and c == n-1: return count
-            for r0, c0 in moves:
-                x,y = r+r0, c+c0
-                # if 0<=x<n and 0<=y<n and grid[x][y] == 0 and (x,y) not in seen:
-                    # seen.add((x,y))
-                    # queue.append((x,y,count+1))
-                if 0<=x<n and 0<=y<n and grid[x][y] == 0:
-                    grid[x][y] = 2
-                    queue.append((x,y,count+1))
-        return -1
-        
-#         # DFS, time O(N), space O(N)
-#         if grid[0][0]==1: return -1
-#         n = len(grid)
-#         moves = [(1,0), (1,1), (0,1), (-1,1), (-1,0), (-1,-1), (0,-1), (1,-1)]
-#         max_int = 10**4+1
-        
-#         def helper(x, y, cnt) -> int:
-#             if x == n-1 and y == n-1:
-#                 return cnt
+from collections import deque
 ​
-#             if x<0 or x>=n or y<0 or y>=n or grid[x][y] != 0:
-#                 return max_int
-            
-#             grid[x][y] = 2
-#             cost = max_int
-#             for dx, dy in moves:
-#                 next_cnt = helper(x+dx, y+dy,cnt+1)
-#                 cost = min(cost, next_cnt)
-#             grid[x][y] = 0
-            
-#             return cost
+​
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        #Optimized with overwrite, BFS, Time O(N^2), Space O(N^2)
+        #similar but easier than 752. Open the Lock
+        if grid[0][0] == 1:
+            return -1
+        n = len(grid)
+        directions = [(0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1)]
+        def _find_next_step(x:int, y:int, step:int, n:int) -> None:
+            for dx, dy in directions:
+                x_new, y_new = x+dx, y+dy
+                if 0<=x_new<n and 0<=y_new<n and grid[x_new][y_new] == 0 and (x_new,y_new) not in seen:
+                    queue.append((x_new, y_new, step+1))
+                    seen.add((x_new, y_new))
+            return
         
-#         ans = helper(0,0,1)
-#         return -1 if ans == max_int else ans
+        upper_bound = n*n+1
+        ans = upper_bound
+        queue = deque([(0,0,1)])
+        seen = set([(0,0)]) 
+        while queue:
+            x, y, step = queue.popleft()
+            if x == n-1 and y == n-1:
+                ans = min(ans, step)
+                continue
+            _find_next_step(x,y,step,n)
+        return ans if ans < upper_bound else -1
